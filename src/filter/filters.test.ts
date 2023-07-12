77dac8a79311ce13f7496d836c906fb593f64cfb
Jim OBrien <jimobrien930@gmail.com>
2023-07-12 00:18:38 -0700

    add filter parser and tests

---
+++ src/filter/filters.test.ts
@@ -0,0 +1,127 @@
+import {
+  apply,
+  handleAnd,
+  handleGreaterThanOrEqual,
+  handleOr,
+  handleRegex,
+  handleSome,
+} from './filters.js'
+import { describe, expect, test } from 'vitest'
+
+describe('parser', () => {
+  describe('$and', () => {
+    test('should return true when all conditions are met', () => {
+      const filter = [{ value: '0xa' }, { value: '0xb' }]
+      const context = { value: '0xa' }
+      expect(handleAnd(context, filter)).to.be.false
+    })
+  })
+
+  describe('$or', () => {
+    test('should return true when at least one condition is met', () => {
+      const filter = [{ value: '0xa' }, { value: '0xb' }]
+      const context = { value: '0xa' }
+      expect(handleOr(context, filter)).to.be.true
+    })
+  })
+
+  describe('$some', () => {
+    test('should return true when at least one item in the array meets the condition', () => {
+      const filter = { value: '0xa' }
+      const context = [{ value: '0xa' }, { value: '0xb' }]
+      expect(handleSome(context, filter)).to.be.true
+    })
+  })
+
+  describe('$gte', () => {
+    test('should return true when context is greater than or equal to filter', () => {
+      const filter = '0xa'
+      const context = '0xb'
+      expect(handleGreaterThanOrEqual(context, filter)).to.be.true
+    })
+  })
+
+  describe('$regex', () => {
+    test('should return true when the regular expression matches the context', () => {
+      const filter = '^0x[a-f0-9]*$'
+      const context = '0xabcd'
+      expect(handleRegex(context, filter)).to.be.true
+    })
+  })
+
+  describe('apply', () => {
+    test('should correctly filter the given transaction', () => {
+      const transaction = {
+        blockHash:
+          '0x2ed328c196cd31a299f51b8e7dc4dffef98184a37c0d8a1f40165f3fd9c668de',
+        blockNumber: '0x65715bb',
+        from: '0xac049e3e594af0f5293be1393b053ab68bea4142',
+        gas: '0x43707',
+        gasPrice: '0x777',
+        maxFeePerGas: '0x795',
+        maxPriorityFeePerGas: '0x735',
+        hash: '0x0cb83425ae1907fbfc27963c8b690dc7b435c7ed32d43a95f8e143628f6eb8e9',
+        input:
+          '0x3593564c000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000064a20643000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000001200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000008b587691a67824756000000000000000000000000000000000000000000000000000000000514379200000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000429e1028f5f1d5ede59748ffcee5532509976840e0000bb842000000000000000000000000000000000000060001f47f5c764cbc14f9669b88837ca1490cca17c31607000000000000000000000000000000000000000000000000000000000000',
+        nonce: '0xa8',
+        to: '0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad',
+        transactionIndex: '0x4',
+        value: '0x0',
+        type: '0x2',
+        accessList: [],
+        chainId: '0xa',
+        v: '0x0',
+        r: '0x1e1b9faa0b562914b5b4f58e2f4dc67115d8f036227a855bc6c5ec2c0be237f5',
+        s: '0x6d4c35b77da3a0d934f2e186944820498d12546ae57f014927b2497ae367b6fe',
+      }
+
+      const filter = {
+        chainId: '0xa',
+        to: '0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad',
+        input: {
+          $abi: [
+            {
+              inputs: [
+                { internalType: 'bytes', name: 'commands', type: 'bytes' },
+                { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
+                { internalType: 'uint256', name: 'deadline', type: 'uint256' },
+              ],
+              name: 'execute',
+              outputs: [],
+              stateMutability: 'payable',
+              type: 'function',
+            },
+          ],
+          sighash: '0x3593564c',
+          commands: '0x00',
+          inputs: {
+            $some: {
+              $abiParams: [
+                'address recipient',
+                'uint256 amountIn',
+                'uint256 amountOut',
+                'bytes path',
+                'bool payerIsUser',
+              ],
+              amountOut: {
+                $gte: '0x4c4b400',
+              },
+              path: {
+                $and: [
+                  {
+                    $regex: '^0x9e1028f5f1d5ede59748ffcee5532509976840e0',
+                  },
+                  {
+                    $regex: '.*7f5c764cbc14f9669b88837ca1490cca17c31607$',
+                  },
+                ],
+              },
+            },
+          },
+        },
+      }
+
+      expect(apply(transaction, filter)).to.be.true
+    })
+  })
+})
---
+++ src/filter/filters.ts
@@ -0,0 +1,174 @@
+import {
+  type Abi,
+  decodeAbiParameters,
+  decodeFunctionData,
+  getAbiItem,
+  parseAbiParameters,
+  slice,
+} from 'viem'
+
+export const handleAnd = (context: any, filter: Filter[]): boolean => {
+  for (let i = 0; i < filter.length; i++) {
+    if (!apply(context, filter[i])) return false
+  }
+  return true
+}
+
+export const handleOr = (context: any, filter: Filter[]): boolean => {
+  for (let i = 0; i < filter.length; i++) {
+    if (apply(context, filter[i])) return true
+  }
+  return false
+}
+
+export const handleSome = (context: any, filter: FilterObject): boolean => {
+  for (let i = 0; i < context.length; i++) {
+    const result = apply(context[i], filter)
+    if (result) return true
+  }
+  return false
+}
+
+export const handleLessThan = (
+  context: any,
+  filter: bigint | number | string,
+): boolean => {
+  return BigInt(context) < BigInt(filter)
+}
+
+export const handleLessThanOrEqual = (
+  context: any,
+  filter: bigint | number | string,
+): boolean => {
+  return BigInt(context) <= BigInt(filter)
+}
+
+export const handleGreaterThan = (
+  context: any,
+  filter: bigint | number | string,
+): boolean => {
+  return BigInt(context) > BigInt(filter)
+}
+
+export const handleGreaterThanOrEqual = (
+  context: any,
+  filter: bigint | number | string,
+): boolean => {
+  return BigInt(context) >= BigInt(filter)
+}
+
+export const handleFirst = (context: any, filter: FilterObject): boolean => {
+  return apply(context[0], filter)
+}
+
+export const handleLast = (context: any, filter: FilterObject): boolean => {
+  return apply(context[context.length - 1], filter)
+}
+
+export const handleRegex = (context: any, filter: string): boolean => {
+  const re = new RegExp(filter)
+  return re.test(context)
+}
+
+export const handleAbiDecode = (context: any, filter: { $abi: Abi }) => {
+  const sighash = slice(context, 0, 4)
+
+  const { functionName, args = [] } = decodeFunctionData({
+    abi: filter.$abi,
+    data: context,
+  })
+
+  const abiItem = getAbiItem({
+    abi: filter.$abi,
+    name: functionName,
+    args,
+  })
+
+  const namedArgs = [...abiItem.inputs].reduce(
+    (acc: Record<string, any>, input, index) => {
+      acc[`${input.name || index}`] = args[index]
+      return acc
+    },
+    {},
+  )
+
+  return { ...namedArgs, sighash, functionName }
+}
+
+export const handleAbiParamDecode = (
+  context: any,
+  filter: { $abiParams: string[] },
+) => {
+  const params = parseAbiParameters(filter.$abiParams.join(', '))
+  const args = decodeAbiParameters(params, context)
+  const namedArgs = params.reduce((acc: Record<string, any>, param, index) => {
+    acc[`${param.name || index}`] = args[index]
+    return acc
+  }, {})
+
+  return namedArgs
+}
+
+const operators = {
+  // Logical operators
+  $and: handleAnd,
+  $or: handleOr,
+
+  // Array operators
+  $some: handleSome,
+  $first: handleFirst,
+  $last: handleLast,
+
+  // Numeric operators
+  $lte: handleLessThanOrEqual,
+  $lt: handleLessThan,
+  $gte: handleGreaterThanOrEqual,
+  $gt: handleGreaterThan,
+  $regex: handleRegex,
+}
+
+const preprocessors = {
+  $abi: handleAbiDecode,
+  $abiParams: handleAbiParamDecode,
+}
+
+type OperatorKey = keyof typeof operators
+type PreprocessorKey = keyof typeof preprocessors
+
+type Primitive = string | number | boolean
+
+type FilterObject = {
+  [K in string]?: Filter
+}
+
+export type Filter = Primitive | FilterObject | Filter[]
+
+export function apply(
+  originalContext: Record<string, any>,
+  filters: any,
+): boolean {
+  let context = originalContext
+
+  for (const key in filters) {
+    if (!Object.hasOwnProperty.call(filters, key)) continue
+
+    if (key in preprocessors) {
+      context = preprocessors[key as PreprocessorKey](context, filters)
+      continue
+    }
+
+    if (key in operators) {
+      const operator = operators[key as OperatorKey]
+      if (!operator(context, filters[key])) return false
+      continue
+    }
+
+    if (typeof filters[key] === 'object') {
+      if (!apply(context[key], filters[key])) return false
+    } else if (context[key] !== filters[key]) {
+      return false
+    }
+  }
+
+  return true
+}
