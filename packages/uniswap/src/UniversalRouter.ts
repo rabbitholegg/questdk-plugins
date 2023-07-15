951fcf762717aeaf8fadb64d54b58aace2a4937e
Jim OBrien <jimobrien930@gmail.com>
2023-07-15 07:54:53 -0700

    fix lint, add rome

--- packages/uniswap/src/UniversalRouter.ts
+++ packages/uniswap/src/UniversalRouter.ts
@@ -1,65 +1,65 @@
-import type { SwapAction } from "@rabbitholegg/questdk";
-import type { FilterOperator } from "@rabbitholegg/questdk";
-import { compressJson } from "@rabbitholegg/questdk";
-import { UNIVERSAL_ROUTER_ADDRESS } from "@uniswap/universal-router-sdk";
-import { abi } from "@uniswap/universal-router/artifacts/contracts/UniversalRouter.sol/UniversalRouter.json";
-import { toHex } from "viem";
+import type { SwapAction } from '@rabbitholegg/questdk'
+import type { FilterOperator } from '@rabbitholegg/questdk'
+import { compressJson } from '@rabbitholegg/questdk'
+import { UNIVERSAL_ROUTER_ADDRESS } from '@uniswap/universal-router-sdk'
+import { abi } from '@uniswap/universal-router/artifacts/contracts/UniversalRouter.sol/UniversalRouter.json'
+import { toHex } from 'viem'
 
 const buildV3PathQuery = (tokenIn?: string, tokenOut?: string) => {
   // v3 paths are formatted as 0x<token><fee><token>
 
-  const conditions: FilterOperator[] = [];
+  const conditions: FilterOperator[] = []
 
   if (tokenIn) {
-    conditions.push({ $regex: `^${tokenIn}` });
+    conditions.push({ $regex: `^${tokenIn}` })
   }
 
   if (tokenOut) {
     // Chop the 0x prefix before comparing
-    conditions.push({ $regex: `${tokenOut.slice(2)}$` });
+    conditions.push({ $regex: `${tokenOut.slice(2)}$` })
   }
 
   return {
     $and: conditions,
-  };
-};
+  }
+}
 
 const buildV2PathQuery = (tokenIn?: string, tokenOut?: string) => {
   // v2 paths are formatted as [<token>, <token>]
-  const conditions: FilterOperator[] = [];
+  const conditions: FilterOperator[] = []
 
   if (tokenIn) {
-    conditions.push({ $first: tokenIn });
+    conditions.push({ $first: tokenIn })
   }
 
   if (tokenOut) {
-    conditions.push({ $last: tokenOut });
+    conditions.push({ $last: tokenOut })
   }
 
   return {
     $and: conditions,
-  };
-};
+  }
+}
 
 /* 
   Command param type definitions:
   https://github.com/Uniswap/universal-router-sdk/blob/6ec60ce9ff2853e236ba8f40a3aaa8819a97bd8b/src/utils/routerCommands.ts#L74
 */
 export const V3_SWAP_EXACT_TYPES = [
-  "address recipient",
-  "uint256 amountIn",
-  "uint256 amountOut",
-  "bytes path",
-  "bool payerIsUser",
-];
+  'address recipient',
+  'uint256 amountIn',
+  'uint256 amountOut',
+  'bytes path',
+  'bool payerIsUser',
+]
 
 export const V2_SWAP_EXACT_TYPES = [
-  "address recipient",
-  "uint256 amountIn",
-  "uint256 amountOut",
-  "address[] path",
-  "bool payerIsUser",
-];
+  'address recipient',
+  'uint256 amountIn',
+  'uint256 amountOut',
+  'address[] path',
+  'bool payerIsUser',
+]
 
 const swap = (swap: SwapAction) => {
   const {
@@ -70,13 +70,13 @@ const swap = (swap: SwapAction) => {
     amountIn,
     amountOut,
     recipient,
-  } = swap;
+  } = swap
 
   return compressJson({
     chainId: toHex(chainId),
     to: contractAddress || UNIVERSAL_ROUTER_ADDRESS(chainId),
     input: {
-      $abi: abi.filter((fragment) => fragment.name === "execute"),
+      $abi: abi.filter((fragment) => fragment.name === 'execute'),
       inputs: {
         $some: {
           $or: [
@@ -98,7 +98,7 @@ const swap = (swap: SwapAction) => {
         },
       },
     },
-  });
-};
+  })
+}
 
-export { swap };
+export { swap }
