0504689379eab8594b22f2e38d0fdaba851b2cb7
Quazia <alf40k@gmail.com>
2023-08-23 15:57:50 -0400

    Feat(template): chain-ids file constants

---
+++ packages/arbitrum/src/chain-ids.ts
@@ -0,0 +1,5 @@
+// The arbitrum Native Bridge only supports three chains, Arbitrum, Arbitrum Nova, and Ethereum
+export const CHAIN_ID_ARRAY = [1, 42161, 42170];
+export const ETH_CHAIN_ID = 1;
+export const ARB_ONE_CHAIN_ID = 42161;
+export const ARB_NOVA_CHAIN_ID = 42170;
