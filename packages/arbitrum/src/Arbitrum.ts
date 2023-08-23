154495c58fbe885a19368a7896764b264397d44c
Quazia <alf40k@gmail.com>
2023-08-23 16:15:12 -0400

    Feat(arbitrum): default address for bridge

--- packages/arbitrum/src/Arbitrum.ts
+++ packages/arbitrum/src/Arbitrum.ts
@@ -1,11 +1,11 @@
 
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
-import { CHAIN_ID_ARRAY} from './chain-ids'
+import { type Address, toHex } from 'viem'
+import { CHAIN_ID_ARRAY, ETH_CHAIN_ID, ARB_ONE_CHAIN_ID, ARB_NOVA_CHAIN_ID } from './chain-ids'
+import { MAINNET_TO_ARB_NOVA_GATEWAY, MAINNET_TO_ARB_ONE_GATEWAY, ARB_NOVA_TO_MAINNET_GATEWAY, ARB_ONE_TO_MAINNET_GATEWAY } from './contract-addresses'
 import { ArbitrumTokens } from './supported-token-addresses'
-import { type Address } from 'viem'
-
 // If you're implementing swap or mint, simply duplicate this function and change the name
-export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
+export const bridge = async (bridge: BridgeActionParams) => {
   // This is the information we'll use to compose the Transaction object
   const {
     sourceChainId,
@@ -16,14 +16,15 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
     recipient,
   } = bridge
 
+  const defaultContractAddress = getContractAddressFromChainId(sourceChainId, destinationChainId);
+
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
+    chainId: toHex(sourceChainId), // The chainId of the source chain
+    to:  contractAddress || defaultContractAddress,   // The contract address of the bridge
     input: {},  // The input object is where we'll put the ABI and the parameters
   })
 }
-
 
 export const getSupportedTokenAddresses = async (_chainId: number) => {
   return ArbitrumTokens[_chainId];
@@ -34,3 +35,17 @@ export const getSupportedChainIds = async () => {
   return CHAIN_ID_ARRAY;
 }
 
+const getContractAddressFromChainId = (sourceChainId: number, destinationChainId): Address => {
+  // This is klunky but the alternative is some sort of convoluted 2D mapping
+  if(sourceChainId === ARB_NOVA_CHAIN_ID )
+    return ARB_NOVA_TO_MAINNET_GATEWAY;
+  if(sourceChainId === ARB_ONE_CHAIN_ID )
+    return ARB_ONE_TO_MAINNET_GATEWAY;
+  if(sourceChainId === ETH_CHAIN_ID ) {
+    if(destinationChainId === ARB_NOVA_CHAIN_ID )
+      return MAINNET_TO_ARB_NOVA_GATEWAY;
+    if(destinationChainId === ARB_ONE_CHAIN_ID )
+      return MAINNET_TO_ARB_ONE_GATEWAY;
+  }
+
+}
