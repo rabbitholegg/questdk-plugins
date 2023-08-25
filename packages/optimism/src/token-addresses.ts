d1b9c0f34e6f56a9cc5ae4e239152aef5f04b5ad
Quazia <alf40k@gmail.com>
2023-08-25 17:35:14 -0400

    Feat(optimism): add token-addresses

--- packages/optimism/src/token-addresses.ts
+++ packages/optimism/src/token-addresses.ts
@@ -1,1 +1,4 @@
-const ETH_TOKEN_ADDRESS = '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000'
+export const ETH_TOKEN_ADDRESS = '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0000';
+export const USDC_TOKEN_ADDRESS = '0x7F5c764cBc14f9669B88837ca1490cCa17c31607';
+export const TETHER_TOKEN_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+
