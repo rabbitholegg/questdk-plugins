e9ab0c030e964a06f7d288ee22d5ea967cf7639b
Quazia <alf40k@gmail.com>
2023-08-29 19:05:05 -0400

    Feat(polygon): const supported token addresses

--- packages/polygon/src/supported-token-addresses.ts
+++ packages/polygon/src/supported-token-addresses.ts
@@ -5,20 +5,23 @@ import {
 } from './chain-ids.js'
 
 export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
-
+export const MATIC_ADDRESS_MAINNET = '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0';
+export const MATIC_ADDRESS_POLYGON = '0x0000000000000000000000000000000000001010';
+export const USDC_ADDRESS_MAINNET = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+export const WETH_ADDRESS_POLYGON = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619';
 // For now we're mainly supported the highest traffic Polygon tokens as listed here
 //https://github.com/maticnetwork/polygon-token-list
 export const PolygonTokens: Record<number, Address[]> = {
   [ETH_CHAIN_ID]: [
     ETHER_ADDRESS, //ether
-    '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0', //matic
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', //usdc
+    MATIC_ADDRESS_MAINNET, //matic
+    USDC_ADDRESS_MAINNET, //usdc
     '0xdac17f958d2ee523a2206206994597c13d831ec7', //usdt
     '0x6b175474e89094c44da98b954eedeac495271d0f', //dai
   ],
   [POLYGON_CHAIN_ID]: [
-    '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619', //ether
-    '0x0000000000000000000000000000000000001010', //matic
+    WETH_ADDRESS_POLYGON, //WETH
+    MATIC_ADDRESS_POLYGON, //matic
     '0x2791bca1f2de4661ed88a30c99a7a9449aa84174', //usdc
     '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', //usdt
     '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063', //dai
