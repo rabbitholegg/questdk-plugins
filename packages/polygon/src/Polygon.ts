65fecbeb2a78a8fc5015a8e8c38c1bcf8d041573
Quazia <alf40k@gmail.com>
2023-08-29 19:36:13 -0400

    Chore(polygon): cleanup comments&imports

--- packages/polygon/src/Polygon.ts
+++ packages/polygon/src/Polygon.ts
@@ -2,20 +2,20 @@
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
 import {POLYGON_BRIDGE_ABI_FUNCS} from './abi.js'
 import {POLYGON_CHAIN_ID, CHAIN_ID_ARRAY} from './chain-ids.js'
-import { PolygonTokens, ETHER_ADDRESS} from './supported-token-addresses.js'
+import { PolygonTokens, ETH_ADRESS_MAINNET} from './supported-token-addresses.js'
 import { MAINNET_BRIDGE } from './contract-addresses.js'
 // If you're implementing swap or mint, simply duplicate this function and change the name
-export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
+export const bridge = async (bridge: BridgeActionParams) => {
   // This is the information we'll use to compose the Transaction object
   const {
     sourceChainId,
-    destinationChainId,
     contractAddress,
     tokenAddress,
     amount,
     recipient,
   } = bridge
 
+  // L2 Transactions are the same for ETH and ERC20
   if(sourceChainId === POLYGON_CHAIN_ID) {
     return compressJson({
       chainId: sourceChainId, // The chainId of the source chain
@@ -26,7 +26,8 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
       },  
     })
   }
-  if(tokenAddress === ETHER_ADDRESS){
+  // Handle L1 ETH tx
+  if(tokenAddress === ETH_ADRESS_MAINNET){
     return compressJson({
       chainId: sourceChainId, // The chainId of the source chain
       to:  contractAddress || MAINNET_BRIDGE,   // on Polgon the contract that handles withdrawals is the token contract
@@ -37,6 +38,7 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
       },  
     })
   }
+  // Handle L1 ERC20 tx
   // The deposit function takes a bytes chunk as the last argument, and the amount is encoded in that value
   return compressJson({
     chainId: sourceChainId, // The chainId of the source chain
