1e8db41304eb76b340192545e11ea65918c2b1c7
Quazia <Quazia@users.noreply.github.com>
2023-08-30 00:45:23 +0000

    chore: format

--- packages/polygon/src/Polygon.ts
+++ packages/polygon/src/Polygon.ts
@@ -1,61 +1,56 @@
-
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
-import {POLYGON_BRIDGE_ABI_FUNCS} from './abi.js'
-import {POLYGON_CHAIN_ID, CHAIN_ID_ARRAY} from './chain-ids.js'
-import { PolygonTokens, ETH_ADRESS_MAINNET} from './supported-token-addresses.js'
+import { POLYGON_BRIDGE_ABI_FUNCS } from './abi.js'
+import { POLYGON_CHAIN_ID, CHAIN_ID_ARRAY } from './chain-ids.js'
+import {
+  PolygonTokens,
+  ETH_ADRESS_MAINNET,
+} from './supported-token-addresses.js'
 import { MAINNET_BRIDGE } from './contract-addresses.js'
 // If you're implementing swap or mint, simply duplicate this function and change the name
 export const bridge = async (bridge: BridgeActionParams) => {
   // This is the information we'll use to compose the Transaction object
-  const {
-    sourceChainId,
-    contractAddress,
-    tokenAddress,
-    amount,
-    recipient,
-  } = bridge
+  const { sourceChainId, contractAddress, tokenAddress, amount, recipient } =
+    bridge
 
   // L2 Transactions are the same for ETH and ERC20
-  if(sourceChainId === POLYGON_CHAIN_ID) {
+  if (sourceChainId === POLYGON_CHAIN_ID) {
     return compressJson({
       chainId: sourceChainId, // The chainId of the source chain
-      to:  contractAddress || tokenAddress,   // on Polgon the contract that handles withdrawals is the token contract
+      to: contractAddress || tokenAddress, // on Polgon the contract that handles withdrawals is the token contract
       input: {
         $abi: POLYGON_BRIDGE_ABI_FUNCS,
         amount: amount,
-      },  
+      },
     })
   }
   // Handle L1 ETH tx
-  if(tokenAddress === ETH_ADRESS_MAINNET){
+  if (tokenAddress === ETH_ADRESS_MAINNET) {
     return compressJson({
       chainId: sourceChainId, // The chainId of the source chain
-      to:  contractAddress || MAINNET_BRIDGE,   // on Polgon the contract that handles withdrawals is the token contract
+      to: contractAddress || MAINNET_BRIDGE, // on Polgon the contract that handles withdrawals is the token contract
       value: amount,
       input: {
         $abi: POLYGON_BRIDGE_ABI_FUNCS,
         user: recipient,
-      },  
+      },
     })
   }
   // Handle L1 ERC20 tx
   // The deposit function takes a bytes chunk as the last argument, and the amount is encoded in that value
   return compressJson({
     chainId: sourceChainId, // The chainId of the source chain
-    to:  contractAddress || MAINNET_BRIDGE,   // on Polgon the contract that handles withdrawals is the token contract
+    to: contractAddress || MAINNET_BRIDGE, // on Polgon the contract that handles withdrawals is the token contract
     input: {
       $abi: POLYGON_BRIDGE_ABI_FUNCS,
       user: recipient,
       rootToken: tokenAddress,
-    },  
+    },
   })
-
 }
 
 export const getSupportedTokenAddresses = async (_chainId: number) => {
-  return PolygonTokens[_chainId];
+  return PolygonTokens[_chainId]
 }
-
 
 export const getSupportedChainIds = async () => {
   // This function should return a list of supported chainIds
