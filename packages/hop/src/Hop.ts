548fe0d598cae07b3758bbefdac0c6c2f042ff70
Quazia <alf40k@gmail.com>
2023-08-31 18:27:18 -0400

    Chore(hop): cleanup getSupportedTokenAddresses

--- packages/hop/src/Hop.ts
+++ packages/hop/src/Hop.ts
@@ -3,8 +3,9 @@ import { mainnet as addresses } from '@hop-protocol/core/addresses'
 import { mainnet } from '@hop-protocol/core/networks'
 import { utils } from '@hop-protocol/sdk'
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
+import { type  Bridges, type Bridge, type L2BridgeProps, type L1BridgeProps } from './types.js'
 
-// If you're implementing swap or mint, simply duplicate this function and change the name
+
 export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
   // This is the information we'll use to compose the Transaction object
   const {
@@ -18,7 +19,7 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
 
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
+    chainId: sourceChainId, // The chainId of the source chain
     to:  0x0,   // The contract address of the bridge
     input: {},  // The input object is where we'll put the ABI and the parameters
   })
@@ -30,10 +31,11 @@ export const getSupportedTokenAddresses = (_chainId: number) => {
   // For each entry in bridge take the token address [ l1CanonicalToken or l2CanonicalToken ]
   if(addresses && addresses.bridges) {
     // TODO: This is _way_ too many any types - this needs to be tightened up
-    return Object.entries((addresses as any).bridges!).map(([, bridge] ) => {
-      if(bridge && (bridge as any)[chainSlug])
+    return Object.entries(addresses.bridges! as Bridges).map(([, bridge]: [string, Bridge] ) => {
       // Find the bridge element whose key matches the chainSlug and return the token address
-      return _chainId === 1 ? (bridge as any)[chainSlug].l1CanonicalToken : (bridge as any)[chainSlug].l2CanonicalToken
+      if(bridge && (bridge as any)[chainSlug])
+      return _chainId === 1 ? (bridge[chainSlug] as L1BridgeProps).l1CanonicalToken : (bridge[chainSlug] as L2BridgeProps).l2CanonicalToken
+      return '0x0'
     })
   }
   return []
