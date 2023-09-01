28f963886206c8ace21fd60d8916a7d4e05fcc5b
Quazia <alf40k@gmail.com>
2023-09-01 18:51:24 -0400

    TEST(hop): fix chain ids

--- packages/hop/src/Hop.test.ts
+++ packages/hop/src/Hop.test.ts
@@ -5,6 +5,9 @@ import { l1BridgeAbi, l2AmmWrapperAbi } from '@hop-protocol/core/abi'
 import { DEPOSIT_ETH, DEPOSIT_ERC20, WITHDRAW_ETH, WITHDRAW_ERC20 } from './test-transactions.js'
 const ETH_CHAIN_ID = 1
 const POLYGON_CHAIN_ID = 137
+const BASE_CHAIN_ID = 8453
+const OPTIMISM_CHAIN_ID = 10
+const ARBITRUM_ONE_CHAIN_ID = 42161
 export const ETH_ADRESS_MAINNET = '0x0000000000000000000000000000000000000000'
 export const WETH_ADDRESS_POLYGON = '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
 export const USDC_ADDRESS_MAINNET = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
@@ -95,24 +98,22 @@ describe('Given the optimism plugin', () => {
       const transaction = DEPOSIT_ETH
       const filter = await bridge({
         sourceChainId: ETH_CHAIN_ID,
-        destinationChainId: POLYGON_CHAIN_ID,
+        destinationChainId: BASE_CHAIN_ID,
         tokenAddress: ETH_ADRESS_MAINNET,
         amount: GreaterThanOrEqual(parseEther('.6')),
         recipient: '0xf9F31dc3399032304c6bdf93f3e028F70edD22A6',
       })
-
       expect(apply(transaction, filter)).to.be.true
     })
     test('should pass filter with valid L1 Token tx', async () => {
       const transaction = DEPOSIT_ERC20
       const filter = await bridge({
         sourceChainId: ETH_CHAIN_ID,
-        destinationChainId: POLYGON_CHAIN_ID,
+        destinationChainId: OPTIMISM_CHAIN_ID,
         tokenAddress: USDC_ADDRESS_MAINNET,
-        amount: GreaterThanOrEqual('250000000'), // $250 USDC,
+        amount: GreaterThanOrEqual('240000000'), // $250 USDC,
         recipient: '0x1A929b5d550C45ca8A0cFDB82F7c9C15FC278f31',
       })
-
       expect(apply(transaction, filter)).to.be.true
     })
     test('should pass filter with valid L2 ETH tx', async () => {
@@ -124,19 +125,17 @@ describe('Given the optimism plugin', () => {
         amount: GreaterThanOrEqual(parseEther('9')),
         recipient: '0x467B79AAfD7977F6d1E772e0b121047AC655C389'
       })
-
       expect(apply(transaction, filter)).to.be.true
     })
     test('should pass filter with valid L2 token tx', async () => {
       const transaction = WITHDRAW_ERC20
       const filter = await bridge({
         sourceChainId: POLYGON_CHAIN_ID,
-        destinationChainId: ETH_CHAIN_ID,
+        destinationChainId: ARBITRUM_ONE_CHAIN_ID,
         tokenAddress: HOP_ADDRESS_POLYGON,
         amount: GreaterThanOrEqual(parseEther('4952')),
         recipient: '0x34327028C727613872cd80122B9a489a4B9C0bFA'
       })
-
       expect(apply(transaction, filter)).to.be.true
     })
   })
