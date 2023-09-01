4c79c0033f68b2ef801e4282f0388f018cabd280
Quazia <alf40k@gmail.com>
2023-08-31 21:01:43 -0400

    Fix(hop): better guards on undefined data

--- packages/hop/src/Hop.ts
+++ packages/hop/src/Hop.ts
@@ -3,9 +3,9 @@ import { mainnet as addresses } from '@hop-protocol/core/addresses'
 import { mainnet } from '@hop-protocol/core/networks'
 import { utils } from '@hop-protocol/sdk'
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
-import { type  Bridges, type Bridge, type L2BridgeProps, type L1BridgeProps } from './types'
+import { type  Bridges, type Bridge, type L2BridgeProps, type L1BridgeProps } from './types.js'
 import { l1BridgeAbi, l2AmmWrapperAbi, l2BridgeAbi } from '@hop-protocol/core/abi'
-
+import { type Address } from 'viem'
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
 export const bridge = (bridge: BridgeActionParams) => {
@@ -19,12 +19,16 @@ export const bridge = (bridge: BridgeActionParams) => {
   } = bridge
   const bridges = addresses.bridges as Bridges
   const chainSlug = utils.chainIdToSlug("mainnet", sourceChainId);
+  console.log("ChainSlug", chainSlug)
   if(sourceChainId === 1) {
     const bridgeData: [string, Bridge] = Object.entries(bridges).filter(
       ([, bridge]: [string, Bridge] ) => 
-      { return (bridge[chainSlug] as L1BridgeProps).l1CanonicalToken === tokenAddress }
-      )[0]
-    const bridgeProps = (bridgeData[1][chainSlug] as L1BridgeProps)
+      { 
+        if(bridge[chainSlug]) return (bridge[chainSlug] as L1BridgeProps).l1CanonicalToken === tokenAddress 
+        else return false
+      })[0]
+      console.log("BridgeData", bridgeData)
+      const bridgeProps = (bridgeData[1][chainSlug] as L1BridgeProps)
     const contractTarget = bridgeProps.l1Bridge
     // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
     return compressJson({
@@ -38,7 +42,12 @@ export const bridge = (bridge: BridgeActionParams) => {
       },  // The input object is where we'll put the ABI and the parameters
     })
   } else {
-    const bridgeData: [string, Bridge] = Object.entries(bridges).filter(([, bridge]: [string, Bridge] ) => { return (bridge[chainSlug] as L2BridgeProps).l2CanonicalToken === tokenAddress })[0]
+
+    const bridgeData: [string, Bridge] = Object.entries(bridges).filter(([, bridge]: [string, Bridge] ) => { 
+      if(bridge[chainSlug]) return (bridge[chainSlug] as L2BridgeProps).l2CanonicalToken === tokenAddress 
+      else return false
+    })[0]
+    console.log("BridgeData", bridgeData)
     const bridgeProps = (bridgeData[1][chainSlug] as L2BridgeProps)
     // Currently only HOP lacks an AMM wrapper
     const hasAMM = bridgeProps.l2AmmWrapper !== ZERO_ADDRESS
@@ -58,7 +67,7 @@ export const bridge = (bridge: BridgeActionParams) => {
   }
 }
 // https://github.com/hop-protocol/hop/blob/develop/packages/core/src/metadata/tokens.ts
-export const getSupportedTokenAddresses = (_chainId: number) => {
+export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
   const chainSlug = utils.chainIdToSlug("mainnet", _chainId);
 
   // For each entry in bridge take the token address [ l1CanonicalToken or l2CanonicalToken ]
@@ -66,14 +75,14 @@ export const getSupportedTokenAddresses = (_chainId: number) => {
     return Object.entries(addresses.bridges! as Bridges).map(([, bridge]: [string, Bridge] ) => {
       // Find the bridge element whose key matches the chainSlug and return the token address
       if(bridge && (bridge as any)[chainSlug])
-      return _chainId === 1 ? (bridge[chainSlug] as L1BridgeProps).l1CanonicalToken : (bridge[chainSlug] as L2BridgeProps).l2CanonicalToken
+      return _chainId === 1 ? (bridge[chainSlug] as L1BridgeProps).l1CanonicalToken : (bridge[chainSlug] as L2BridgeProps).l2CanonicalToken 
       return '0x0'
-    })
+    }) as Address[]
   }
-  return []
+  return [] as Address[]
 }
 
 // https://github.com/hop-protocol/hop/blob/develop/packages/core/src/networks/mainnet.ts
-export const getSupportedChainIds = () => {
+export const getSupportedChainIds = async () => {
   return Object.entries(mainnet).map(([, chain]) => { return chain.networkId })
 }
