25f1f2bf73c9adf0f7756d8e9f0925f0edd040a4
Quazia <alf40k@gmail.com>
2023-09-05 15:25:24 -0400

    Feat(across): dupe template

---
+++ packages/across/src/index.ts
@@ -0,0 +1,23 @@
+// This file is standard for more projects.
+// The main degree of nuance is in the 
+
+import {
+  type IActionPlugin,
+  PluginActionNotImplementedError,
+} from '@rabbitholegg/questdk'
+
+import {
+  bridge,
+  getSupportedChainIds,
+  getSupportedTokenAddresses,
+} from './Across.js'
+
+// Replace *project* with the name of the project
+export const Across: IActionPlugin = {
+  pluginId: 'across',
+  getSupportedTokenAddresses,
+  getSupportedChainIds,
+  bridge,
+  swap: async () => new PluginActionNotImplementedError(),
+  mint: async () => new PluginActionNotImplementedError(),
+}
