f5f9f36de59e0af2aaa91f731c4f4c4cc68ad0e7
Quazia <alf40k@gmail.com>
2023-09-05 20:02:13 -0400

    Feat(across): getSupportedChainIds

--- packages/across/src/Across.ts
+++ packages/across/src/Across.ts
@@ -1,7 +1,7 @@
 
 import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
-
+import { CHAIN_ID_ARRAY } from './chain-ids.js'
 // If you're implementing swap or mint, simply duplicate this function and change the name
 export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
   // This is the information we'll use to compose the Transaction object
@@ -27,7 +27,6 @@ export const getSupportedTokenAddresses = async (_chainId: number): Promise<Addr
 }
 
 
-export const getSupportedChainIds = async (): Promise<number[]> => {
-  // This should return all of the ChainIds that are supported by the Project we're integrating
-
+export const getSupportedChainIds = async () => {
+  return CHAIN_ID_ARRAY
 }
