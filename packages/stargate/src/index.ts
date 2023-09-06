79c222ec07257d0d4e3cde3224ff988b429f6f91
Quazia <alf40k@gmail.com>
2023-09-06 16:53:01 -0400

    Feat(stargate): dupe template

---
+++ packages/stargate/src/index.ts
@@ -0,0 +1,23 @@
+// This file is standard for more projects.
+// The main degree of nuance is in the 
+
+import {
+  type IActionPlugin,
+  PluginActionNotImplementedError,
+} from '@rabbitholegg/questdk'
+
+import {
+  bridge,
+  getSupportedChainIds,
+  getSupportedTokenAddresses,
+} from './Stargate.js'
+
+// Replace *project* with the name of the project
+export const Stargate: IActionPlugin = {
+  pluginId: 'stargate',
+  getSupportedTokenAddresses,
+  getSupportedChainIds,
+  bridge,
+  swap: async () => new PluginActionNotImplementedError(),
+  mint: async () => new PluginActionNotImplementedError(),
+}
