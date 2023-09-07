d43988390e0474294e5610de1c0a36fe4d802251
Quazia <alf40k@gmail.com>
2023-09-07 17:56:49 -0400

    Feat(stargate): getTokenFromPool

---
+++ packages/stargate/src/get-token-from-pool.ts
@@ -0,0 +1,30 @@
+import { createPublicClient, http, type Address, Chain } from 'viem'
+
+// The only way to get the token is to lookup the factory, lookup the pool, then lookup the token
+export const getTokenFromPool = async (poolId: bigint, chainId:number, addr: Address) => {
+  const client = createPublicClient({
+    chain: { 
+      id: chainId
+    } as Chain,
+    transport: http(),
+  })
+  //Get Factory from Router
+  const factory = await client.readContract({
+    address: addr,
+    abi: [{"inputs":[],"name":"factory","outputs":[{"internalType":"contract Factory","name":"","type":"address"}],"stateMutability":"view","type":"function"}],
+    functionName: 'factory'
+  })
+  // Get the pool from the factory
+  const pool =  await client.readContract({
+    address: factory,
+    abi: [{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"getPool","outputs":[{"internalType":"contract Pool","name":"","type":"address"}],"stateMutability":"view","type":"function"}],
+    functionName: 'getPool',
+    args: [poolId]
+  })
+  // Get the token from the pool
+  return await client.readContract({
+    address: pool,
+    abi: [{"inputs":[],"name":"token","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}],
+    functionName: 'token'
+  })
+}
