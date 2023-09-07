f8fa7d4f0edcf420c8b72dde0358a0f3b15ea0ce
Quazia <alf40k@gmail.com>
2023-09-07 18:33:49 -0400

    Feat(stargate): add source pool decoding

--- packages/stargate/src/Stargate.ts
+++ packages/stargate/src/Stargate.ts
@@ -3,9 +3,10 @@ import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 import { STARGATE_BRIDGE_ABI } from './abi.js'
 import { CHAIN_ID_ARRAY } from './chain-ids.js'
+import { CHAIN_AND_POOL_TO_TOKEN_ADDRESS, CHAIN_ID_TO_ROUTER_ADDRESS, CHAIN_ID_TO_ETH_ROUTER_ADDRESS } from './contract-addresses.js'
 
 // If you're implementing swap or mint, simply duplicate this function and change the name
-export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
+export const bridge = async (bridge: BridgeActionParams) => {
   // This is the information we'll use to compose the Transaction object
   const {
     sourceChainId,
@@ -15,13 +16,19 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
     amount,
     recipient,
   } = bridge
-
+  const sourcePool = CHAIN_AND_POOL_TO_TOKEN_ADDRESS[sourceChainId][tokenAddress];
+  const targetContractAddress = sourcePool === 13 ? CHAIN_ID_TO_ETH_ROUTER_ADDRESS[sourceChainId] : CHAIN_ID_TO_ROUTER_ADDRESS[sourceChainId];
+  
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
+    chainId: sourceChainId, // The chainId of the source chain
+    to:  contractAddress || targetContractAddress,   // The contract address of the bridge
     input: {
       $abi: STARGATE_BRIDGE_ABI, // The ABI of the bridge contract
+      _srcPoolId: sourcePool,    // The source poolId
+      _amountLD: amount,         // The amount of tokens to send
+      _toAddress: recipient,     // The recipient of the tokens
+      _dstChainId: destinationChainId, // The chainId of the destination chain
     },  // The input object is where we'll put the ABI and the parameters
   })
 }
