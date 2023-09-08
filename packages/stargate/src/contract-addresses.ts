5599368a0e94c958cecad38f061689e3a4d17924
Quazia <Quazia@users.noreply.github.com>
2023-09-08 21:59:24 +0000

    chore: format

--- packages/stargate/src/contract-addresses.ts
+++ packages/stargate/src/contract-addresses.ts
@@ -58,8 +58,9 @@ export const CHAIN_ID_TO_ROUTER_ADDRESS = {
   [BASE_LAYER_ZERO_CHAIN_ID]: BASE_ROUTER,
 } as const
 
-
-export const CHAIN_AND_POOL_TO_TOKEN_ADDRESS: {[chainId: number] : {[address: string]:number}} = {
+export const CHAIN_AND_POOL_TO_TOKEN_ADDRESS: {
+  [chainId: number]: { [address: string]: number }
+} = {
   [ETH_LAYER_ZERO_CHAIN_ID]: {
     '0xdf0770dF86a8034b3EFEf0A1Bb3c889B8332FF56': 1,
     '0x38ea452219524bb87e18de1c24d3bb59510bd783': 2,
