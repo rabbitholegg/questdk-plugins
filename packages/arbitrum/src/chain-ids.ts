bcd6d7ccc6d058e38ab975114dc7b8c4b5a79832
Quazia <Quazia@users.noreply.github.com>
2023-09-12 16:58:46 +0000

    chore: format

--- packages/arbitrum/src/chain-ids.ts
+++ packages/arbitrum/src/chain-ids.ts
@@ -3,4 +3,8 @@ export const ETH_CHAIN_ID = 1
 export const ARB_ONE_CHAIN_ID = 42161
 export const ARB_NOVA_CHAIN_ID = 42170
 
-export const CHAIN_ID_ARRAY = [ETH_CHAIN_ID, ARB_ONE_CHAIN_ID, ARB_NOVA_CHAIN_ID]
+export const CHAIN_ID_ARRAY = [
+  ETH_CHAIN_ID,
+  ARB_ONE_CHAIN_ID,
+  ARB_NOVA_CHAIN_ID,
+]
