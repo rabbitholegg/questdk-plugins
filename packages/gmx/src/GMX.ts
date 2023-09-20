23e6425f7f9f58cd3fa44bdb8f8584dc52805525
Quazia <alf40k@gmail.com>
2023-09-20 19:57:46 -0400

    Feat(gmx): implement swap function

--- packages/gmx/src/GMX.ts
+++ packages/gmx/src/GMX.ts
@@ -1,11 +1,30 @@
 
-import { type SwapActionParams, compressJson } from '@rabbitholegg/questdk'
+import { type SwapActionParams, compressJson, type FilterOperator } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 import {
-  CHAIN_ID_ARRAY,
-  ARB_ONE_CHAIN_ID,
+  CHAIN_ID_ARRAY
 } from './chain-ids.js'
-export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> => {
+import { GMX_SWAP_ABI } from './abi.js'
+const buildPathQuery = (tokenIn?: string, tokenOut?: string) => {
+  // v3 paths are formatted as 0x<token><fee><token>
+
+  const conditions: FilterOperator[] = []
+
+  if (tokenIn) {
+    conditions.push({ $regex: `^${tokenIn}` })
+  }
+
+  if (tokenOut) {
+    // Chop the 0x prefix before comparing
+    conditions.push({ $regex: `${tokenOut.slice(2)}$` })
+  }
+
+  return {
+    $and: conditions,
+  }
+}
+
+export const swap = async (swap: SwapActionParams) => {
   // This is the information we'll use to compose the Transaction object
   const {
     contractAddress,
@@ -18,14 +37,19 @@ export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> =
 
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the swap
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    to:  contractAddress,   // The contract address of the swap
+    input: {
+      $abi:GMX_SWAP_ABI,
+      _path: buildPathQuery(tokenIn, tokenOut), // The path of the swap
+      _amountIn: amountIn,                     // The amount of the input token
+      _minOut: amountOut,                      // The minimum amount of the output token
+      _receiver: recipient,                    // The recipient of the output token
+    },  // The input object is where we'll put the ABI and the parameters
   })
 }
 
-export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
-
+export const getSupportedTokenAddresses = async (_chainId: number) => {
+  return []
 }
 
 
