9df98f7500ce8a202c2b91613be13ece72ea3a28
Quazia <Quazia@users.noreply.github.com>
2023-09-21 17:44:32 +0000

    chore: format

--- packages/gmx/src/GMX.test.ts
+++ packages/gmx/src/GMX.test.ts
@@ -1,4 +1,8 @@
-import { GreaterThanOrEqual, apply, type FilterOperator } from '@rabbitholegg/questdk/filter'
+import {
+  GreaterThanOrEqual,
+  apply,
+  type FilterOperator,
+} from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
 import { GMX_SWAP_ABI } from './abi.js'
 import { swap } from './GMX.js'
@@ -12,50 +16,46 @@ describe('Given the gmx plugin', () => {
   const USDT_ADDRESS = '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9'
   const TEST_USER = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619'
   describe('When handling the swap', () => {
-
     test('should return a valid action filter', async () => {
+      const filter = await swap({
+        chainId: ARB_ONE_CHAIN_ID,
+        contractAddress: GMX_ROUTER_ADDRESS,
+        tokenIn: BRIDGED_USDC_ADDRESS,
+        tokenOut: USDT_ADDRESS,
+        amountIn: GreaterThanOrEqual(100000n),
+        amountOut: GreaterThanOrEqual(100000n),
+        recipient: TEST_USER,
+      })
 
-    const filter = await swap({
-      chainId: ARB_ONE_CHAIN_ID,
-      contractAddress: GMX_ROUTER_ADDRESS,
-      tokenIn: BRIDGED_USDC_ADDRESS,
-      tokenOut: USDT_ADDRESS,
-      amountIn: GreaterThanOrEqual(100000n),
-      amountOut: GreaterThanOrEqual(100000n),
-      recipient: TEST_USER,
-    })
-
-    expect(filter).to.deep.equal({
-      to: GMX_ROUTER_ADDRESS,
-      chainId: ARB_ONE_CHAIN_ID,
-      input: {
-        $abi: GMX_SWAP_ABI,
-        _path: [BRIDGED_USDC_ADDRESS, USDT_ADDRESS],
-        _amountIn: {
-          $gte: '100000',
+      expect(filter).to.deep.equal({
+        to: GMX_ROUTER_ADDRESS,
+        chainId: ARB_ONE_CHAIN_ID,
+        input: {
+          $abi: GMX_SWAP_ABI,
+          _path: [BRIDGED_USDC_ADDRESS, USDT_ADDRESS],
+          _amountIn: {
+            $gte: '100000',
+          },
+          _minOut: {
+            $gte: '100000',
+          },
+          _receiver: TEST_USER,
         },
-        _minOut: {
-          $gte: '100000',
-        },
-        _receiver: TEST_USER,
-      },
-    })
+      })
     })
 
-    test('should pass filter with valid transactions',  async() => {
+    test('should pass filter with valid transactions', async () => {
       const transaction = SWAP_ETH
       const filter = await swap({
         chainId: ARB_ONE_CHAIN_ID,
         contractAddress: GMX_ROUTER_ADDRESS,
-        tokenIn: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f",
-        tokenOut: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+        tokenIn: '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f',
+        tokenOut: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
         amountIn: GreaterThanOrEqual(3400000),
-        amountOut: GreaterThanOrEqual(parseEther(".57")),
-        recipient: "0xDA63F22BF4bDC0B88536bDf4375fc9E14862ABD8",
+        amountOut: GreaterThanOrEqual(parseEther('.57')),
+        recipient: '0xDA63F22BF4bDC0B88536bDf4375fc9E14862ABD8',
       })
       expect(apply(transaction, filter)).to.be.true
     })
-  
-
   })
 })
