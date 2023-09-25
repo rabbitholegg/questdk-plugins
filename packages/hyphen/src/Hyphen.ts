a35ead6482e9b0fcacbfa9a791efa54d5c366385
Mmackz <matthewmackenz@gmail.com>
2023-09-25 12:55:05 -0700

    feat(hyphen): add bridge filter for ETH

--- packages/hyphen/src/Hyphen.ts
+++ packages/hyphen/src/Hyphen.ts
@@ -1,10 +1,17 @@
-
-import { type BridgeActionParams, compressJson } from '@rabbitholegg/questdk'
+import {
+  type BridgeActionParams,
+  type TransactionFilter,
+  compressJson,
+} from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
+import { ABI } from './abi.js'
+import { NATIVE_TOKEN_ADDRESS, CHAIN_TO_TOKENS } from './chain-to-tokens'
+import { CHAIN_TO_CONTRACT } from './chain-to-contract'
+import { type ChainIds, CHAIN_ID_ARRAY } from './chain-ids'
 
-// If you're implementing swap or mint, simply duplicate this function and change the name
-export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
-  // This is the information we'll use to compose the Transaction object
+export const bridge = async (
+  bridge: BridgeActionParams,
+): Promise<TransactionFilter> => {
   const {
     sourceChainId,
     destinationChainId,
@@ -14,20 +21,30 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
     recipient,
   } = bridge
 
-  // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
-  return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
-  })
+  const bridgeContract =
+    contractAddress ?? CHAIN_TO_CONTRACT[sourceChainId as ChainIds]
+
+  // if transfer is using the native gas token
+  if (tokenAddress === NATIVE_TOKEN_ADDRESS) {
+    return compressJson({
+      chainId: sourceChainId, // The chainId of the source chain
+      to: bridgeContract, // The contract address of the bridge
+      value: amount,
+      input: {
+        $abi: ABI,
+        toChainId: destinationChainId,
+        receiver: recipient,
+      },
+    })
+  }
 }
 
-export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
+export const getSupportedTokenAddresses = async (
+  _chainId: number,
+): Promise<Address[]> => {
   // Given a specific chain we would expect this function to return a list of supported token addresses
 }
 
-
 export const getSupportedChainIds = async (): Promise<number[]> => {
   // This should return all of the ChainIds that are supported by the Project we're integrating
-
 }
