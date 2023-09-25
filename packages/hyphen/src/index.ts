852c1d154c8a408a8ed0e0e578c8f1c1801a4205
Mmackz <matthewmackenz@gmail.com>
2023-09-25 12:52:40 -0700

    fix(hyphen): change plugin name

--- packages/hyphen/src/index.ts
+++ packages/hyphen/src/index.ts
@@ -1,6 +1,3 @@
-// This file is standard for more projects.
-// The main degree of nuance is in the 
-
 import {
   type IActionPlugin,
   PluginActionNotImplementedError,
@@ -10,9 +7,8 @@ import {
   bridge,
   getSupportedChainIds,
   getSupportedTokenAddresses,
-} from './Project.js'
+} from './Hyphen.js'
 
-// Replace *project* with the name of the project
 export const Hyphen: IActionPlugin = {
   pluginId: 'hyphen',
   getSupportedTokenAddresses,
