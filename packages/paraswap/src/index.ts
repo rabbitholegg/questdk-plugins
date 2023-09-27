ee14a81ee9202f338f4bf1edc256862f7856e7eb
Quazia <alf40k@gmail.com>
2023-09-27 18:56:04 -0500

    Feat(paraswap): Dupe template

---
+++ packages/paraswap/src/index.ts
@@ -0,0 +1,23 @@
+// This file is standard for more projects.
+// The main degree of nuance is in the 
+
+import {
+  type IActionPlugin,
+  PluginActionNotImplementedError,
+} from '@rabbitholegg/questdk'
+
+import {
+  swap,
+  getSupportedChainIds,
+  getSupportedTokenAddresses,
+} from './Paraswap.js'
+
+// Replace *project* with the name of the project
+export const Paraswap: IActionPlugin = {
+  pluginId: 'paraswap',
+  getSupportedTokenAddresses,
+  getSupportedChainIds,
+  bridge: async () => new PluginActionNotImplementedError(),
+  swap,
+  mint: async () => new PluginActionNotImplementedError(),
+}
