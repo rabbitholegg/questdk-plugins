43afb3f4ae5babf4de859caa40fd62abbbf30e03
Mmackz <matthewmackenz@gmail.com>
2023-09-30 16:33:44 -0700

    feat(basepaint): add constants for contract address and mint abi

---
+++ packages/basepaint/src/constants.ts
@@ -0,0 +1,16 @@
+// Contract Address for BasePaint
+export const CONTRACT_ADDRESS = "0xba5e05cb26b78eda3a2f8e3b3814726305dcac83";
+
+// Function Signature for Mint
+export const MINT_ABI = [
+   {
+      inputs: [
+         { internalType: "uint256", name: "day", type: "uint256" },
+         { internalType: "uint256", name: "count", type: "uint256" }
+      ],
+      name: "mint",
+      outputs: [],
+      stateMutability: "payable",
+      type: "function"
+   }
+];
