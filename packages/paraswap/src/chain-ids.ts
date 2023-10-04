125b444967812add03ee7dacd460e2a0d12089f3
Quazia <alf40k@gmail.com>
2023-10-04 15:09:13 -0400

    Feat(paraswap): add chain-ids

---
+++ packages/paraswap/src/chain-ids.ts
@@ -0,0 +1,8 @@
+// Hardcoded for now - can change this if we need more widespread support
+export const ETH_CHAIN_ID = 1
+export const ARB_ONE_CHAIN_ID = 42161
+
+export const CHAIN_ID_ARRAY = [
+  ETH_CHAIN_ID,
+  ARB_ONE_CHAIN_ID,,
+]
