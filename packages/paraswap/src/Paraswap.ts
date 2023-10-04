f74765ef4d26d31da94aba008211358e8dcfe2af
Quazia <alf40k@gmail.com>
2023-10-04 16:53:09 -0400

    Feat(paraswap): initial swap implementation

--- packages/paraswap/src/Paraswap.ts
+++ packages/paraswap/src/Paraswap.ts
@@ -2,29 +2,67 @@
 import { type SwapActionParams, compressJson } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 import { CHAIN_ID_ARRAY } from './chain-ids.js'
+import { constructGetTokens, constructFetchFetcher } from '@paraswap/sdk';
+import { PARASWAP_ABI } from './abi.js';
+const fetcher = constructFetchFetcher(window.fetch);
 
 // If you're implementing swap or mint, simply duplicate this function and change the name
-export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> => {
+export const swap = async (swap: SwapActionParams) => {
   // This is the information we'll use to compose the Transaction object
   const {
-    sourceChainId,
-    destinationChainId,
+    chainId,
     contractAddress,
-    tokenAddress,
-    amount,
+    tokenIn,
+    tokenOut,
+    amountIn,
+    amountOut,
     recipient,
   } = swap
 
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    chainId: chainId, // The chainId of the source chain
+    to:  contractAddress,   // The contract address of the bridge
+    input: {
+      $abiAbstract: PARASWAP_ABI,
+      $or: [
+        {
+          assets: [tokenIn, tokenOut],
+          funds: {
+            recipient: recipient,
+          },
+          fromAmount: amountIn,
+          expectedAmount: amountOut,
+        },
+        {
+          assets: [tokenIn, tokenOut],
+          funds: {
+            recipient: recipient,
+          },
+          fromAmount: amountIn,
+          expectedAmount: amountOut,
+        },
+        {
+          fromToken: tokenIn,
+          toToken: tokenOut,
+          fromAmount: amountIn,
+          expectedAmount: amountOut,
+        },
+        {
+          fromToken: tokenIn,
+          toToken: tokenOut,
+          fromAmount: amountIn,
+          toAmount: amountOut,
+        },
+      ]
+    },
   })
 }
 
 export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
-  // Given a specific chain we would expect this function to return a list of supported token addresses
+  // TODO: should we build this function for all chains? Might save a little compute having it pre-built once
+  const {getTokens} = constructGetTokens({chainId: _chainId, fetcher});
+  return (await getTokens()).map(token => (token.address as Address));
 }
 
 
