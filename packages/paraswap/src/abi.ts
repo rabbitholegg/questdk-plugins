7683deb2b455e0fb4defb4fea3e2db08c1f836fa
Quazia <alf40k@gmail.com>
2023-10-05 15:49:41 -0400

    Feat(paraswap): add camelot to ABI

--- packages/paraswap/src/abi.ts
+++ packages/paraswap/src/abi.ts
@@ -862,6 +862,110 @@ export const PARASWAP_ABI = [
         ],
         "stateMutability": "payable",
         "type": "function"
-    }
+    },
+    {
+        "inputs": [
+          {
+            "components": [
+              {
+                "internalType": "bytes",
+                "name": "path",
+                "type": "bytes"
+              },
+              {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+              },
+              {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "amountOutMinimum",
+                "type": "uint256"
+              }
+            ],
+            "internalType": "struct ISwapRouter.ExactInputParams",
+            "name": "params",
+            "type": "tuple"
+          }
+        ],
+        "name": "exactInput",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "amountOut",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "components": [
+              {
+                "internalType": "address",
+                "name": "tokenIn",
+                "type": "address"
+              },
+              {
+                "internalType": "address",
+                "name": "tokenOut",
+                "type": "address"
+              },
+              {
+                "internalType": "address",
+                "name": "recipient",
+                "type": "address"
+              },
+              {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint256",
+                "name": "amountOutMinimum",
+                "type": "uint256"
+              },
+              {
+                "internalType": "uint160",
+                "name": "limitSqrtPrice",
+                "type": "uint160"
+              }
+            ],
+            "internalType": "struct ISwapRouter.ExactInputSingleParams",
+            "name": "params",
+            "type": "tuple"
+          }
+        ],
+        "name": "exactInputSingle",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "amountOut",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "payable",
+        "type": "function"
+      },
 ]
-      
+
+
+
