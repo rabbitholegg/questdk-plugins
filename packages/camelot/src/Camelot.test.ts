2a8a71c78bf99b9ed4c3f27cec48aca9d8407acc
Quazia <alf40k@gmail.com>
2023-10-12 15:31:03 -0400

    Feat(camelot): dupe initial template

---
+++ packages/camelot/src/Camelot.test.ts
@@ -0,0 +1,20 @@
+import { GreaterThanOrEqual, apply } from '@rabbitholegg/questdk/filter'
+import { describe, expect, test } from 'vitest'
+
+describe('Given the camelot plugin', () => {
+  describe('When handling the bridge', () => {
+
+    test('should return a valid action filter', () => {
+      
+    })
+
+    test('should pass filter with valid transactions',  () => {
+      
+    })
+    
+    test('should not pass filter with invalid transactions',  () => {
+      
+    })
+
+  })
+})
---
+++ packages/camelot/src/index.ts
@@ -0,0 +1,22 @@
+// This file is standard for more projects.
+// The main degree of nuance is in the 
+
+import {
+  type IActionPlugin,
+  PluginActionNotImplementedError,
+} from '@rabbitholegg/questdk'
+
+import {
+  swap,
+  getSupportedChainIds,
+  getSupportedTokenAddresses,
+} from './Camelot.js'
+
+export const Camelot: IActionPlugin = {
+  pluginId: 'camelot',
+  getSupportedTokenAddresses,
+  getSupportedChainIds,
+  bridge: async () => new PluginActionNotImplementedError()
+  swap,
+  mint: async () => new PluginActionNotImplementedError(),
+}
