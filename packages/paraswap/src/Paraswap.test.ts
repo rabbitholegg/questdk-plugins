c757839c63418528a6e80c8552b7345391190464
Quazia <alf40k@gmail.com>
2023-10-11 20:52:57 -0400

    Test(paraswap): add invalid tx tests

--- packages/paraswap/src/Paraswap.test.ts
+++ packages/paraswap/src/Paraswap.test.ts
@@ -4,7 +4,7 @@ import { SWAP_MULTI, SWAP_SIMPLE } from './test-transactions'
 import { swap } from './Paraswap.js'
 import { ARB_ONE_CHAIN_ID } from './chain-ids.js'
 import { parseEther, type Address } from 'viem'
-
+import { PARASWAP_ABI } from './abi.js'
 const USDT_ADDRESS = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'
 const USDCE_ADDRESS = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8'
 const VELA_ADDRESS = '0x088cd8f5ef3652623c22d48b1605dcfe860cd704'
@@ -13,15 +13,14 @@ const AUGUSTUS_SWAPPER_ARBITRUM = '0xdef171fe48cf0115b1d80b88dc8eab59176fee57'
 describe('Given the paraswap plugin', () => {
   describe('When handling the bridge', () => {
 
-    test('should return a valid action filter', () => {
-      
+    test.only('should return a valid action filter', async () => {
     })
 
     test('should pass filter with valid transactions',  () => {
       
     })
     
-    test('should not pass filter with invalid transactions',  async () => {
+    test('should pass filter with valid simple transactions',  async () => {
       const transaction = SWAP_SIMPLE
       const filter = await swap({
         chainId: ARB_ONE_CHAIN_ID,
@@ -32,12 +31,34 @@ describe('Given the paraswap plugin', () => {
       })
       expect(apply(transaction, filter)).to.be.true
     })
-    test('should not pass filter with invalid transactions',  async () => {
+    test('should not pass filter with invalid simple transactions',  async () => {
+      const transaction = SWAP_SIMPLE
+      const filter = await swap({
+        chainId: ARB_ONE_CHAIN_ID,
+        contractAddress: AUGUSTUS_SWAPPER_ARBITRUM,
+        tokenIn: USDT_ADDRESS.toLowerCase() as Address,
+        tokenOut: USDCE_ADDRESS.toLowerCase() as Address,
+        amountIn: GreaterThanOrEqual(339000000000),
+      })
+      expect(apply(transaction, filter)).to.be.true
+    })
+    test('should pass filter with valid multi transactions',  async () => {
       const transaction = SWAP_MULTI
       const filter = await swap({
         chainId: ARB_ONE_CHAIN_ID,
         contractAddress: AUGUSTUS_SWAPPER_ARBITRUM,
         tokenIn: USDCE_ADDRESS.toLowerCase() as Address,
+        tokenOut: VELA_ADDRESS.toLowerCase() as Address,
+        amountOut: GreaterThanOrEqual(parseEther('0.037')),
+      })
+      expect(apply(transaction, filter)).to.be.true
+    })
+    test('should not pass filter with invalid transactions',  async () => {
+      const transaction = SWAP_MULTI
+      const filter = await swap({
+        chainId: ARB_ONE_CHAIN_ID,
+        contractAddress: AUGUSTUS_SWAPPER_ARBITRUM,
+        tokenIn: USDT_ADDRESS.toLowerCase() as Address,
         tokenOut: VELA_ADDRESS.toLowerCase() as Address,
         amountOut: GreaterThanOrEqual(parseEther('0.037')),
       })
