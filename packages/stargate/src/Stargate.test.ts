63f24f44797cf192e8763dd1cac6cf4c4984f75a
Mmackz <matthewmackenz@gmail.com>
2023-10-15 18:50:35 -0700

    test(stargate): replace instances of sgETH with native token address

--- packages/stargate/src/Stargate.test.ts
+++ packages/stargate/src/Stargate.test.ts
@@ -21,19 +21,17 @@ import { STARGATE_BRIDGE_ABI } from './abi.js'
 import { parseEther } from 'viem'
 import {
   NATIVE_CHAIN_AND_POOL_TO_TOKEN_ADDRESS,
+  NATIVE_TOKEN_ADDRESS,
   CHAIN_ID_TO_ROUTER_ADDRESS,
   CHAIN_ID_TO_ETH_ROUTER_ADDRESS,
 } from './contract-addresses.js'
 
 const ARBITRUM_USDC_ADDRESS = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'
 const ARBITRUM_USDT_ADDRESS = '0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9'
-const ARBITRUM_SGETH_ADDRESS = '0x82cbecf39bee528b5476fe6d1550af59a9db6fc0'
 
 const ETHEREUM_USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-const ETHEREUM_SGETH_ADDRESS = '0x72E2F4830b9E45d52F80aC08CB2bEC0FeF72eD9c'
 
 const OP_USDCE_ADDRESS = '0x7f5c764cbc14f9669b88837ca1490cca17c31607'
-const OP_SGETH_ADDRESS = '0xb69c8cbcd90a39d8d3d3ccf0a3e968511c3856a0'
 
 const TEST_USER = '0x7c3bd1a09d7d86920451def20ae503322c8d0412'
 
@@ -99,7 +97,7 @@ describe('Given the Stargate plugin', () => {
       const filter = await bridge({
         sourceChainId: ETH_CHAIN_ID,
         destinationChainId: ARBITRUM_CHAIN_ID,
-        tokenAddress: ETHEREUM_SGETH_ADDRESS,
+        tokenAddress: NATIVE_TOKEN_ADDRESS,
         amount: GreaterThanOrEqual(100000n),
         recipient: TEST_USER,
       })
@@ -123,7 +121,7 @@ describe('Given the Stargate plugin', () => {
       const filter = await bridge({
         sourceChainId: ETH_CHAIN_ID,
         destinationChainId: ARBITRUM_CHAIN_ID,
-        tokenAddress: ETHEREUM_SGETH_ADDRESS,
+        tokenAddress: NATIVE_TOKEN_ADDRESS,
         amount: GreaterThanOrEqual(parseEther('.04')),
         recipient: '0x7c3bd1a09d7d86920451def20ae503322c8d0412',
       })
@@ -134,7 +132,7 @@ describe('Given the Stargate plugin', () => {
       const filter = await bridge({
         sourceChainId: ARBITRUM_CHAIN_ID,
         destinationChainId: OPTIMISM_CHAIN_ID,
-        tokenAddress: ARBITRUM_SGETH_ADDRESS,
+        tokenAddress: NATIVE_TOKEN_ADDRESS,
         amount: GreaterThanOrEqual(parseEther('.6')),
         recipient: '0x08bfa4ef61a457792c45240829529b43b019d941',
       })
@@ -180,7 +178,7 @@ describe('Given the Stargate plugin', () => {
       const filter = await bridge({
         sourceChainId: OPTIMISM_CHAIN_ID,
         destinationChainId: ARBITRUM_LAYER_ZERO_CHAIN_ID,
-        tokenAddress: OP_SGETH_ADDRESS,
+        tokenAddress: NATIVE_TOKEN_ADDRESS,
         amount: GreaterThanOrEqual('1000000'),
       })
       expect(apply(transaction, filter)).to.be.false
