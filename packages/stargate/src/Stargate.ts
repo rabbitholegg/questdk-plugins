54e9970c89cb6850191f9e080b69f67aa3c5730c
Mmackz <matthewmackenz@gmail.com>
2023-10-15 19:29:14 -0700

    fix(stargate): throw if tokenAddress is undefined

--- packages/stargate/src/Stargate.ts
+++ packages/stargate/src/Stargate.ts
@@ -34,11 +34,11 @@ export const bridge = async (bridge: BridgeActionParams) => {
     tokenAddress === NATIVE_TOKEN_ADDRESS
       ? 13
       : NATIVE_CHAIN_AND_POOL_TO_TOKEN_ADDRESS[sourceChainId][
-          tokenAddress.toLowerCase()
+          tokenAddress?.toLowerCase()
         ]
 
   if (typeof sourcePool === 'undefined') {
-    throw new Error(`No pool found for provided tokenAddress: ${tokenAddress}.`)
+    throw new Error(`No pool found for provided tokenAddress: ${tokenAddress}`)
   }
 
   if (sourcePool === 13) {
