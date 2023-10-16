f092493db312ef3fe0503a92fe77a2a3a3394725
Mmackz <matthewmackenz@gmail.com>
2023-10-15 19:07:46 -0700

    fix(stargate): handle undefined sourcePool with error throw

--- packages/stargate/src/Stargate.ts
+++ packages/stargate/src/Stargate.ts
@@ -37,12 +37,16 @@ export const bridge = async (bridge: BridgeActionParams) => {
           tokenAddress.toLowerCase()
         ]
 
+  if (typeof sourcePool === 'undefined') {
+    throw new Error(`No pool found for provided tokenAddress: ${tokenAddress}.`)
+  }
+
   if (sourcePool === 13) {
     const targetContractAddress =
       CHAIN_ID_TO_ETH_ROUTER_ADDRESS[
         LAYER_ONE_TO_LAYER_ZERO_CHAIN_ID[sourceChainId]
       ]
-      
+
     return compressJson({
       chainId: sourceChainId, // The chainId of the source chain
       to: contractAddress || targetContractAddress, // The contract address of the bridge
