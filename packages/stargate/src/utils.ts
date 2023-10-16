f12b8096cac6774c30c0de3564a6ac32b2de450d
Mmackz <matthewmackenz@gmail.com>
2023-10-15 21:09:37 -0700

    feat(stargate): add utility functions

---
+++ packages/stargate/src/utils.ts
@@ -0,0 +1,22 @@
+import { getSupportedChainIds, getSupportedTokenAddresses } from "./Stargate";
+
+/**
+ * Grabs chain IDs from chains that have supported tokens based on the output
+ * of `getSupportedTokenAddresses`.
+ *
+ * @returns A promise that resolves to an array of filtered chain IDs (numbers).
+ */
+export async function getFilteredChainIds(): Promise<number[]> {
+   const chainIds = await getSupportedChainIds();
+   const filteredChainIds = await Promise.all(chainIds.map(async (chainId) => {
+     const tokens = await getSupportedTokenAddresses(chainId);
+     return tokens.length !== 0 ? chainId : null;
+   }));
+   return filteredChainIds.filter(id => id !== null) as number[];
+}
+
+export function shortenAddress(address): string {
+   const prefix = address.slice(0, 6);
+   const suffix = address.slice(-4); 
+   return `${prefix}...${suffix}`;
+ }
