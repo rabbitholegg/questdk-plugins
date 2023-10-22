605d54856140765d67e2bdad1ecf0a2502f2e474
Mmackz <matthewmackenz@gmail.com>
2023-10-22 13:39:25 -0700

    feat(symbiosis): add chain id constants

---
+++ packages/symbiosis/src/chain-ids.ts
@@ -0,0 +1,32 @@
+export const ETH_CHAIN_ID = 1
+export const OPTIMISM_CHAIN_ID = 10
+export const POLYGON_CHAIN_ID = 137
+export const ZK_ERA_CHAIN_ID = 324
+export const MANTLE_CHAIN_ID = 5000
+export const BASE_CHAIN_ID = 8453
+export const ARBITRUM_CHAIN_ID = 42161
+export const LINEA_CHAIN_ID = 59144
+export const SCROLL_CHAIN_ID = 534352
+
+export const CHAIN_ID_ARRAY = [
+  ETH_CHAIN_ID,
+  OPTIMISM_CHAIN_ID,
+  POLYGON_CHAIN_ID,
+  ZK_ERA_CHAIN_ID,
+  MANTLE_CHAIN_ID,
+  BASE_CHAIN_ID,
+  ARBITRUM_CHAIN_ID,
+  LINEA_CHAIN_ID,
+  SCROLL_CHAIN_ID,
+]
+
+export type ChainIds =
+  | typeof ETH_CHAIN_ID
+  | typeof OPTIMISM_CHAIN_ID
+  | typeof POLYGON_CHAIN_ID
+  | typeof ZK_ERA_CHAIN_ID
+  | typeof MANTLE_CHAIN_ID
+  | typeof BASE_CHAIN_ID
+  | typeof ARBITRUM_CHAIN_ID
+  | typeof LINEA_CHAIN_ID
+  | typeof SCROLL_CHAIN_ID
