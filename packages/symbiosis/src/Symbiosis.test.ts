d30b44e52459bffe29b650e5b80a5da8f1aa864f
Mmackz <matthewmackenz@gmail.com>
2023-10-23 21:57:53 -0700

    test(symbiosis): add tests for fail conditions

--- packages/symbiosis/src/Symbiosis.test.ts
+++ packages/symbiosis/src/Symbiosis.test.ts
@@ -1,7 +1,10 @@
 import { GreaterThanOrEqual, apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
 import { bridge } from './Symbiosis'
-import { PASSING_TEST_TRANSACTIONS } from './test-transactions'
+import {
+  PASSING_TEST_TRANSACTIONS,
+  FAILING_TEST_TRANSACTIONS,
+} from './test-transactions'
 import { metaBurnABI, metaRouteABI, metaSynthesizeABI } from './abi'
 import { ETH_CHAIN_ID, OPTIMISM_CHAIN_ID } from './constants'
 import { symbiosis } from './symbiosis-sdk'
@@ -66,6 +69,41 @@ describe('Given the symbiosis plugin', () => {
           recipient,
         })
         expect(apply(transaction, filter)).to.be.true
+      })
+    })
+  })
+  describe('should not pass filter with invalid parameters', () => {
+    test('when sourceChainId is incorrect', async () => {
+      const { transaction, tokenAddress, recipient } =
+        FAILING_TEST_TRANSACTIONS[0]
+
+      const filter = await bridge({
+        sourceChainId: 1, // 42161
+        destinationChainId: 5000,
+        tokenAddress,
+        amount: GreaterThanOrEqual(10000n),
+        recipient,
+      })
+      expect(apply(transaction, filter)).to.be.false
+    })
+    FAILING_TEST_TRANSACTIONS.forEach((testTransaction) => {
+      test(testTransaction.description, async () => {
+        const {
+          transaction,
+          destinationChainId,
+          tokenAddress,
+          amount,
+          recipient,
+        } = testTransaction
+
+        const filter = await bridge({
+          sourceChainId: transaction.chainId,
+          destinationChainId,
+          tokenAddress,
+          amount: GreaterThanOrEqual(amount),
+          recipient,
+        })
+        expect(apply(transaction, filter)).to.be.false
       })
     })
   })
