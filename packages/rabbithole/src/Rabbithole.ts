24e7274e519465c514be1401bf77230c33b81143
Quazia <alf40k@gmail.com>
2023-10-31 12:10:46 -0400

    Feat(rabbithole): implement quest

--- packages/rabbithole/src/Rabbithole.ts
+++ packages/rabbithole/src/Rabbithole.ts
@@ -1,32 +1,44 @@
 
 import { type TransactionFilter, type QuestActionParams, compressJson } from '@rabbitholegg/questdk'
-import { type Address } from 'viem'
-
+import { RABBITHOLE_ABI } from './abi'
+import {DEFAULT_SWAP_TOKEN_LIST, RABBITHOLE_QUEST_FACTORY} from './contract-addresses'
+import { CHAIN_ID_ARRAY } from './chain-ids'
 export const quest = async (quest: QuestActionParams): Promise<TransactionFilter> => {
   // This is the information we'll use to compose the Transaction object
   const {
+    chainId,
+    contractAddress,
     rewardTokenId,
     rewardAmount,
     startTime,
     endTime,
     totalParticipants,
-    actionSpec,
+    actionSpec, // Currently unused
   } = quest
 
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    chainId: chainId, // The chainId of the source chain
+  to:  contractAddress || RABBITHOLE_QUEST_FACTORY,   // The contract address of the bridge
+    input: {
+      $abi: RABBITHOLE_ABI,
+      rewardTokenAddress_: rewardTokenId,
+      endTime_: endTime,
+      startTime_: startTime,
+      totalParticipants_: totalParticipants,
+      rewardAmount_: rewardAmount,
+    },  // The input object is where we'll put the ABI and the parameters
   })
 }
 
 export const getSupportedTokenAddresses = async (_chainId: number) => {
   // Given a specific chain we would expect this function to return a list of supported token addresses
+  return DEFAULT_SWAP_TOKEN_LIST[_chainId]
 }
 
 
 export const getSupportedChainIds = async () => {
   // This should return all of the ChainIds that are supported by the Project we're integrating
+  return CHAIN_ID_ARRAY
 
 }
