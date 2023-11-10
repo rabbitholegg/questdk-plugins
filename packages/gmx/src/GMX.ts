6095c7f2110876e6208a645458843d3b9d1b88e6
Mmackz <matthewmackenz@gmail.com>
2023-11-09 22:00:08 -0800

    feat(gmx): apply marketToken to filter (WIP)

--- packages/gmx/src/GMX.ts
+++ packages/gmx/src/GMX.ts
@@ -1,21 +1,31 @@
 import { type SwapActionParams, compressJson } from '@rabbitholegg/questdk'
-import { getAddress } from 'viem'
-import { OrderType } from './utils.js'
+import { getAddress, type Address } from 'viem'
+import { OrderType, Tokens } from './utils.js'
 import { ARB_ONE_CHAIN_ID, CHAIN_ID_ARRAY } from './chain-ids.js'
 import { GMX_SWAPV1_ABI, GMX_SWAPV2_ABI } from './abi.js'
-
 import {
   DEFAULT_TOKEN_LIST_URL,
   GMX_ROUTERV1_ADDRESS,
   GMX_ROUTERV2_ADDRESS,
   ETH_ADDRESS,
   WETH_ADDRESS,
+  MARKET_TOKENS,
 } from './contract-addresses.js'
 
 export const swap = async (swap: SwapActionParams) => {
   const { chainId, tokenIn, tokenOut, amountIn, amountOut, recipient } = swap
 
   const ETH_USED = tokenIn === ETH_ADDRESS
+  const USDC_OUT = tokenOut === Tokens.USDC
+
+  const marketToken =
+    MARKET_TOKENS[
+      ETH_USED
+        ? WETH_ADDRESS
+        : USDC_OUT
+        ? (tokenIn as Address)
+        : (tokenOut as Address)
+    ]
 
   return compressJson({
     chainId: chainId,
@@ -44,7 +54,7 @@ export const swap = async (swap: SwapActionParams) => {
                 addresses: {
                   initialCollateralToken: ETH_USED ? WETH_ADDRESS : tokenIn,
                   receiver: recipient,
-                  // swapPath: [ETH_USED ? WETH_ADDRESS : tokenIn, tokenOut],
+                  swapPath: { $last: marketToken },
                 },
                 shouldUnwrapNativeToken: tokenOut === ETH_ADDRESS,
               },
