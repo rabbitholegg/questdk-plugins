0dc9ff3f5d49d2493d0e942f45e72bc2fa85fc59
Mmackz <matthewmackenz@gmail.com>
2023-11-09 17:50:11 -0800

    feat(gmx): map tokens to markettokens

--- packages/gmx/src/utils.ts
+++ packages/gmx/src/utils.ts
@@ -12,6 +12,16 @@ export enum OrderType {
   Liquidation = 7,
 }
 
+export enum Tokens {
+  DAI = '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
+  LINK = '0xf97f4df75117a78c1A5a0DBb814Af92458539FB4',
+  UNI = '0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0',
+  USDC = '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+  USDCe = '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8',
+  USDT = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+  WBTC = '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f',
+}
+
 interface Transaction {
   chainId: number
   from: Address
