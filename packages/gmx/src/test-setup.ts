fb473a8653dde3da83ff6b69066b3371d22d947d
Mmackz <matthewmackenz@gmail.com>
2023-11-10 15:03:27 -0800

    test(gmx): add additional tests for WETH

--- packages/gmx/src/test-setup.ts
+++ packages/gmx/src/test-setup.ts
@@ -13,6 +13,7 @@ import {
   ETH_FOR_USDC_V2,
   USDC_FOR_ETH_V2,
   USDC_FOR_WETH_V2,
+  USDCe_FOR_WETH_V2,
   REDUCE_ORDER_V2,
   ALT_ETHOUT_V2,
 } from './test-transactions'
@@ -55,6 +56,7 @@ export const passingTestCasesV2 = [
   createTestCase(ETH_FOR_USDC_V2, 'when swapping ETH to USDC'),
   createTestCase(USDC_FOR_ETH_V2, 'when swapping USDC to ETH'),
   createTestCase(USDC_FOR_WETH_V2, 'when swapping USDC to WETH'),
+  createTestCase(USDCe_FOR_WETH_V2, 'when swapping USDC.e to WETH'),
   createTestCase(ALT_ETHOUT_V2, 'when swapping USDC.e to ETH'),
   createTestCase(SWAP_TOKENS_V2, 'when all parameters are correct'),
 ]
