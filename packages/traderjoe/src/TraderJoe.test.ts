94b916a4dbca11c93bab31972d71cf9bf3bfe5e6
Mmackz <matthewmackenz@gmail.com>
2023-11-14 14:55:39 -0800

    test(traderjoe): test for valid token list

--- packages/traderjoe/src/TraderJoe.test.ts
+++ packages/traderjoe/src/TraderJoe.test.ts
@@ -1,11 +1,12 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
-import { swap } from './TraderJoe'
+import { swap, getSupportedTokenAddresses } from './TraderJoe'
+import { CHAIN_ID_ARRAY } from './chain-ids'
 import { passingTestCases, failingTestCases } from './test-setup'
 
 describe('Given the TraderJoe plugin', () => {
   describe('When handling the swap action', () => {
-    describe('should return a valid action filter', () => {})
+    // describe('should return a valid action filter', () => {})
 
     describe('should pass filter with valid transactions', () => {
       passingTestCases.forEach((testCase) => {
@@ -23,6 +24,15 @@ describe('Given the TraderJoe plugin', () => {
         test(description, async () => {
           const filter = await swap(params)
           expect(apply(transaction, filter)).to.be.false
+        })
+      })
+    })
+
+    describe('should return a valid token list', () => {
+      CHAIN_ID_ARRAY.forEach((chainId) => {
+        test(`for chainId ${chainId}`, async () => {
+          const tokenList = await getSupportedTokenAddresses(chainId)
+          expect(tokenList).to.not.be.empty
         })
       })
     })
