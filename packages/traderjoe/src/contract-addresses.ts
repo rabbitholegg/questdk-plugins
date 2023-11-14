dbcebbeac5ecccb58cf13a3b331b48e3eb94935f
Mmackz <matthewmackenz@gmail.com>
2023-11-14 14:56:07 -0800

    feat(traderjoe): setup default token list for arbitrum

--- packages/traderjoe/src/contract-addresses.ts
+++ packages/traderjoe/src/contract-addresses.ts
@@ -1,1 +1,26 @@
+import { type Address } from "viem"
+import { ARB_CHAIN_ID } from "./chain-ids"
+import { Tokens } from "./utils"
+
 export const LB_ROUTER = '0xb4315e873dbcf96ffd0acd8ea43f689d8c20fb30'
+
+export const DEFAULT_SWAP_TOKEN_LIST: {
+  [chainId: number]: readonly Address[]
+} = {
+  [ARB_CHAIN_ID]: [
+    Tokens.ETH,
+    Tokens.ARB,
+    Tokens.DAI,
+    Tokens.GMX,
+    Tokens.JOE,
+    Tokens.LINK,
+    Tokens.MAGIC,
+    Tokens.RDNT,
+    Tokens.STG,
+    Tokens.USDC,
+    Tokens.USDCE,
+    Tokens.USDT,
+    Tokens.WBTC,
+    Tokens.WETH,
+  ],
+} as const
