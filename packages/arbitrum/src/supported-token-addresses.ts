7ddeca980f50dd125d6c1bf9c6ca3c5b17429a07
Mmackz <matthewmackenz@gmail.com>
2023-11-18 12:54:22 -0800

    feat(arbitrum): add support for ARB token

--- packages/arbitrum/src/supported-token-addresses.ts
+++ packages/arbitrum/src/supported-token-addresses.ts
@@ -2,15 +2,16 @@ import { type Address, zeroAddress as ETH_ADDRESS } from 'viem'
 import { ETH_CHAIN_ID, ARB_ONE_CHAIN_ID, ARB_NOVA_CHAIN_ID } from './chain-ids'
 
 // For now we're mainly supported the highest traffic Arbitrum tokens
-// Support for USDC seems like it might be non-trivial
-// https://docs.arbitrum.io/bridge-tokens/concepts/usdc-concept
-
 // excluded USDT due to it not being compatible with arbitrum nova
+
+// -------------!!!!! IMPORTANT!!!!!----------------
+// WHEN ADDING NEW TOKENS, MAKE SURE TO UPDATE l1ToL2Map in utils.ts
 export const ArbitrumTokens: Record<number, Address[]> = {
   [ETH_CHAIN_ID]: [
     ETH_ADDRESS,
     '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', // USDC
     '0x6B175474E89094C44Da98b954EedeAC495271d0F', // DAI
+    '0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1', // ARB
     '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', // WBTC
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // WETH
   ],
@@ -18,6 +19,7 @@ export const ArbitrumTokens: Record<number, Address[]> = {
     ETH_ADDRESS,
     '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', // USDC
     '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1', // DAI
+    '0x912CE59144191C1204E64559FE8253a0e49E6548', // ARB
     '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f', // WBTC
     '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // WETH
   ],
@@ -25,6 +27,7 @@ export const ArbitrumTokens: Record<number, Address[]> = {
     ETH_ADDRESS,
     '0x750ba8b76187092B0D1E87E28daaf484d1b5273b', // USDC
     '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1', // DAI
+    '0xf823C3cD3CeBE0a1fA952ba88Dc9EEf8e0Bf46AD', // ARB
     '0x1d05e4e72cD994cdF976181CfB0707345763564d', // WBTC
     '0x722E8BdD2ce80A4422E880164f2079488e115365', // WETH
   ],
--- packages/arbitrum/src/utils.ts
+++ packages/arbitrum/src/utils.ts
@@ -11,31 +11,39 @@ const l1ToL2Map: [string, Address][] = [
   [
     // USDC
     JSON.stringify([
-      '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8',
-      '0x750ba8b76187092b0d1e87e28daaf484d1b5273b',
+      '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8', // one
+      '0x750ba8b76187092b0d1e87e28daaf484d1b5273b', // nova
     ]),
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // ethereum
+  ],
+  [
+    // ARB
+    JSON.stringify([
+      '0x912CE59144191C1204E64559FE8253a0e49E6548', // one
+      '0xf823C3cD3CeBE0a1fA952ba88Dc9EEf8e0Bf46AD', // nova
+    ]),
+    '0xB50721BCf8d664c30412Cfbc6cf7a15145234ad1', // ethereum
   ],
   [
     // WBTC
     JSON.stringify([
-      '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f',
-      '0x1d05e4e72cd994cdf976181cfb0707345763564d',
+      '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', // one
+      '0x1d05e4e72cd994cdf976181cfb0707345763564d', // nova
     ]),
-    '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+    '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // ethereum
   ],
   [
     // WETH
     JSON.stringify([
-      '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
-      '0x722e8bdd2ce80a4422e880164f2079488e115365',
+      '0x82af49447d8a07e3bd95bd0d56f35241523fbab1', // one
+      '0x722e8bdd2ce80a4422e880164f2079488e115365', // nova
     ]),
-    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // ethereum
   ],
   [
     // DAI
-    JSON.stringify(['0xda10009cbd5d07dd0cecc66161fc93d7c9000da1']),
-    '0x6b175474e89094c44da98b954eedeac495271d0f',
+    JSON.stringify(['0xda10009cbd5d07dd0cecc66161fc93d7c9000da1']), // one/nova
+    '0x6b175474e89094c44da98b954eedeac495271d0f', // ethereum
   ],
 ]
 
