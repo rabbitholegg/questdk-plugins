c126210ed348dd4d41d0ec7fcd3fd6643a0881b0
Mmackz <matthewmackenz@gmail.com>
2023-11-18 00:59:02 -0800

    feat(arbitrum): add additional function signature

--- packages/arbitrum/src/abi.ts
+++ packages/arbitrum/src/abi.ts
@@ -2,6 +2,18 @@
 export const GATEWAY_OUTBOUND_TRANSFER_FRAG = [
   {
     inputs: [
+      { internalType: 'address', name: '_l1Token', type: 'address' },
+      { internalType: 'address', name: '_to', type: 'address' },
+      { internalType: 'uint256', name: '_amount', type: 'uint256' },
+      { internalType: 'bytes', name: '_data', type: 'bytes' },
+    ],
+    name: 'outboundTransfer',
+    outputs: [{ internalType: 'bytes', name: '', type: 'bytes' }],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
       { internalType: 'address', name: '_token', type: 'address' },
       { internalType: 'address', name: '_to', type: 'address' },
       { internalType: 'uint256', name: '_amount', type: 'uint256' },
