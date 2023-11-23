bdcb51b83cb283876fbabc30410395f44eb28bfe
Mmackz <matthewmackenz@gmail.com>
2023-11-23 12:52:51 -0800

    feat(okutrade): setup from template and connect to registry

---
+++ packages/okutrade/src/OkuTrade.test.ts
@@ -0,0 +1,17 @@
+import { GreaterThanOrEqual, apply } from '@rabbitholegg/questdk/filter'
+import { describe, expect, test } from 'vitest'
+
+// Replace *project* with the name of the project
+describe('Given the OkuTrade plugin', () => {
+  describe('When handling the swap action', () => {
+    describe('should return a valid action filter', () => {
+      test('with a valid swap transaction', () => {})
+    })
+
+    describe('should pass filter with valid swap transactions', () => {})
+
+    describe('should not pass filter with invalid transactions', () => {})
+
+    describe('a valid tokenlist should be returned for each supported network', () => {})
+  })
+})
