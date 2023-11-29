2f071d529c1891509eca0818918b50e681530ced
Mmackz <matthewmackenz@gmail.com>
2023-11-28 20:59:54 -0800

    feat(balancer): implement swap filter

--- packages/balancer/src/Balancer.ts
+++ packages/balancer/src/Balancer.ts
@@ -5,24 +5,8 @@ import {
 } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 import { BALANCER_ABI } from './abi'
-import { FilterOperator } from '@rabbitholegg/questdk'
-
-export const buildPathQuery = (In?: string, Out?: string) => {
-  // v2 paths are formatted as [<token>, <token>]
-  const conditions: FilterOperator[] = []
-
-  if (In) {
-    conditions.push({ $first: In })
-  }
-
-  if (Out) {
-    conditions.push({ $last: Out })
-  }
-
-  return {
-    $and: conditions,
-  }
-}
+import { VAULT_CONTRACT } from './constants'
+import { buildAmountQuery, buildPathQuery } from './utils'
 
 export const swap = async (
   swap: SwapActionParams,
@@ -37,37 +21,42 @@ export const swap = async (
     recipient,
   } = swap
 
-  // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
-    chainId: chainId, // The chainId of the source chain
-    to: 0x0, // The contract address of the bridge
+    chainId: chainId,
+    to: contractAddress ?? VAULT_CONTRACT,
     input: {
       $abiAbstract: BALANCER_ABI,
       $or: [
         {
+          // swap
           singleSwap: {
             assetIn: tokenIn,
             assetOut: tokenOut,
             amount: amountIn,
           },
-          recipient: recipient,
+          funds: {
+            recipient: recipient,
+          },
         },
         {
+          // batch swap
           assets: buildPathQuery(tokenIn, tokenOut),
-          recipient: recipient,
-          limits: buildPathQuery(amountIn, amountOut),
+          limits: buildAmountQuery(amountIn, amountOut),
+          funds: {
+            recipient: recipient,
+          },
         },
       ],
-    }, // The input object is where we'll put the ABI and the parameters
+    },
   })
 }
 
 export const getSupportedTokenAddresses = async (
   _chainId: number,
 ): Promise<Address[]> => {
-  // Given a specific chain we would expect this function to return a list of supported token addresses
+  return []
 }
 
 export const getSupportedChainIds = async (): Promise<number[]> => {
-  // This should return all of the ChainIds that are supported by the Project we're integrating
+  return []
 }
