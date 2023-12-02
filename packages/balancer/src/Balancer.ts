4183f506de934d5877362a7bf45748afb919533c
Mmackz <matthewmackenz@gmail.com>
2023-12-01 22:27:13 -0800

    feat(balancer): add patch contract for AA testing

--- packages/balancer/src/Balancer.ts
+++ packages/balancer/src/Balancer.ts
@@ -5,7 +5,7 @@ import {
 } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 import { BALANCER_ABI } from './abi'
-import { CHAIN_ID_ARRAY, VAULT_CONTRACT } from './constants'
+import { CHAIN_ID_ARRAY, VAULT_CONTRACT, PATCH_CONTRACT } from './constants'
 import { buildAmountQuery, buildPathQuery } from './utils'
 import { CHAIN_TO_TOKENS } from './token-addresses'
 
@@ -24,7 +24,9 @@ export const swap = async (
 
   return compressJson({
     chainId: chainId,
-    to: contractAddress ?? VAULT_CONTRACT,
+    to: contractAddress ?? {
+      $or: [VAULT_CONTRACT.toLowerCase(), PATCH_CONTRACT.toLowerCase()],
+    },
     input: {
       $abiAbstract: BALANCER_ABI,
       $or: [
