00286f35f9297787852a1f4d7dd1716a74863248
Mmackz <matthewmackenz@gmail.com>
2023-12-01 22:13:02 -0800

    feat(balancer): add patch contract

--- packages/balancer/src/constants.ts
+++ packages/balancer/src/constants.ts
@@ -1,6 +1,7 @@
 import { Chains } from './utils'
 
 export const VAULT_CONTRACT = '0xba12222222228d8ba445958a75a0704d566bf2c8'
+export const PATCH_CONTRACT = '0x5ff137d4b0fdcd49dca30c7cf57e578a026d2789'
 
 export const CHAIN_ID_ARRAY = [
   Chains.ETHEREUM,
