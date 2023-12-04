8cc4c6dbc5ec0b1da0bae04757983d5c36164c37
Quazia <alf40k@gmail.com>
2023-12-04 18:30:01 -0500

    Test(zora): add filter test

--- packages/zora/src/Zora.test.ts
+++ packages/zora/src/Zora.test.ts
@@ -1,13 +1,35 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
 import { failingTestCases, passingTestCases } from './test-setup'
+import { BASIC_PURCHASE } from './test-transactions'
 import { mint } from './Zora'
+import { ZORA_MINTER_ABI } from './abi'
 
 describe('Given the zora plugin', () => {
   describe('When handling the mint', () => {
 
-    test('should return a valid action filter', () => {
-      
+    test('should return a valid action filter', async () => {
+      const { params } = BASIC_PURCHASE
+      const filter = await mint(params)
+      expect(filter).to.deep.equal({
+        chainId: 10,
+        to: '0xfff631ef40557f8705e89053af794a1dcfa0a90b',
+        input: {
+          $abi: ZORA_MINTER_ABI,
+          $and: [
+            {
+              $or: [
+                {
+                  recipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                },
+                {
+                  tokenRecipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                },
+              ],
+            }
+          ],
+        },
+    })
     })
 
     describe('should pass filter with valid transactions', () => {
