62d4d9812c249d7638b7633528af02e3c9a6ed14
Mmackz <matthewmackenz@gmail.com>
2023-12-06 18:43:50 -0800

    chore(pnpm): format

--- packages/woofi/src/WooFi.test.ts
+++ packages/woofi/src/WooFi.test.ts
@@ -4,16 +4,11 @@ import { describe, expect, test } from 'vitest'
 // Replace *project* with the name of the project
 describe('Given the WooFi plugin', () => {
   describe('When handling the swap action', () => {
-
-    describe('should return a valid action filter', () => {
-      
-    })
+    describe('should return a valid action filter', () => {})
 
     describe('should pass filter with valid transactions', () => {})
-    
-    describe('should not pass filter with invalid transactions',  () => {
-      
-    })
+
+    describe('should not pass filter with invalid transactions', () => {})
 
     // describe('should return a valid token list', () => {
     //   CHAIN_ID_ARRAY.forEach((chainId) => {
@@ -23,6 +18,5 @@ describe('Given the WooFi plugin', () => {
     //     })
     //   })
     // })
-
   })
 })
