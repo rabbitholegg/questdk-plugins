ce18db29fc37e542740ea1d2932b6532c10208dd
Mmackz <matthewmackenz@gmail.com>
2023-12-06 22:26:40 -0800

    feat(woofi): export chainId array

---
+++ packages/woofi/src/chain-ids.ts
@@ -0,0 +1,11 @@
+import { Chains } from "./utils";
+
+export const CHAIN_ID_ARRAY = [
+  Chains.ETHEREUM,
+  Chains.OPTIMISM,
+  Chains.POLYGON_POS,
+  Chains.ZKSYNC_ERA,
+  Chains.BASE,
+  Chains.ARBITRUM_ONE,
+  Chains.AVALANCHE,
+]
