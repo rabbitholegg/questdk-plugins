2bce76da06974773120bac19e3f2985184cf59dd
Mmackz <matthewmackenz@gmail.com>
2023-12-12 19:30:48 -0800

    feat(vela): add ABI for vault contract

---
+++ packages/vela/src/abi.ts
@@ -0,0 +1,42 @@
+export const VAULT_ABI = [
+  {
+    inputs: [
+      { internalType: 'uint256', name: '_tokenId', type: 'uint256' },
+      { internalType: 'bool', name: '_isLong', type: 'bool' },
+      { internalType: 'enum OrderType', name: '_orderType', type: 'uint8' },
+      { internalType: 'uint256[]', name: '_params', type: 'uint256[]' },
+      { internalType: 'address', name: '_refer', type: 'address' },
+    ],
+    name: 'newPositionOrder',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'uint256', name: 'a', type: 'uint256' },
+      { internalType: 'uint256', name: 'b', type: 'uint256' },
+      { internalType: 'uint256', name: 'c', type: 'uint256' },
+    ],
+    name: 'newPositionOrderPacked',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      { internalType: 'uint256', name: '_tokenId', type: 'uint256' },
+      { internalType: 'bool', name: '_isLong', type: 'bool' },
+      { internalType: 'enum OrderType', name: '_orderType', type: 'uint8' },
+      { internalType: 'uint256[]', name: '_params', type: 'uint256[]' },
+      { internalType: 'address', name: '_refer', type: 'address' },
+      { internalType: 'bool[]', name: '_isTPs', type: 'bool[]' },
+      { internalType: 'uint256[]', name: '_prices', type: 'uint256[]' },
+      { internalType: 'uint256[]', name: '_amountPercents', type: 'uint256[]' },
+    ],
+    name: 'newPositionOrderWithTPSL',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
