49d3e7a2bffbd3d9c5cf0d6dda3152567e2e2d4c
Mmackz <matthewmackenz@gmail.com>
2023-12-12 22:20:04 -0800

    feat(vela): use ranges to determine tokenId from packed data

--- packages/vela/src/contract-addresses.ts
+++ packages/vela/src/contract-addresses.ts
@@ -1,21 +1,53 @@
 import { type Address, zeroAddress } from 'viem'
 import { Chains } from './utils'
+import {
+  GreaterThanOrEqual,
+  type FilterOperator,
+  LessThan,
+} from '@rabbitholegg/questdk'
 
 // Tokens
 const ETHER = zeroAddress
-const ARBITRUM_BTC = '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f'
-const ARBITRUM_ARB = '0x912CE59144191C1204E64559FE8253a0e49E6548'
-const ARBITRUM_CRV = '0x11cDb42B0EB46D95f990BeDD4695A6e3fA034978'
-const ARBITRUM_LINK = '0xf97f4df75117a78c1A5a0DBb814Af92458539FB4'
+const ARBITRUM_BTC = '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f'
+const ARBITRUM_ARB = '0x912ce59144191c1204e64559fe8253a0e49e6548'
+const ARBITRUM_CRV = '0x11cdb42b0eb46d95f990bedd4695a6e3fa034978'
+const ARBITRUM_LINK = '0xf97f4df75117a78c1a5a0dbb814af92458539fb4'
 
 export const VAULT_CONTRACT = '0xc4abade3a15064f9e3596943c699032748b13352'
 
-export const TOKEN_TO_ID: { [token: string]: number } = {
-  ETHER: 2, // ETH
-  ARBITRUM_BTC: 1, // wBTC
-  ARBITRUM_ARB: 7, // ARB
-  ARBITRUM_CRV: 36, // CRV
-  ARBITRUM_LINK: 39, // LINK
+const tokenIdMultiplier = BigInt(2 ** 240)
+
+export const TOKEN_TO_ID: { [token: string]: FilterOperator } = {
+  [ETHER]: {
+    $and: [
+      GreaterThanOrEqual(2n * tokenIdMultiplier),
+      LessThan(3n * tokenIdMultiplier),
+    ],
+  },
+  [ARBITRUM_BTC]: {
+    $and: [
+      GreaterThanOrEqual(1n * tokenIdMultiplier),
+      LessThan(2n * tokenIdMultiplier),
+    ],
+  },
+  [ARBITRUM_ARB]: {
+    $and: [
+      GreaterThanOrEqual(7n * tokenIdMultiplier),
+      LessThan(8n * tokenIdMultiplier),
+    ],
+  },
+  [ARBITRUM_CRV]: {
+    $and: [
+      GreaterThanOrEqual(36n * tokenIdMultiplier),
+      LessThan(37n * tokenIdMultiplier),
+    ],
+  },
+  [ARBITRUM_LINK]: {
+    $and: [
+      GreaterThanOrEqual(39n * tokenIdMultiplier),
+      LessThan(40n * tokenIdMultiplier),
+    ],
+  },
 }
 
 export const CHAIN_TO_TOKENS: { [chainId: number]: Address[] } = {
