16b10a87b7cd519cdc26628d6598a84e53a6b189
Mmackz <matthewmackenz@gmail.com>
2023-12-13 21:02:17 -0800

    test(vela): test valid token list is returned for each supported chain

--- packages/vela/src/Vela.test.ts
+++ packages/vela/src/Vela.test.ts
@@ -1,8 +1,9 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
-import { swap } from './Vela'
-import { passingTestCases, failingTestCases } from './test-transactions'
+import { swap, getSupportedTokenAddresses } from './Vela'
+import { CHAIN_ID_ARRAY } from './chain-ids'
 import { VAULT_ABI } from './abi'
+import { passingTestCases, failingTestCases } from './test-transactions'
 
 describe('Given the vela plugin', () => {
   describe('When handling the swap action', () => {
@@ -50,6 +51,24 @@ describe('Given the vela plugin', () => {
         test(description, async () => {
           const filter = await swap(params)
           expect(apply(transaction, filter)).to.be.false
+        })
+      })
+    })
+
+    describe('should return a valid list of tokens for each supported chain', () => {
+      CHAIN_ID_ARRAY.forEach((chainId) => {
+        test(`for chainId: ${chainId}`, async () => {
+          const tokens = await getSupportedTokenAddresses(chainId)
+          const addressRegex = /^0x[a-fA-F0-9]{40}$/
+          expect(tokens).to.be.an('array')
+          expect(tokens).to.have.length.greaterThan(0)
+          expect(tokens).to.have.length.lessThan(100)
+          tokens.forEach((token) => {
+            expect(token).to.match(
+              addressRegex,
+              `Token address ${token} is not a valid Ethereum address`,
+            )
+          })
         })
       })
     })
