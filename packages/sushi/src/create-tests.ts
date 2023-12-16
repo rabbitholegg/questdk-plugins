58806c142ac8308a93d21edfab003b8e042ba221
Mmackz <matthewmackenz@gmail.com>
2023-12-15 21:54:22 -0800

    test(sushi): add tests for a;; swap conditions

---
+++ packages/sushi/src/create-tests.ts
@@ -0,0 +1,35 @@
+import { createTestCase } from './utils'
+import {
+  PROCESS_ROUTE_ETH_TOKEN,
+  PROCESS_ROUTE_TOKEN_ETH,
+  PROCESS_ROUTE_TOKEN_TOKEN,
+  ETH_FOR_EXACT_TOKENS,
+  EXACT_ETH_FOR_TOKENS,
+  EXACT_TOKENS_FOR_ETH,
+  EXACT_TOKENS_FOR_TOKENS,
+  TOKENS_FOR_EXACT_ETH,
+  TOKENS_FOR_EXACT_TOKENS,
+} from './test-transactions'
+import { GreaterThanOrEqual } from '@rabbitholegg/questdk'
+import { parseEther } from 'viem'
+
+export const passingTestCases = [
+  createTestCase(PROCESS_ROUTE_ETH_TOKEN, 'when swapping ETH to tokens using route processor'),
+  createTestCase(PROCESS_ROUTE_TOKEN_ETH, 'when swapping tokens to ETH using route processor'),
+  createTestCase(PROCESS_ROUTE_TOKEN_TOKEN, 'when swapping tokens to tokens using route processor'),
+  createTestCase(ETH_FOR_EXACT_TOKENS, 'when swapping ETH for exact tokens using V2 router'),
+  createTestCase(EXACT_ETH_FOR_TOKENS, 'when swapping exact ETH for tokens using V2 router'),
+  createTestCase(EXACT_TOKENS_FOR_ETH, 'when swapping exact tokens for ETH using V2 router'),
+  createTestCase(EXACT_TOKENS_FOR_TOKENS, 'when swapping exact tokens for tokens using V2 router'),
+  createTestCase(TOKENS_FOR_EXACT_ETH, 'when swapping tokens for exact ETH using V2 router'),
+  createTestCase(TOKENS_FOR_EXACT_TOKENS, 'when swapping tokens for exact tokens using V2 router'),
+]
+
+export const failingTestCases = [
+  createTestCase(PROCESS_ROUTE_ETH_TOKEN, 'when chianId is not correct', { chainId: 10 }),
+  createTestCase(PROCESS_ROUTE_TOKEN_ETH, 'when tokenIn is not correct', { tokenIn: '0x4200000000000000000000000000000000000006'}),
+  createTestCase(PROCESS_ROUTE_TOKEN_TOKEN, 'when tokenIn is not correct', { tokenOut: '0x4200000000000000000000000000000000000006'}),
+  createTestCase(ETH_FOR_EXACT_TOKENS, 'when amountIn is insufficient', { amountIn: GreaterThanOrEqual(parseEther('100000'))}),
+  createTestCase(EXACT_ETH_FOR_TOKENS, 'when amountOut is insufficient', { amountOut: GreaterThanOrEqual(parseEther('100000'))}),
+  createTestCase(EXACT_TOKENS_FOR_ETH, 'when recipient is not correct', { recipient: '0x96e0fd08cbcd2f8c9fa20557898464cb970c9a75'}),
+]
