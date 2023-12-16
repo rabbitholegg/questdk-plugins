0ef4b787b0b7156663f88f04673c23cd9a52f9f1
Mmackz <matthewmackenz@gmail.com>
2023-12-15 21:57:56 -0800

    test(sushi): add unit tests for both passing and failing conditions

--- packages/sushi/src/Sushi.test.ts
+++ packages/sushi/src/Sushi.test.ts
@@ -1,20 +1,30 @@
 import { GreaterThanOrEqual, apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { passingTestCases, failingTestCases } from './create-tests'
+import { swap } from './Sushi'
 
 describe('Given the Sushi plugin', () => {
   describe('When handling the swap action', () => {
+    describe('should return a valid action filter', () => {})
 
-    describe('should return a valid action filter', () => {
-      
+    describe('should pass filter with valid transactions', () => {
+      passingTestCases.forEach((testCase) => {
+        const { transaction, description, params } = testCase
+        test(description, async () => {
+          const filter = await swap(params)
+          expect(apply(transaction, filter)).to.be.true
+        })
+      })
     })
 
-    describe('should pass filter with valid transactions',  () => {
-      
+    describe('should not pass filter with invalid transactions', () => {
+      failingTestCases.forEach((testCase) => {
+        const { transaction, description, params } = testCase
+        test(description, async () => {
+          const filter = await swap(params)
+          expect(apply(transaction, filter)).to.be.false
+        })
+      })
     })
-    
-    describe('should not pass filter with invalid transactions',  () => {
-      
-    })
-
   })
 })
