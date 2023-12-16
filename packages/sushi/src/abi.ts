cb5203cdd3829fbe2c470837c6c0770fbd990398
Mmackz <matthewmackenz@gmail.com>
2023-12-16 12:53:58 -0800

    refactor(sushi): use array for process route abi

--- packages/sushi/src/abi.ts
+++ packages/sushi/src/abi.ts
@@ -1,17 +1,19 @@
-export const PROCESS_ROUTE_ABI = {
-  inputs: [
-    { internalType: 'address', name: 'tokenIn', type: 'address' },
-    { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
-    { internalType: 'address', name: 'tokenOut', type: 'address' },
-    { internalType: 'uint256', name: 'amountOutMin', type: 'uint256' },
-    { internalType: 'address', name: 'to', type: 'address' },
-    { internalType: 'bytes', name: 'route', type: 'bytes' },
-  ],
-  name: 'processRoute',
-  outputs: [{ internalType: 'uint256', name: 'amountOut', type: 'uint256' }],
-  stateMutability: 'payable',
-  type: 'function',
-}
+export const PROCESS_ROUTE_ABI = [
+  {
+    inputs: [
+      { internalType: 'address', name: 'tokenIn', type: 'address' },
+      { internalType: 'uint256', name: 'amountIn', type: 'uint256' },
+      { internalType: 'address', name: 'tokenOut', type: 'address' },
+      { internalType: 'uint256', name: 'amountOutMin', type: 'uint256' },
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: 'bytes', name: 'route', type: 'bytes' },
+    ],
+    name: 'processRoute',
+    outputs: [{ internalType: 'uint256', name: 'amountOut', type: 'uint256' }],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
 
 export const ROUTER_ABI = [
   {
