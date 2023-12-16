78a1617cb00864b00eb53f967fc0d23dfb65ea4a
Mmackz <matthewmackenz@gmail.com>
2023-12-15 23:02:38 -0800

    feat(sushi): add filter for legacy V2 swaps

--- packages/sushi/src/Sushi.ts
+++ packages/sushi/src/Sushi.ts
@@ -1,8 +1,17 @@
+import {
+  type TransactionFilter,
+  type SwapActionParams,
+  compressJson,
+} from '@rabbitholegg/questdk'
+import { type Address, zeroAddress as NATIVE_TOKEN } from 'viem'
+import { CHAIN_TO_CONTRACTS } from './contract-addresses'
+import { ROUTER_ABI } from './abi'
+import { WNATIVE_ADDRESS } from '@sushiswap/core-sdk'
+import { buildV2PathQuery } from './utils'
 
-import { type TransactionFilter, type SwapActionParams, compressJson } from '@rabbitholegg/questdk'
-import { type Address } from 'viem'
-
-export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> => {
+export const swap = async (
+  swap: SwapActionParams,
+): Promise<TransactionFilter> => {
   const {
     chainId,
     contractAddress,
@@ -13,17 +22,49 @@ export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> =
     recipient,
   } = swap
 
+  const swapContracts = CHAIN_TO_CONTRACTS[chainId]?.map((address) =>
+    address.toLowerCase(),
+  )
+
+  const to =
+    contractAddress ?? swapContracts ? { $or: swapContracts } : undefined
+
+  const nativeIn = tokenIn === NATIVE_TOKEN
+  const nativeOut = tokenOut === NATIVE_TOKEN
+
   return compressJson({
     chainId,
-    to:  0x0,   // Use lowercase if using array
-    input: {},
+    to,
+    value: nativeIn ? amountIn : undefined,
+    input: {
+      $abi: ROUTER_ABI,
+      $and: [
+        {
+          to: recipient,
+          path: buildV2PathQuery(
+            nativeIn ? WNATIVE_ADDRESS[chainId] : tokenIn,
+            nativeOut ? WNATIVE_ADDRESS[chainId] : tokenOut,
+          ),
+        },
+        {
+          $or: [
+            {
+              amountIn: nativeIn ? undefined : amountIn,
+              amountOutMin: amountOut,
+            },
+            { amountOut: amountOut },
+          ],
+        },
+      ],
+    },
   })
 }
 
-export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
+export const getSupportedTokenAddresses = async (
+  _chainId: number,
+): Promise<Address[]> => {
   return []
 }
-
 
 export const getSupportedChainIds = async (): Promise<number[]> => {
   return []
