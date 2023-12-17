81b810f1bae4a5a6d9eb33d67b2bee6a61a4f911
Mmackz <matthewmackenz@gmail.com>
2023-12-16 20:07:38 -0800

    feat(treasure): export array of token addresses

--- packages/treasure/src/constants.ts
+++ packages/treasure/src/constants.ts
@@ -1,8 +1,12 @@
+import { type Address } from 'viem'
+
 // Router Contract
 export const V2_ROUTER = '0x23805449f91bb2d2054d9ba288fdc8f09b5eac79'
 
 // Token Addresses
-export const MAGIC = '0x539bde0d7dbd336b79148aa742883198bbf60342'
-export const ANIMA = '0xccd05a0fcfc1380e9da27862adb2198e58e0d66f'
-export const ELM = '0xeeac5e75216571773c0064b3b591a86253791db6'
-export const GFLY = '0x872bad41cfc8ba731f811fea8b2d0b9fd6369585'
+const MAGIC = '0x539bde0d7dbd336b79148aa742883198bbf60342'
+const ANIMA = '0xccd05a0fcfc1380e9da27862adb2198e58e0d66f'
+const ELM = '0xeeac5e75216571773c0064b3b591a86253791db6'
+const GFLY = '0x872bad41cfc8ba731f811fea8b2d0b9fd6369585'
+
+export const MAGICSWAP_TOKENS: Address[] = [MAGIC, ANIMA, ELM, GFLY]
