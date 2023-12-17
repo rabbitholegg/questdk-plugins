a15769130f8408c8459dd965ee55231fcb26a1df
Mmackz <matthewmackenz@gmail.com>
2023-12-16 20:08:22 -0800

    test(treasure): add test for valid token addresses

--- packages/treasure/src/Treasure.test.ts
+++ packages/treasure/src/Treasure.test.ts
@@ -1,5 +1,7 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { Chains } from './utils'
+import { swap, getSupportedTokenAddresses } from './Treasure'
 
 describe('Given the tresure plugin', () => {
   describe('When handling the swap action', () => {
@@ -8,5 +10,21 @@ describe('Given the tresure plugin', () => {
     describe('should pass filter with valid transactions', () => {})
 
     describe('should not pass filter with invalid transactions', () => {})
+
+    describe('should return a valid list of tokens for each supported chain', () => {
+      test(`for chainId: ${Chains.ARBITRUM_ONE}`, async () => {
+        const tokens = await getSupportedTokenAddresses(Chains.ARBITRUM_ONE)
+        const addressRegex = /^0x[a-fA-F0-9]{40}$/
+        expect(tokens).to.be.an('array')
+        expect(tokens).to.have.length.greaterThan(0)
+        expect(tokens).to.have.length.lessThan(100)
+        tokens.forEach((token) => {
+          expect(token).to.match(
+            addressRegex,
+            `Token address ${token} is not a valid Ethereum address`,
+          )
+        })
+      })
+    })
   })
 })
