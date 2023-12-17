88f3ffeed8e9efb088d2dfc9f095d51b28b90b19
Mmackz <matthewmackenz@gmail.com>
2023-12-16 21:14:22 -0800

    test(treasure): test valid action filter is returned from swap function

--- packages/treasure/src/Treasure.test.ts
+++ packages/treasure/src/Treasure.test.ts
@@ -1,12 +1,62 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { getAddress } from 'viem'
 import { Chains } from './utils'
 import { swap, getSupportedTokenAddresses } from './Treasure'
 import { passingTestCases, failingTestCases } from './test-transactions'
+import { V2_ROUTER_ABI } from './abi'
+import { MAGIC, ANIMA, V2_ROUTER } from './constants'
 
 describe('Given the tresure plugin', () => {
   describe('When handling the swap action', () => {
-    describe('should return a valid action filter', () => {})
+    describe('should return a valid action filter', () => {
+      test('when making a valid swap', async () => {
+        const { params } = passingTestCases[0]
+        const filter = await swap(params)
+        expect(filter).to.deep.equal({
+          chainId: 42161,
+          to: V2_ROUTER,
+          input: {
+            $abi: V2_ROUTER_ABI,
+            $and: [
+              {
+                to: '0x4c0ccf331fb23ca8bd5139b886cc821ede7b4204',
+                path: {
+                  $and: [
+                    {
+                      $first: getAddress(ANIMA),
+                    },
+                    {
+                      $last: getAddress(MAGIC),
+                    },
+                  ],
+                },
+              },
+              {
+                $or: [
+                  {
+                    amountIn: {
+                      $gte: '174560000000000000000',
+                    },
+                    amountOutMin: {
+                      $gte: '24980000000000000000',
+                    },
+                  },
+                  {
+                    amountInMax: {
+                      $gte: '174560000000000000000',
+                    },
+                    amountOut: {
+                      $gte: '24980000000000000000',
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        })
+      })
+    })
 
     describe('should pass filter with valid transactions', () => {
       passingTestCases.forEach((testCase) => {
