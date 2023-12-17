7a74307809d41e3e76734d5af18284e0519ed0f7
Mmackz <matthewmackenz@gmail.com>
2023-12-16 20:20:42 -0800

    refactor(treasure): export anima and magic tokens to be used in testing

--- packages/treasure/src/constants.ts
+++ packages/treasure/src/constants.ts
@@ -4,8 +4,8 @@ import { type Address } from 'viem'
 export const V2_ROUTER = '0x23805449f91bb2d2054d9ba288fdc8f09b5eac79'
 
 // Token Addresses
-const MAGIC = '0x539bde0d7dbd336b79148aa742883198bbf60342'
-const ANIMA = '0xccd05a0fcfc1380e9da27862adb2198e58e0d66f'
+export const MAGIC = '0x539bde0d7dbd336b79148aa742883198bbf60342'
+export const ANIMA = '0xccd05a0fcfc1380e9da27862adb2198e58e0d66f'
 const ELM = '0xeeac5e75216571773c0064b3b591a86253791db6'
 const GFLY = '0x872bad41cfc8ba731f811fea8b2d0b9fd6369585'
 
