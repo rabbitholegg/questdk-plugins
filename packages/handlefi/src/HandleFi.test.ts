2f0837ac99a3c848acbb5701260c2e963264c353
Mmackz <matthewmackenz@gmail.com>
2023-12-19 14:15:55 -0800

    feat(handlefi): initialize plugin from template

---
+++ packages/handlefi/src/HandleFi.test.ts
@@ -0,0 +1,12 @@
+import { apply } from '@rabbitholegg/questdk/filter'
+import { describe, expect, test } from 'vitest'
+
+describe('Given the handlefi plugin', () => {
+  describe('When handling the swap action', () => {
+    describe('should return a valid action filter', () => {})
+
+    describe('should pass filter with valid transactions', () => {})
+
+    describe('should not pass filter with invalid transactions', () => {})
+  })
+})
---
+++ packages/handlefi/src/HandleFi.ts
@@ -0,0 +1,36 @@
+import {
+  type TransactionFilter,
+  type SwapActionParams,
+  compressJson,
+} from '@rabbitholegg/questdk'
+import { type Address } from 'viem'
+
+export const swap = async (
+  swap: SwapActionParams,
+): Promise<TransactionFilter> => {
+  const {
+    chainId,
+    contractAddress,
+    tokenIn,
+    tokenOut,
+    amountIn,
+    amountOut,
+    recipient,
+  } = swap
+
+  return compressJson({
+    chainId: 0, // The chainId of the source chain
+    to: 0x0, // The contract address of the bridge
+    input: {}, // The input object is where we'll put the ABI and the parameters
+  })
+}
+
+export const getSupportedTokenAddresses = async (
+  _chainId: number,
+): Promise<Address[]> => {
+  return []
+}
+
+export const getSupportedChainIds = async (): Promise<number[]> => {
+  return []
+}
