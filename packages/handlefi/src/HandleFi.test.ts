31e54f3b6ba57885c3bca59e32a934a808ff2704
Mmackz <matthewmackenz@gmail.com>
2023-12-20 22:20:10 -0800

    test(handlefi): setup test cases for paraswap routes

--- packages/handlefi/src/HandleFi.test.ts
+++ packages/handlefi/src/HandleFi.test.ts
@@ -1,12 +1,34 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { swap } from './HandleFi'
+import { passingTestCases, failingTestCases } from './test-transactions'
 
 describe('Given the handlefi plugin', () => {
   describe('When handling the swap action', () => {
-    describe('should return a valid action filter', () => {})
+    describe('should return a valid action filter', () => {
+      test('', () => {
+        expect(true).to.be.true
+      })
+    })
 
-    describe('should pass filter with valid transactions', () => {})
+    describe('should pass filter with valid transactions', () => {
+      passingTestCases.forEach((testCase) => {
+        const { transaction, description, params } = testCase
+        test(description, async () => {
+          const filter = await swap(params)
+          expect(apply(transaction, filter)).to.be.true
+        })
+      })
+    })
 
-    describe('should not pass filter with invalid transactions', () => {})
+    describe('should not pass filter with invalid transactions', () => {
+      failingTestCases.forEach((testCase) => {
+        const { transaction, description, params } = testCase
+        test(description, async () => {
+          const filter = await swap(params)
+          expect(apply(transaction, filter)).to.be.false
+        })
+      })
+    })
   })
 })
