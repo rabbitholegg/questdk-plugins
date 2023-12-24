91bfbdc3e33478b72467070f017f3578050eb2ec
Mmackz <matthewmackenz@gmail.com>
2023-12-23 18:17:12 -0800

    test(handlefi): test valid token addresses are returned for each valid chain

--- packages/handlefi/src/HandleFi.test.ts
+++ packages/handlefi/src/HandleFi.test.ts
@@ -1,7 +1,8 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
-import { swap } from './HandleFi'
+import { swap, getSupportedTokenAddresses } from './HandleFi'
 import { passingTestCases, failingTestCases } from './test-transactions'
+import { ARBITRUM_ONE } from './constants'
 import {
   PARASWAP_ABI,
   V2_ROUTER_ABI,
@@ -209,6 +210,22 @@ describe('Given the handlefi plugin', () => {
         test(description, async () => {
           const filter = await swap(params)
           expect(apply(transaction, filter)).to.be.false
+        })
+      })
+    })
+
+    describe('should return a valid list of tokens for each supported chain', () => {
+      test(`for chainId: ${ARBITRUM_ONE}`, async () => {
+        const tokens = await getSupportedTokenAddresses(ARBITRUM_ONE)
+        const addressRegex = /^0x[a-fA-F0-9]{40}$/
+        expect(tokens).to.be.an('array')
+        expect(tokens).to.have.length.greaterThan(0)
+        expect(tokens).to.have.length.lessThan(100)
+        tokens.forEach((token) => {
+          expect(token).to.match(
+            addressRegex,
+            `Token address ${token} is not a valid Ethereum address`,
+          )
         })
       })
     })
