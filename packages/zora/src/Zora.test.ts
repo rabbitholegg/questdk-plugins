e1350e2b08c050d0b440fc7925efe75aca68b47f
Mmackz <matthewmackenz@gmail.com>
2023-12-27 15:30:57 -0800

    test(zora): add additional tests for batch mint function

--- packages/zora/src/Zora.test.ts
+++ packages/zora/src/Zora.test.ts
@@ -3,65 +3,121 @@ import { describe, expect, test } from 'vitest'
 import { failingTestCases, passingTestCases } from './test-setup'
 import { BASIC_PURCHASE } from './test-transactions'
 import { mint } from './Zora'
-import { ZORA_MINTER_ABI_1155, ZORA_MINTER_ABI_721 } from './abi'
+import {
+  ZORA_MINTER_ABI_1155,
+  ZORA_MINTER_ABI_721,
+  UNIVERSAL_MINTER_ABI,
+} from './abi'
 
 describe('Given the zora plugin', () => {
   describe('When handling the mint', () => {
-    // test('should return a valid action filter', async () => {
-    //   const { params } = BASIC_PURCHASE
-    //   const filter = await mint(params)
-    //   expect(filter).to.deep.equal({
-    //     chainId: 10,
-    //     to: {
-    //       $or: [
-    //         '0xfff631ef40557f8705e89053af794a1dcfa0a90b',
-    //         '0x97eb05b8db496b12244bccf17cf377d00a99b67a',
-    //       ],
-    //     },
-    //     input: {
-    //       $or: [
-    //         {
-    //           $abiAbstract: ZORA_MINTER_ABI_721,
-    //           $and: [
-    //             {
-    //               $or: [
-    //                 {
-    //                   recipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //                 {
-    //                   tokenRecipient:
-    //                     '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //                 {
-    //                   to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //               ],
-    //             },
-    //           ],
-    //         },
-    //         {
-    //           $abiAbstract: ZORA_MINTER_ABI_1155,
-    //           $and: [
-    //             {
-    //               $or: [
-    //                 {
-    //                   recipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //                 {
-    //                   tokenRecipient:
-    //                     '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //                 {
-    //                   to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
-    //                 },
-    //               ],
-    //             },
-    //           ],
-    //         },
-    //       ],
-    //     },
-    //   })
-    // })
+    test('should return a valid action filter', async () => {
+      const { params } = BASIC_PURCHASE
+      const filter = await mint(params)
+      expect(filter).to.deep.equal({
+        chainId: 10,
+        to: {
+          $or: [
+            '0xfff631ef40557f8705e89053af794a1dcfa0a90b',
+            '0x97eb05b8db496b12244bccf17cf377d00a99b67a',
+          ],
+        },
+        input: {
+          $or: [
+            {
+              $abiAbstract: UNIVERSAL_MINTER_ABI,
+              _targets: {
+                $some: '0xfFF631EF40557f8705e89053aF794a1DCFA0A90b',
+              },
+              _calldatas: {
+                $some: {
+                  $or: [
+                    {
+                      $abi: ZORA_MINTER_ABI_721,
+                      $and: [
+                        {
+                          $or: [
+                            {
+                              recipient:
+                                '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                            {
+                              tokenRecipient:
+                                '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                            {
+                              to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                    {
+                      $abi: ZORA_MINTER_ABI_1155,
+                      $and: [
+                        {
+                          $or: [
+                            {
+                              recipient:
+                                '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                            {
+                              tokenRecipient:
+                                '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                            {
+                              to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              },
+            },
+            {
+              $abi: ZORA_MINTER_ABI_721,
+              $and: [
+                {
+                  $or: [
+                    {
+                      recipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                    {
+                      tokenRecipient:
+                        '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                    {
+                      to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              $abi: ZORA_MINTER_ABI_1155,
+              $and: [
+                {
+                  $or: [
+                    {
+                      recipient: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                    {
+                      tokenRecipient:
+                        '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                    {
+                      to: '0x628d4c61d81ac4f286b1778a063ed2f8810bc367',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      })      
+    })
 
     describe('should pass filter with valid transactions', () => {
       passingTestCases.forEach((testCase) => {
@@ -73,14 +129,14 @@ describe('Given the zora plugin', () => {
       })
     })
 
-    // describe('should not pass filter with invalid transactions', () => {
-    //   failingTestCases.forEach((testCase) => {
-    //     const { transaction, params, description } = testCase
-    //     test(description, async () => {
-    //       const filter = await mint(params)
-    //       expect(apply(transaction, filter)).to.be.false
-    //     })
-    //   })
-    // })
+    describe('should not pass filter with invalid transactions', () => {
+      failingTestCases.forEach((testCase) => {
+        const { transaction, params, description } = testCase
+        test(description, async () => {
+          const filter = await mint(params)
+          expect(apply(transaction, filter)).to.be.false
+        })
+      })
+    })
   })
 })
--- packages/zora/src/test-setup.ts
+++ packages/zora/src/test-setup.ts
@@ -8,13 +8,15 @@ import {
 } from './test-transactions'
 
 export const passingTestCases = [
-  // createTestCase(BASIC_PURCHASE, 'when doing a basic purchase'),
-  // createTestCase(MINT_WITH_REWARDS, 'Minting with rewards'),
-  // createTestCase(MINT_WITH_REWARDS_1155, 'Minting with rewards 1155'),
+  createTestCase(BASIC_PURCHASE, 'when doing a basic purchase'),
+  createTestCase(MINT_WITH_REWARDS, 'Minting with rewards'),
+  createTestCase(MINT_WITH_REWARDS_1155, 'Minting with rewards 1155'),
   createTestCase(MINT_BATCH_WITHOUT_FEES, 'When using the batch mint function'),
-  // createTestCase(MINT_WITH_REWARDS, 'when contractAddress is checksummed', {
-  //   contractAddress: getAddress(MINT_WITH_REWARDS.params.contractAddress),
-  // }),
+  createTestCase(MINT_WITH_REWARDS, 'when contractAddress is checksummed', {
+    contractAddress: getAddress(MINT_WITH_REWARDS.params.contractAddress),
+  }),
+  createTestCase(MINT_BATCH_WITHOUT_FEES, 'using "any tokenId" on batchmint', { tokenId: undefined, amount: undefined }),
+  createTestCase(MINT_WITH_REWARDS, 'using "any tokenId" on single mint', { tokenId: undefined, amount: undefined })
 ]
 
 export const failingTestCases = [
@@ -32,5 +34,11 @@ export const failingTestCases = [
   }),
   createTestCase(MINT_WITH_REWARDS_1155, 'when amount is incorrect', {
     amount: '72',
+  }),
+  createTestCase(MINT_BATCH_WITHOUT_FEES, 'when using batchMint and contractAddress is incorrect', {
+    contractAddress: '0x4f330940159fB3368F5b06b34212C0cDB4e2C032',
+  }),
+  createTestCase(MINT_BATCH_WITHOUT_FEES, 'when using batchMint and tokenId is incorrect', {
+    tokenId: 10,
   }),
 ]
