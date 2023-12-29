d7dd0511aaf3715e43d3635f6a3f3532c0edea65
Mmackz <matthewmackenz@gmail.com>
2023-12-29 10:33:59 -0800

    refactor(camelot): use V3 path query function

--- packages/camelot/src/utils.ts
+++ packages/camelot/src/utils.ts
@@ -61,7 +61,7 @@ export function createTestCase<T extends ActionParams>(
   }
 }
 
-export const buildPathQuery = (tokenIn?: string, tokenOut?: string) => {
+export const buildV2PathQuery = (tokenIn?: string, tokenOut?: string) => {
   // v2 paths are formatted as [<token>, <token>]
   const conditions: FilterOperator[] = []
 
@@ -71,6 +71,25 @@ export const buildPathQuery = (tokenIn?: string, tokenOut?: string) => {
 
   if (tokenOut) {
     conditions.push({ $last: tokenOut })
+  }
+
+  return {
+    $and: conditions,
+  }
+}
+
+export const buildV3PathQuery = (tokenIn?: string, tokenOut?: string) => {
+  // v3 paths are formatted as 0x<token><fee><token>
+
+  const conditions: FilterOperator[] = []
+
+  if (tokenIn) {
+    conditions.push({ $regex: `^${tokenIn.toLowerCase()}` })
+  }
+
+  if (tokenOut) {
+    // Chop the 0x prefix before comparing
+    conditions.push({ $regex: `${tokenOut.slice(2).toLowerCase()}$` })
   }
 
   return {
