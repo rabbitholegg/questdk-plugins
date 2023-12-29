e856ff9af6bf1e5081613d5842049cc4975bba8c
Mmackz <matthewmackenz@gmail.com>
2023-12-29 13:43:31 -0800

    refactor(camelot): adjust filter test

--- packages/camelot/src/Camelot.test.ts
+++ packages/camelot/src/Camelot.test.ts
@@ -74,11 +74,11 @@ describe('Given the camelot plugin', () => {
                           $and: [
                             {
                               $regex:
-                                '^0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9',
+                                '^0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
                             },
                             {
                               $regex:
-                                '82af49447d8a07e3bd95bd0d56f35241523fbab1$',
+                                'fd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9$',
                             },
                           ],
                         },
