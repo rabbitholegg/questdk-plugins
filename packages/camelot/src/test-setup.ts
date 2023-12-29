ee8b511a061c9b30299298f5c41d5a3c06712daa
Mmackz <matthewmackenz@gmail.com>
2023-12-29 12:09:13 -0800

    test(camelot): add v3 tests

--- packages/camelot/src/test-setup.ts
+++ packages/camelot/src/test-setup.ts
@@ -4,6 +4,8 @@ import { Tokens, createTestCase } from './utils'
 import {
   V2_SWAP_ETH,
   V2_SWAP_TOKENS,
+  V3_SWAP_ETH,
+  V3_TOKEN_TO_ETH,
   PARASWAP_SIMPLESWAP,
   PARASWAP_MULTISWAP,
   PARASWAP_MEGASWAP,
@@ -16,6 +18,8 @@ import {
 export const passingTestCases = [
   createTestCase(V2_SWAP_ETH, 'when swapping ETH for tokens'),
   createTestCase(V2_SWAP_TOKENS, 'when swapping tokens for tokens'),
+  createTestCase(V3_SWAP_ETH, 'when swapping ETH for tokens V3'),
+  createTestCase(V3_TOKEN_TO_ETH, 'when swapping Tokens for ETH V3'),
   createTestCase(PARASWAP_SIMPLESWAP, 'for simple swap'),
   createTestCase(PARASWAP_MULTISWAP, 'for multi swap'),
   createTestCase(PARASWAP_MEGASWAP, 'for mega swap'),
