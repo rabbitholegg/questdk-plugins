f37b3b9c804dd8722e0f9ac5e249791bd560c81c
Mmackz <matthewmackenz@gmail.com>
2024-01-02 22:39:58 -0800

    test(soundxyz): setup unit tests

--- packages/soundxyz/src/Soundxyz.test.ts
+++ packages/soundxyz/src/Soundxyz.test.ts
@@ -1,21 +1,31 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { mint } from './Soundxyz'
+import { passingTestCases, failingTestCases } from './test-transactions'
 
 // Replace *project* with the name of the project
 describe('Given the soundxyz plugin', () => {
   describe('When handling the mint action', () => {
+    describe('should return a valid action filter', () => {})
 
-    describe('should return a valid action filter', () => {
-      
+    describe('should pass filter with valid transactions', () => {
+      passingTestCases.forEach((testCase) => {
+        const { description, params, transaction } = testCase
+        test(description, async () => {
+          const filter = await mint(params)
+          expect(apply(transaction, filter)).to.be.true
+        })
+      })
     })
 
-    describe('should pass filter with valid transactions',  () => {
-      
+    describe('should not pass filter with invalid transactions', () => {
+      failingTestCases.forEach((testCase) => {
+        const { description, params, transaction } = testCase
+        test(description, async () => {
+          const filter = await mint(params)
+          expect(apply(transaction, filter)).to.be.false
+        })
+      })
     })
-    
-    describe('should not pass filter with invalid transactions',  () => {
-      
-    })
-
   })
 })
