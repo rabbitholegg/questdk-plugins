7f234143a0313cae70a642226bc1e650a81d40d8
Mmackz <matthewmackenz@gmail.com>
2024-01-02 22:39:24 -0800

    feat(soundxyz): add constants for contracts and abi

---
+++ packages/soundxyz/src/abi.ts
@@ -0,0 +1,93 @@
+export const SUPERMINTER_ABI = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: 'address', name: 'edition', type: 'address' },
+          { internalType: 'uint8', name: 'tier', type: 'uint8' },
+          { internalType: 'uint8', name: 'scheduleNum', type: 'uint8' },
+          { internalType: 'address', name: 'to', type: 'address' },
+          { internalType: 'uint32', name: 'quantity', type: 'uint32' },
+          { internalType: 'address', name: 'allowlisted', type: 'address' },
+          {
+            internalType: 'uint32',
+            name: 'allowlistedQuantity',
+            type: 'uint32',
+          },
+          {
+            internalType: 'bytes32[]',
+            name: 'allowlistProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint96', name: 'signedPrice', type: 'uint96' },
+          { internalType: 'uint32', name: 'signedQuantity', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedClaimTicket', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedDeadline', type: 'uint32' },
+          { internalType: 'bytes', name: 'signature', type: 'bytes' },
+          { internalType: 'address', name: 'affiliate', type: 'address' },
+          {
+            internalType: 'bytes32[]',
+            name: 'affiliateProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint256', name: 'attributionId', type: 'uint256' },
+        ],
+        internalType: 'struct ISuperMinter.MintTo',
+        name: 'p',
+        type: 'tuple',
+      },
+    ],
+    name: 'mintTo',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
+
+export const SUPERMINTER_V2_ABI = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: 'address', name: 'edition', type: 'address' },
+          { internalType: 'uint8', name: 'tier', type: 'uint8' },
+          { internalType: 'uint8', name: 'scheduleNum', type: 'uint8' },
+          { internalType: 'address', name: 'to', type: 'address' },
+          { internalType: 'uint32', name: 'quantity', type: 'uint32' },
+          { internalType: 'address', name: 'allowlisted', type: 'address' },
+          {
+            internalType: 'uint32',
+            name: 'allowlistedQuantity',
+            type: 'uint32',
+          },
+          {
+            internalType: 'bytes32[]',
+            name: 'allowlistProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint96', name: 'signedPrice', type: 'uint96' },
+          { internalType: 'uint32', name: 'signedQuantity', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedClaimTicket', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedDeadline', type: 'uint32' },
+          { internalType: 'bytes', name: 'signature', type: 'bytes' },
+          { internalType: 'address', name: 'affiliate', type: 'address' },
+          {
+            internalType: 'bytes32[]',
+            name: 'affiliateProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint256', name: 'attributionId', type: 'uint256' },
+        ],
+        internalType: 'struct ISuperMinterV2.MintTo',
+        name: 'p',
+        type: 'tuple',
+      },
+    ],
+    name: 'mintTo',
+    outputs: [
+      { internalType: 'uint256', name: 'fromTokenId', type: 'uint256' },
+    ],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
