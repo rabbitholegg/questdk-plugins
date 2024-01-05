6e806c95ee4d4dccc51bc2c4c2bfcb6cc1812507
Mmackz <matthewmackenz@gmail.com>
2024-01-05 15:23:36 -0800

    fix(traderjoe): fix issue with amount not working correctly when using WETH

--- packages/traderjoe/src/TraderJoe.ts
+++ packages/traderjoe/src/TraderJoe.ts
@@ -8,6 +8,14 @@ import { LBRouterV21ABI, LB_ROUTER_V21_ADDRESS } from '@traderjoe-xyz/sdk-v2'
 import { DEFAULT_SWAP_TOKEN_LIST, Tokens } from './contract-addresses'
 import { CHAIN_ID_ARRAY } from './chain-ids'
 import { buildPathQuery, Chains } from './utils'
+import {
+  EXACT_NATIVE_FOR_TOKENS_ABI,
+  EXACT_TOKENS_FOR_NATIVE_ABI,
+  EXACT_TOKENS_FOR_TOKENS_ABI,
+  NATIVE_FOR_EXACT_TOKENS_ABI,
+  TOKENS_FOR_EXACT_NATIVE_ABI,
+  TOKENS_FOR_EXACT_TOKENS_ABI,
+} from './abi'
 
 export const swap = async (
   swap: SwapActionParams,
@@ -26,33 +34,53 @@ export const swap = async (
   const nativeOut = tokenOut === NATIVE_TOKEN
   const to = contractAddress ?? LB_ROUTER_V21_ADDRESS[chainId as Chains]
 
+  const input = {
+    $abi: LBRouterV21ABI,
+    to: recipient,
+    path: {
+      tokenPath: buildPathQuery(
+        nativeIn ? Tokens[chainId]?.WETH : tokenIn,
+        nativeOut ? Tokens[chainId]?.WETH : tokenOut,
+      ),
+    },
+  }
+
   return compressJson({
     chainId,
     to,
-    value: nativeIn ? amountIn : undefined,
+    value: nativeIn ? amountIn : tokenIn ? 0 : undefined,
     input: {
-      $abi: LBRouterV21ABI,
       $and: [
-        {
-          to: recipient,
-          path: {
-            tokenPath: buildPathQuery(
-              nativeIn ? Tokens[chainId]?.WETH : tokenIn,
-              nativeOut ? Tokens[chainId]?.WETH : tokenOut,
-            ),
-          },
-        },
+        input,
         {
           $or: [
             {
-              // exactNativeforTokens, exactTokensForTokens
-              amountIn: nativeIn ? undefined : amountIn,
+              // exactNativeforTokens
+              $abi: EXACT_NATIVE_FOR_TOKENS_ABI,
               amountOutMin: amountOut,
             },
-            { amountOut: amountOut }, // nativeForExactTokens
-            { amountInMax: amountIn, amountOut: amountOut }, // tokensForExactTokens
-            { amountIn: amountIn, amountOutMinNATIVE: amountOut }, // exactTokensForNative
-            { amountInMax: amountIn, amountNATIVEOut: amountOut }, // tokensForExactNative
+            {
+              // exactNativeforTokens
+              $abi: EXACT_TOKENS_FOR_TOKENS_ABI,
+              amountIn,
+              amountOutMin: amountOut,
+            },
+            { $abi: NATIVE_FOR_EXACT_TOKENS_ABI, amountOut: amountOut }, // nativeForExactTokens
+            {
+              $abi: TOKENS_FOR_EXACT_TOKENS_ABI,
+              amountInMax: amountIn,
+              amountOut: amountOut,
+            }, // tokensForExactTokens
+            {
+              $abi: EXACT_TOKENS_FOR_NATIVE_ABI,
+              amountIn: amountIn,
+              amountOutMinNATIVE: amountOut,
+            }, // exactTokensForNative
+            {
+              $abi: TOKENS_FOR_EXACT_NATIVE_ABI,
+              amountInMax: amountIn,
+              amountNATIVEOut: amountOut,
+            }, // tokensForExactNative
           ],
         },
       ],
