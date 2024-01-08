3db7e5fcb0d653f90dd2ddaf1690dc7270067100
Mmackz <matthewmackenz@gmail.com>
2024-01-08 14:28:06 -0800

    test(vela): add additional test cases

--- packages/vela/src/test-transactions.ts
+++ packages/vela/src/test-transactions.ts
@@ -4,7 +4,7 @@ import {
   LessThanOrEqual,
   OrderType,
 } from '@rabbitholegg/questdk'
-import { parseUnits } from 'viem'
+import { parseUnits, zeroAddress } from 'viem'
 import { type TestParams, createTestCase, Chains } from './utils'
 
 const ARB_NEW_POSITION: TestParams<OptionsActionParams> = {
@@ -44,7 +44,25 @@ const ARB_MARKET_ORDER: TestParams<OptionsActionParams> = {
   },
 }
 
-const BTC_MARKET_ORDER: TestParams<OptionsActionParams> = {
+const LINK_STOP_MARKET: TestParams<OptionsActionParams> = {
+  transaction: {
+    chainId: 42161,
+    to: '0xc4abade3a15064f9e3596943c699032748b13352',
+    from: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+    input:
+      '0xdf33dc16002702000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a7fda314b1a74b2b5aa0000000000001da8ac8354dbce97db72c0000000000b870f0d0b7b6ecc05cb19a000000',
+    value: '250000000000000',
+  },
+  params: {
+    chainId: Chains.ARBITRUM_ONE,
+    token: '0xf97f4df75117a78c1a5a0dbb814af92458539fb4', // LINK (39)
+    amount: GreaterThanOrEqual(parseUnits('37.5971', 18)),
+    recipient: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+    orderType: OrderType.Market,
+  },
+}
+
+const BTC_LIMIT_ORDER: TestParams<OptionsActionParams> = {
   transaction: {
     chainId: 42161,
     to: '0xc4abade3a15064f9e3596943c699032748b13352',
@@ -55,8 +73,26 @@ const BTC_MARKET_ORDER: TestParams<OptionsActionParams> = {
   },
   params: {
     chainId: Chains.ARBITRUM_ONE,
-    token: '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', // ARB BTC (1)
+    token: '0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f', // BTC (1)
     amount: GreaterThanOrEqual(parseUnits('20.315245', 18)),
+    recipient: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+    orderType: OrderType.Limit,
+  },
+}
+
+const ETH_STOPLIMIT: TestParams<OptionsActionParams> = {
+  transaction: {
+    chainId: 42161,
+    to: '0xc4abade3a15064f9e3596943c699032748b13352',
+    from: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+    input:
+      '0xdf33dc16000283000000000000000000000000000000000000000000000000000000000000006cbaececab5863a0846a3000000000006cbaececab5863a0846a30000000000001809f2534742567c0fe5380000000000f0637740c89760d89ef43000000',
+    value: '250000000000000',
+  },
+  params: {
+    chainId: Chains.ARBITRUM_ONE,
+    token: zeroAddress, // ETH (2)
+    amount: GreaterThanOrEqual(parseUnits('22.0531', 18)),
     recipient: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
     orderType: OrderType.Limit,
   },
@@ -64,13 +100,33 @@ const BTC_MARKET_ORDER: TestParams<OptionsActionParams> = {
 
 export const passingTestCases = [
   createTestCase(ARB_NEW_POSITION, 'when opening a new position'),
-  createTestCase(ARB_MARKET_ORDER, 'when opening a new position'),
-  createTestCase(BTC_MARKET_ORDER, 'when opening a market order'),
+  createTestCase(ARB_MARKET_ORDER, 'when opening a market order'),
+  createTestCase(LINK_STOP_MARKET, 'when opening a stop-market order'),
+  createTestCase(BTC_LIMIT_ORDER, 'when opening a limit order'),
+  createTestCase(ETH_STOPLIMIT, 'when opening a stop-limit order'),
   createTestCase(ARB_MARKET_ORDER, 'when amount is exact', {
     amount: parseUnits('34.82474', 18),
   }),
   createTestCase(ARB_MARKET_ORDER, 'when using $lte operator', {
     amount: LessThanOrEqual(parseUnits('34.82474', 18)),
+  }),
+  createTestCase(ARB_NEW_POSITION, 'when amount is set to any', {
+    amount: undefined,
+  }),
+  createTestCase(ARB_NEW_POSITION, 'when orderType is set to any', {
+    orderType: undefined,
+  }),
+  createTestCase(ARB_NEW_POSITION, 'when recipient is set to any', {
+    recipient: undefined,
+  }),
+  createTestCase(ARB_MARKET_ORDER, 'when token is set to any', {
+    token: undefined,
+  }),
+  createTestCase(BTC_LIMIT_ORDER, 'when all parameters are set to any', {
+    amount: undefined,
+    orderType: undefined,
+    recipient: undefined,
+    token: undefined,
   }),
 ]
 
@@ -104,4 +160,8 @@ export const failingTestCases = [
   createTestCase(ARB_NEW_POSITION, 'when recipient is not correct', {
     recipient: '0x512daa85f8d2c863d0cfc8f65ab7842629d409f6',
   }),
+  createTestCase(ARB_NEW_POSITION, 'when orderType is not correct', {
+    orderType: OrderType.Limit,
+  }),
+  
 ]
