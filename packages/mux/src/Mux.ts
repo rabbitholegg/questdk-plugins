51c84eda214789f588fd3d434cdfd93021efe10e
Mmackz <matthewmackenz@gmail.com>
2024-01-11 09:21:45 -0800

    fix(mux): fix issue with ordertype

--- packages/mux/src/Mux.ts
+++ packages/mux/src/Mux.ts
@@ -5,25 +5,37 @@ import {
   compressJson,
 } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
-import { CHAIN_ID_TO_ORDER_BOOK_ADDRESS, OrderBook__factory } from '@mux-network/mux.js'
+import {
+  CHAIN_ID_TO_ORDER_BOOK_ADDRESS,
+  OrderBook__factory,
+  CHAIN_ID_TO_AGGREGATOR_FACTORY_ADDRESS,
+  AggregatorGmxV2Adapter__factory,
+} from '@mux-network/mux.js'
 import { buildSubAccountIdQuery } from './helpers'
 
 export const options = async (
   trade: OptionsActionParams,
 ): Promise<TransactionFilter> => {
   const { chainId, token, amount, recipient, orderType } = trade
-
   const muxContract = CHAIN_ID_TO_ORDER_BOOK_ADDRESS[chainId] ?? '0x0'
+  const aggregatorContract =
+    CHAIN_ID_TO_AGGREGATOR_FACTORY_ADDRESS[chainId] ?? '0x0'
   const subAccountQuery = await buildSubAccountIdQuery(
     recipient,
     token,
     chainId,
   )
+  const muxDeadline =
+    orderType === OrderType.Market
+      ? 0
+      : orderType === OrderType.Limit
+      ? { $gt: '0' }
+      : undefined
 
   return compressJson({
     chainId,
     to: {
-      $or: [muxContract.toLowerCase()],
+      $or: [muxContract.toLowerCase(), aggregatorContract.toLowerCase()],
     },
     input: {
       $or: [
@@ -31,7 +43,10 @@ export const options = async (
           $abi: OrderBook__factory.abi,
           subAccountId: subAccountQuery,
           collateralAmount: amount,
-          deadline: orderType === OrderType.Market ? 0 : { $gte: '0' },
+          deadline: muxDeadline,
+        },
+        {
+          $abi: AggregatorGmxV2Adapter__factory.abi,
         },
       ],
     },
