ad9af151b1a72a953feac6c98e25919bb7a09a4e
Mmackz <matthewmackenz@gmail.com>
2024-01-10 23:26:23 -0800

    test(mux): add tests for mux orderbook routes

--- packages/mux/src/Mux.test.ts
+++ packages/mux/src/Mux.test.ts
@@ -1,20 +1,22 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
+import { options } from './Mux'
+import { passingTestCases } from './test-transactions'
 
 describe('Given the mux plugin', () => {
-  describe('When handling the options action', () => {
+  describe('When handling the options action', async () => {
+    describe('should return a valid action filter', () => {})
 
-    describe('should return a valid action filter', () => {
-      
-    })
+    describe('should pass filter with valid transactions', () => {
+      passingTestCases.forEach((testCase) => {
+        const { description, transaction, params } = testCase
+        test(description, async () => {
+          const filter = await options(params)
+          expect(apply(transaction, filter)).to.be.true
+        })
+      })
 
-    describe('should pass filter with valid transactions',  () => {
-      
+      describe('should not pass filter with invalid transactions', () => {})
     })
-    
-    describe('should not pass filter with invalid transactions',  () => {
-      
-    })
-
   })
 })
