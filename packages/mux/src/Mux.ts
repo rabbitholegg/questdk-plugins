881156ef4d3d2fee8921bffbf6f96e6144db9247
Mmackz <matthewmackenz@gmail.com>
2024-01-10 23:26:43 -0800

    feat(mux): implement filter for mux orderbook transactions

--- packages/mux/src/Mux.ts
+++ packages/mux/src/Mux.ts
@@ -1,18 +1,38 @@
 import {
   type TransactionFilter,
   type OptionsActionParams,
+  OrderType,
   compressJson,
 } from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
+import { CHAIN_ID_TO_ORDER_BOOK_ADDRESS } from '@mux-network/mux.js'
+import { MUX_ORDERBOOK_ABI } from './abi'
+import { buildSubAccountIdQuery } from './utils'
 
 export const options = async (
   trade: OptionsActionParams,
 ): Promise<TransactionFilter> => {
-  const { chainId, contractAddress, token, amount, recipient, orderType } =
+  const { chainId, token, amount, recipient, orderType } =
     trade
+
+  const muxContract = CHAIN_ID_TO_ORDER_BOOK_ADDRESS[chainId] ?? '0x0'
+  const subAccountQuery = await buildSubAccountIdQuery(recipient, token, chainId)
 
   return compressJson({
     chainId,
+    to: {
+      $or: [muxContract.toLowerCase()],
+    },
+    input: {
+      $or: [
+        {
+          $abi: MUX_ORDERBOOK_ABI,
+          subAccountId: subAccountQuery,
+          collateralAmount: amount,
+          deadline: orderType === OrderType.Market ? 0 : { $gte: '0' },
+        },
+      ],
+    },
   })
 }
 
