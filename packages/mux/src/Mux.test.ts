390a4f51336a49f59680b35c27b56cc4d9d56313
Mmackz <matthewmackenz@gmail.com>
2024-01-11 14:30:00 -0800

    test(mux): add v2 multicall tests

--- packages/mux/src/Mux.test.ts
+++ packages/mux/src/Mux.test.ts
@@ -15,14 +15,13 @@ describe('Given the mux plugin', () => {
           expect(apply(transaction, filter)).to.be.true
         })
       })
-
-      describe('should not pass filter with invalid transactions', () => {
-        failingTestCases.forEach((testCase) => {
-          const { description, transaction, params } = testCase
-          test(description, async () => {
-            const filter = await options(params)
-            expect(apply(transaction, filter)).to.be.false
-          })
+    })
+    describe('should not pass filter with invalid transactions', () => {
+      failingTestCases.forEach((testCase) => {
+        const { description, transaction, params } = testCase
+        test(description, async () => {
+          const filter = await options(params)
+          expect(apply(transaction, filter)).to.be.false
         })
       })
     })
