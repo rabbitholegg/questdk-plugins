0aeb84019749a302d784c04b3fbd6c9d147eeb70
Mmackz <matthewmackenz@gmail.com>
2024-01-11 21:21:15 -0800

    test(mux): add test for valid action filter

--- packages/mux/src/Mux.test.ts
+++ packages/mux/src/Mux.test.ts
@@ -1,11 +1,99 @@
 import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
 import { options } from './Mux'
-import { passingTestCases, failingTestCases } from './test-transactions'
+import { GNS_ABI } from './abi'
+import {
+  AggregatorProxyFactory__factory,
+  AggregatorGmxV2Adapter__factory,
+  OrderBook__factory,
+} from '@mux-network/mux.js'
+import {
+  passingTestCases,
+  failingTestCases,
+  MUX_ETH_MARKET_LONG,
+} from './test-transactions'
 
 describe('Given the mux plugin', () => {
   describe('When handling the options action', () => {
-    describe('should return a valid action filter', () => {})
+    describe('should return a valid action filter', () => {
+      test('when making a valid trade', async () => {
+        const { params } = MUX_ETH_MARKET_LONG
+        const filter = await options(params)
+        expect(filter).to.deep.equal({
+          chainId: 42161,
+          to: {
+            $or: [
+              '0xa19fd5ab6c8dcffa2a295f78a5bb4ac543aaf5e3',
+              '0x2ff2f1d9826ae2410979ae19b88c361073ab0918',
+              '0x2c7e82641f03fa077f88833213210a86027f15dc',
+            ],
+          },
+          input: {
+            $or: [
+              {
+                // mux contract
+                $abi: OrderBook__factory.abi,
+                subAccountId: {
+                  $and: [
+                    {
+                      $regex: '^0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+                    },
+                    {
+                      $regex: '0x[a-fA-F0-9]{40}03',
+                    },
+                  ],
+                },
+                collateralAmount: {
+                  $gte: '1000000000000000',
+                },
+                deadline: 0,
+              },
+              {
+                // aggregator contract gmx V2
+                $abi: AggregatorProxyFactory__factory.abi,
+                args: {
+                  tokenIn: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+                  amountIn: {
+                    $gte: '1000000000000000',
+                  },
+                  flags: {
+                    $bitmask: {
+                      bitmask: '0x40',
+                      value: 64,
+                    },
+                  },
+                },
+              },
+              {
+                // aggregator contract gmx V2 multicall
+                $abiAbstract: AggregatorGmxV2Adapter__factory.abi,
+                createParams: {
+                  swapPath: {
+                    $regex: '^0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+                  },
+                  initialCollateralAmount: {
+                    $gte: '1000000000000000',
+                  },
+                  orderType: 2,
+                },
+              },
+              {
+                // gns contract
+                $abi: GNS_ABI,
+                t: {
+                  trader: '0x865c301c46d64de5c9b124ec1a97ef1efc1bcbd1',
+                  positionSizeDai: {
+                    $gte: '1000000000000000',
+                  },
+                },
+                orderType: 0,
+                referrer: '0x10C2CbfE29f4f5e4C24d54d36C8F283A61eB0c2f',
+              },
+            ],
+          },
+        })
+      })
+    })
 
     describe('should pass filter with valid transactions', () => {
       passingTestCases.forEach((testCase) => {
