abe2eec39223f30633a350f251f5f71f6db9a03f
Tien Tran <tien@pendle.finance>
2024-01-12 13:27:43 +0700

    add router addresses

--- packages/pendle/src/contract-addresses.ts
+++ packages/pendle/src/contract-addresses.ts
@@ -1,19 +1,23 @@
 import { SUPPORTED_CHAINS, type SupportedChainId } from './chain-ids'
 import { type Address } from 'viem'
-import ethereum from '@pendle/core-v2/deployments/1-core.json'
-import optimism from '@pendle/core-v2/deployments/10-core.json'
-import bsc from '@pendle/core-v2/deployments/56-core.json'
-import arbitrum from '@pendle/core-v2/deployments/42161-core.json'
 
 export function toAddress(rawAddress: string): Address {
   return rawAddress.toLowerCase() as Address
 }
 
 const CHAIN_TO_ROUTER_ADDRESS: Record<SupportedChainId, Address> = {
-  [SUPPORTED_CHAINS.ETHEREUM]: toAddress(ethereum.router),
-  [SUPPORTED_CHAINS.OPTIMISM]: toAddress(optimism.router),
-  [SUPPORTED_CHAINS.BSC]: toAddress(bsc.router),
-  [SUPPORTED_CHAINS.ARBITRUM]: toAddress(arbitrum.router),
+  [SUPPORTED_CHAINS.ETHEREUM]: toAddress(
+    '0x00000000005BBB0EF59571E58418F9a4357b68A0',
+  ),
+  [SUPPORTED_CHAINS.OPTIMISM]: toAddress(
+    '0x00000000005BBB0EF59571E58418F9a4357b68A0',
+  ),
+  [SUPPORTED_CHAINS.BSC]: toAddress(
+    '0x00000000005BBB0EF59571E58418F9a4357b68A0',
+  ),
+  [SUPPORTED_CHAINS.ARBITRUM]: toAddress(
+    '0x00000000005BBB0EF59571E58418F9a4357b68A0',
+  ),
 }
 
 export const getRouterAddress = (chainId: SupportedChainId): Address => {
