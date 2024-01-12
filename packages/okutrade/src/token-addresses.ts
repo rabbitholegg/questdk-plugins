97285746147be8ef2f2d7ae74f9c62cd19ef443e
Mmackz <matthewmackenz@gmail.com>
2024-01-11 22:43:40 -0800

    feat(okutrade): add USDC native to base tokens

--- packages/okutrade/src/token-addresses.ts
+++ packages/okutrade/src/token-addresses.ts
@@ -78,6 +78,7 @@ const baseTokenAddresses: Address[] = [
   '0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA', // USDbC
   '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22', // cbETH
   '0x50c5725949A6F0c72E6C4a641F24049A917DB0Cb', // DAI
+  '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913', // USDC
 ]
 
 export const CHAIN_TO_TOKENS: { [chainId: number]: Address[] | undefined } = {
