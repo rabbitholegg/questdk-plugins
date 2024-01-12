3a5716f3ce5daf469867fdab1c98b17413e10c6b
Tien Tran <tien@pendle.finance>
2024-01-12 13:11:43 +0700

    add more action params

--- packages/pendle/src/Pendle.test.ts
+++ packages/pendle/src/Pendle.test.ts
@@ -1,56 +1,92 @@
-import { apply } from '@rabbitholegg/questdk/filter'
+import { GreaterThanOrEqual, apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
-import { swap } from './Pendle.js'
+import { getSupportedTokenAddresses, swap } from './Pendle.js'
 import {
-  SWAP_EXACT_PT_FOR_YT,
-  SWAP_EXACT_SY_FOR_YT,
   SWAP_EXACT_TOKEN_FOR_YT,
-  SWAP_EXACT_YT_FOR_PT,
-  SWAP_EXACT_YT_FOR_SY,
   SWAP_EXACT_YT_FOR_TOKEN,
 } from './test-transactions'
 import { getRouterAddress } from './contract-addresses.js'
-import { SUPPORTED_CHAINS } from './chain-ids.js'
-import { SwapYTV3_ABI } from './abi.js'
+import { SUPPORTED_CHAINS, SUPPORTED_CHAINS_ARRAY } from './chain-ids.js'
+import * as abi from './abi.js'
+import { getDefaultTokenAddresses } from './pendle-backend.js'
 
 describe('Given the Pendle plugin', () => {
-  describe('When handling the swap', () => {
-    const validTransactions = [SWAP_EXACT_TOKEN_FOR_YT, SWAP_EXACT_YT_FOR_SY]
-    const invalidTransactions = [
-      SWAP_EXACT_YT_FOR_TOKEN,
-      SWAP_EXACT_YT_FOR_PT,
-      SWAP_EXACT_SY_FOR_YT,
-      SWAP_EXACT_PT_FOR_YT,
-    ]
+  describe('When handling Pendle backend', () => {
+    test.each(SUPPORTED_CHAINS_ARRAY)(
+      'should return tokens for chain %s',
+      async (chainId) => {
+        const tokens = await getSupportedTokenAddresses(chainId)
+        expect(tokens.length).greaterThan(0)
+      },
+    )
+  })
 
+  describe('When handling the swap', () => {
     test('should return a valid action filter', async () => {
+      const tokenAddress = getDefaultTokenAddresses(
+        SUPPORTED_CHAINS.ETHEREUM,
+      )[0]
       const filter = await swap({
         chainId: SUPPORTED_CHAINS.ETHEREUM,
+        tokenIn: tokenAddress,
+        amountIn: GreaterThanOrEqual(1000000000000000000n),
       })
 
       expect(filter).to.deep.equal({
         chainId: SUPPORTED_CHAINS.ETHEREUM,
         to: getRouterAddress(SUPPORTED_CHAINS.ETHEREUM),
         input: {
-          $abi: SwapYTV3_ABI,
+          $abi: abi.SWAP_EXACT_TOKEN_FOR_YT,
+          input: {
+            tokenIn: tokenAddress,
+            netTokenIn: { $gte: '1000000000000000000' },
+          },
         },
       })
     })
 
-    test.each(validTransactions)(
-      'should pass filter with valid transactions',
-      async (tx) => {
-        const filter = await swap({ chainId: tx.chainId })
-        expect(apply(tx, filter)).to.be.true
-      },
-    )
+    describe('should pass filter with valid transactions', async () => {
+      const filters = await Promise.all([
+        swap({
+          chainId: SWAP_EXACT_TOKEN_FOR_YT.chainId,
+          tokenIn: '0x0000000000000000000000000000000000000000',
+          amountIn: GreaterThanOrEqual(250000000000000000n),
+        }),
+        swap({
+          chainId: SWAP_EXACT_TOKEN_FOR_YT.chainId,
+          tokenIn: '0x0000000000000000000000000000000000000000',
+          amountIn: GreaterThanOrEqual(100000000000000000n),
+        }),
+      ])
 
-    test.each(invalidTransactions)(
-      'should not pass filter with invalid transactions',
-      async (tx) => {
-        const filter = await swap({ chainId: tx.chainId })
-        expect(apply(tx, filter)).to.be.false
-      },
-    )
+      test.each(filters)('should pass filter %#', (filter) => {
+        expect(apply(SWAP_EXACT_TOKEN_FOR_YT, filter)).to.be.true
+      })
+    })
+
+    describe('should not pass filter with invalid transactions', async () => {
+      test('invalid function', async () => {
+        const filter = await swap({ chainId: SWAP_EXACT_YT_FOR_TOKEN.chainId })
+        expect(apply(SWAP_EXACT_YT_FOR_TOKEN, filter)).to.be.false
+      })
+
+      test('tokenIn not correct', async () => {
+        const filter = await swap({
+          chainId: SWAP_EXACT_TOKEN_FOR_YT.chainId,
+          tokenIn: '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc',
+          amountIn: GreaterThanOrEqual(250000000000000000n),
+        })
+        expect(apply(SWAP_EXACT_TOKEN_FOR_YT, filter)).to.be.false
+      })
+
+      test('amountIn not valid', async () => {
+        const filter = await swap({
+          chainId: SWAP_EXACT_TOKEN_FOR_YT.chainId,
+          tokenIn: '0x3175df0976dfa876431c2e9ee6bc45b65d3473cc',
+          amountIn: GreaterThanOrEqual(300000000000000000n),
+        })
+        expect(apply(SWAP_EXACT_TOKEN_FOR_YT, filter)).to.be.false
+      })
+    })
   })
 })
--- packages/pendle/src/Pendle.ts
+++ packages/pendle/src/Pendle.ts
@@ -6,12 +6,13 @@ import {
 import { type Address } from 'viem'
 import { SUPPORTED_CHAINS_ARRAY, type SupportedChainId } from './chain-ids'
 import { getRouterAddress } from './contract-addresses'
-import { SwapYTV3_ABI } from './abi'
+import * as abi from './abi'
+import { getTokenAddresses } from './pendle-backend'
 
 export const swap = async (
   swap: SwapActionParams,
 ): Promise<TransactionFilter> => {
-  const { chainId } = swap
+  const { chainId, tokenIn, amountIn } = swap
 
   const to = SUPPORTED_CHAINS_ARRAY.includes(chainId)
     ? getRouterAddress(chainId as SupportedChainId)
@@ -21,7 +22,11 @@ export const swap = async (
     chainId,
     to,
     input: {
-      $abi: SwapYTV3_ABI,
+      $abi: abi.SWAP_EXACT_TOKEN_FOR_YT,
+      input: {
+        tokenIn: tokenIn,
+        netTokenIn: amountIn,
+      },
     },
   })
 }
@@ -29,7 +34,7 @@ export const swap = async (
 export const getSupportedTokenAddresses = async (
   _chainId: number,
 ): Promise<Address[]> => {
-  return []
+  return getTokenAddresses(_chainId as SupportedChainId)
 }
 
 export const getSupportedChainIds = async (): Promise<number[]> => {
