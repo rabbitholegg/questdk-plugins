dcd08e395bf1c263ca61ec56c35634bc7f60ba86
Quazia <alf40k@gmail.com>
2024-01-12 09:08:07 -0500

    Chore: lint

--- packages/template/src/Project.test.ts
+++ packages/template/src/Project.test.ts
@@ -4,18 +4,10 @@ import { describe, expect, test } from 'vitest'
 // Replace *project* with the name of the project
 describe('Given the project plugin', () => {
   describe('When handling the bridge', () => {
+    test('should return a valid action filter', () => {})
 
-    test('should return a valid action filter', () => {
-      
-    })
+    test('should pass filter with valid transactions', () => {})
 
-    test('should pass filter with valid transactions',  () => {
-      
-    })
-    
-    test('should not pass filter with invalid transactions',  () => {
-      
-    })
-
+    test('should not pass filter with invalid transactions', () => {})
   })
 })
--- packages/template/src/Project.ts
+++ packages/template/src/Project.ts
@@ -1,9 +1,16 @@
-
-import { type TransactionFilter, type BridgeActionParams, type SwapActionParams, type MintActionParams, compressJson } from '@rabbitholegg/questdk'
+import {
+  type TransactionFilter,
+  type BridgeActionParams,
+  type SwapActionParams,
+  type MintActionParams,
+  compressJson,
+} from '@rabbitholegg/questdk'
 import { type Address } from 'viem'
 
 // If you're implementing swap or mint, simply duplicate this function and change the name
-export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFilter> => {
+export const bridge = async (
+  bridge: BridgeActionParams,
+): Promise<TransactionFilter> => {
   // This is the information we'll use to compose the Transaction object
   const {
     sourceChainId,
@@ -17,12 +24,14 @@ export const bridge = async (bridge: BridgeActionParams): Promise<TransactionFil
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
     chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    to: 0x0, // The contract address of the bridge
+    input: {}, // The input object is where we'll put the ABI and the parameters
   })
 }
 
-export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> => {
+export const swap = async (
+  swap: SwapActionParams,
+): Promise<TransactionFilter> => {
   const {
     chainId,
     contractAddress,
@@ -36,35 +45,30 @@ export const swap = async (swap: SwapActionParams): Promise<TransactionFilter> =
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
     chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    to: 0x0, // The contract address of the bridge
+    input: {}, // The input object is where we'll put the ABI and the parameters
   })
 }
 
-export const mint = async (mint: MintActionParams): Promise<TransactionFilter> => {
-  const {
-    chainId,
-    contractAddress,
-    tokenAddress,
-    amount,
-    recipient,
-  } = mint
+export const mint = async (
+  mint: MintActionParams,
+): Promise<TransactionFilter> => {
+  const { chainId, contractAddress, tokenAddress, amount, recipient } = mint
 
   // We always want to return a compressed JSON object which we'll transform into a TransactionFilter
   return compressJson({
     chainId: 0, // The chainId of the source chain
-    to:  0x0,   // The contract address of the bridge
-    input: {},  // The input object is where we'll put the ABI and the parameters
+    to: 0x0, // The contract address of the bridge
+    input: {}, // The input object is where we'll put the ABI and the parameters
   })
 }
 
-
-export const getSupportedTokenAddresses = async (_chainId: number): Promise<Address[]> => {
+export const getSupportedTokenAddresses = async (
+  _chainId: number,
+): Promise<Address[]> => {
   // Given a specific chain we would expect this function to return a list of supported token addresses
 }
 
-
 export const getSupportedChainIds = async (): Promise<number[]> => {
   // This should return all of the ChainIds that are supported by the Project we're integrating
-
 }
