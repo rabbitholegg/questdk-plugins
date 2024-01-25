b696d294baabee6d9fef8dbdcb4f2a89367bd8af
Mmackz <matthewmackenz@gmail.com>
2024-01-25 10:41:40 -0800

    feat(vela): add ABI for TPSL orders

--- packages/vela/src/abi.ts
+++ packages/vela/src/abi.ts
@@ -1,4 +1,4 @@
-export const VAULT_ABI = [
+export const ORDER_PACKED_ABI = [
   {
     inputs: [
       { internalType: 'uint256', name: '_tokenId', type: 'uint256' },
@@ -19,6 +19,25 @@ export const VAULT_ABI = [
       { internalType: 'uint256', name: 'c', type: 'uint256' },
     ],
     name: 'newPositionOrderPacked',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
+
+export const TPSL_ORDER_ABI = [
+  {
+    inputs: [
+      { internalType: 'uint256', name: '_tokenId', type: 'uint256' },
+      { internalType: 'bool', name: '_isLong', type: 'bool' },
+      { internalType: 'enum OrderType', name: '_orderType', type: 'uint8' },
+      { internalType: 'uint256[]', name: '_params', type: 'uint256[]' },
+      { internalType: 'address', name: '_refer', type: 'address' },
+      { internalType: 'bool[]', name: '_isTPs', type: 'bool[]' },
+      { internalType: 'uint256[]', name: '_prices', type: 'uint256[]' },
+      { internalType: 'uint256[]', name: '_amountPercents', type: 'uint256[]' },
+    ],
+    name: 'newPositionOrderWithTPSL',
     outputs: [],
     stateMutability: 'payable',
     type: 'function',
