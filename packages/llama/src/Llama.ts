794fba2acc5ecad62e2b70749ac1764a2a72ce20
gnomadic <edflem+github@gmail.com>
2024-01-25 14:37:38 -0500

    fixing typos and testing signing

--- packages/llama/src/Llama.ts
+++ packages/llama/src/Llama.ts
@@ -1,8 +1,7 @@
 import { type VoteActionParams, compressJson } from '@rabbitholegg/questdk'
 import { LLAMA_ABI_CAST_APPROVE, LLAMA_ABI_CAST_DISAPPROVE } from './abi.js'
 import { CHAIN_ID_ARRAY } from './chain-ids.js'
-import { TALLY_TOKENS } from './token-addresses.js'
-
+import { LLAMA_TOKENS } from './token-addresses.js'
 
 // export type VoteActionParams = {
 //   chainId: number;
@@ -37,17 +36,17 @@ export const vote = async (voteParams: VoteActionParams) => {
     input: {
       $abi: abi,
       ...(support !== undefined && { support: support }),
-      ...(proposalId !== undefined && { actionInfo : {id: proposalId} }),
+      ...(proposalId !== undefined && { actionInfo: { id: proposalId } }),
     },
   })
 }
 
 export const getSupportedTokenAddresses = async (_chainId: number) => {
   // Given a specific chain we would expect this function to return a list of supported token addresses
-  return TALLY_TOKENS[_chainId]
+  return LLAMA_TOKENS[_chainId]
 }
 
 export const getSupportedChainIds = async () => {
   // This should return all of the ChainIds that are supported by the Project we're integrating
-  return [CHAIN_ID_ARRAY]] // only supporting ARB right now
+  return [CHAIN_ID_ARRAY] // only supporting ARB right now
 }
