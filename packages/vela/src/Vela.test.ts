350db01821cfa1928bbf77206048a4fd86cea69f
Mmackz <matthewmackenz@gmail.com>
2024-01-25 10:42:22 -0800

    test(vela): refactor valid action filter test to recognize TPSL orders

--- packages/vela/src/Vela.test.ts
+++ packages/vela/src/Vela.test.ts
@@ -2,7 +2,7 @@ import { apply } from '@rabbitholegg/questdk/filter'
 import { describe, expect, test } from 'vitest'
 import { options, getSupportedTokenAddresses } from './Vela'
 import { CHAIN_ID_ARRAY } from './chain-ids'
-import { VAULT_ABI } from './abi'
+import { ORDER_PACKED_ABI, TPSL_ORDER_ABI } from './abi'
 import { passingTestCases, failingTestCases } from './test-transactions'
 
 describe('Given the vela plugin', () => {
@@ -16,41 +16,68 @@ describe('Given the vela plugin', () => {
           to: '0xc4abade3a15064f9e3596943c699032748b13352',
           from: '0xa99f898530df1514a566f1a6562d62809e99557d',
           input: {
-            $abi: VAULT_ABI,
-            a: {
-              $and: [
-                {
-                  $bitmask: {
-                    bitmask:
-                      '0xFFFF000000000000000000000000000000000000000000000000000000000000',
-                    value:
-                      '12367929453448690307083082505200429610792387278129332706851243409048338432',
-                  },
-                },
-                {
-                  $or: [
+            $or: [
+              {
+                $abi: ORDER_PACKED_ABI,
+                a: {
+                  $and: [
                     {
                       $bitmask: {
                         bitmask:
-                          '0xFF000000000000000000000000000000000000000000000000000000000',
-                        value: '0',
+                          '0xFFFF000000000000000000000000000000000000000000000000000000000000',
+                        value:
+                          '12367929453448690307083082505200429610792387278129332706851243409048338432',
                       },
                     },
                     {
-                      $bitmask: {
-                        bitmask:
-                          '0xFF000000000000000000000000000000000000000000000000000000000',
-                        value:
-                          '13803492693581127574869511724554050904902217944340773110325048447598592',
-                      },
+                      $or: [
+                        {
+                          $bitmask: {
+                            bitmask:
+                              '0xFF000000000000000000000000000000000000000000000000000000000',
+                            value: '0',
+                          },
+                        },
+                        {
+                          $bitmask: {
+                            bitmask:
+                              '0xFF000000000000000000000000000000000000000000000000000000000',
+                            value:
+                              '13803492693581127574869511724554050904902217944340773110325048447598592',
+                          },
+                        },
+                      ],
                     },
                   ],
                 },
-              ],
-            },
-            c: {
-              $gte: '7445378188230133580578636410607088466657280000000000000000000000000000',
-            },
+                c: {
+                  $gte: '7445378188230133580578636410607088466657280000000000000000000000000000',
+                },
+              },
+              {
+                $abi: TPSL_ORDER_ABI,
+                _tokenId: '7',
+                $or: [
+                  {
+                    _orderType: 0,
+                  },
+                  {
+                    _orderType: 2,
+                  },
+                ],
+                _params: [
+                  {
+                    $gte: '0',
+                  },
+                  {
+                    $gte: '0',
+                  },
+                  {
+                    $gte: '21880000000000000000000000000000',
+                  },
+                ],
+              },
+            ],
           },
         })
       })
