6da7f8c66e086c8ce85e966b861ffcb000c59547
Mmackz <matthewmackenz@gmail.com>
2024-01-26 13:14:16 -0800

    feat(boost): add constants for boost pass contract and ABI

---
+++ packages/boost/src/constants.ts
@@ -0,0 +1,14 @@
+export const BOOST_PASS_CONTRACT = '0x9A618D6302f27cdBb97206Ce269A31C1F7da3913'
+
+export const BOOST_PASS_ABI = [
+  {
+    inputs: [
+      { internalType: 'bytes', name: 'signature_', type: 'bytes' },
+      { internalType: 'bytes', name: 'data_', type: 'bytes' },
+    ],
+    name: 'mint',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
