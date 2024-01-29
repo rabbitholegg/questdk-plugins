2b291f0e02ea7d6d063e449d6ab06388201252ba
gnomadic <edflem+github@gmail.com>
2024-01-29 16:11:40 -0500

    updating token addresses

--- packages/llama/src/token-addresses.ts
+++ packages/llama/src/token-addresses.ts
@@ -1,7 +1,11 @@
 import { type Address } from 'viem'
-import { OPTIMISM } from './chain-ids'
+import { ARBITRUM_ONE, BASE, ETHEREUM, OPTIMISM, POLYGON } from './chain-ids'
 
 export const LLAMA_TOKENS: Record<number, Address[]> = {
-  // TODO arguably we should support any token here?  it's a governance token right that's arbitrary
-  [OPTIMISM]: ['0xeccb99806ce0738918f20253f304a373ff197cdd'],
+  // These are the Llama factory addresses for now - we don't need to track a token with this project
+  [OPTIMISM]: ['0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB'],
+  [POLYGON]: ['0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB'],
+  [ETHEREUM]: ['0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB'],
+  [ARBITRUM_ONE]: ['0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB'],
+  [BASE]: ['0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB'],
 }
