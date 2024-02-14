3f5e2b4dc4b99c9b5fb2559eebd36b8f91d89707
Quazia <alf40k@gmail.com>
2024-02-13 23:31:53 -0500

    Feat(Soundxyz): add transaction intents to Soundxyz

--- packages/soundxyz/src/Soundxyz.ts
+++ packages/soundxyz/src/Soundxyz.ts
@@ -3,9 +3,10 @@ import {
   type MintActionParams,
   compressJson,
 } from '@rabbitholegg/questdk'
-import { type Address } from 'viem'
+import { encodeFunctionData, type Address, type TransactionRequest } from 'viem'
 import { SUPERMINTER, SUPERMINTER_V2, SUPERMINTER_ABI } from './constants'
 import { Chains } from './utils'
+import type { MintIntentParams } from '@rabbitholegg/questdk-plugin-utils'
 
 export const mint = async (
   mint: MintActionParams,
@@ -26,6 +27,42 @@ export const mint = async (
       },
     },
   })
+}
+
+export const getMintIntent = async (mint: MintIntentParams): Promise<TransactionRequest> => {
+  const { contractAddress, amount, recipient } = mint;
+
+  const mintTo = {
+    edition: contractAddress,
+    tier: 0, 
+    scheduleNum: 0, 
+    to: recipient,
+    quantity: 1, 
+    allowlisted: '0x0', 
+    allowlistedQuantity: 0, 
+    allowlistProof: ['0x0'], 
+    signedPrice: 0, 
+    signedQuantity: 0, 
+    signedClaimTicket: 0, 
+    signedDeadline: 0, 
+    signature: '0x0', 
+    affiliate: '0x0', 
+    affiliateProof: ['0x0'], 
+    attributionId: 0, 
+  };
+
+  const data = encodeFunctionData({
+    abi: SUPERMINTER_ABI,
+    functionName: 'purchase',
+    args: [mintTo]
+  });
+
+  return {
+    from: recipient,
+    to: contractAddress,
+    data,
+    value: amount.toString()
+  };
 }
 
 export const getSupportedTokenAddresses = async (
