483673b8c57bb531926e772ef31bc3cf2a8678ec
Mmackz <matthewmackenz@gmail.com>
2024-02-15 12:48:32 -0800

    refactor(kote): add abit and contract to constants.ts

---
+++ packages/kote/src/constants.ts
@@ -0,0 +1,11 @@
+export const VILLAGER_MINT_ABI = [
+  {
+    inputs: [],
+    name: 'mintNormal',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
+
+export const VILLAGER_MINT_ADDRESS = '0x292Ff0F0c19373dd9c50faBba574Aaaf6E1BC11B'
