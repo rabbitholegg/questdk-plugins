d25a57a65759ba5637a933f33baa17d06dfe9537
Mmackz <matthewmackenz@gmail.com>
2024-02-19 16:21:47 -0800

    feat(builder): register helper

--- apps/create-plugin/src/builder.ts
+++ apps/create-plugin/src/builder.ts
@@ -2,6 +2,7 @@ const fs = require('fs-extra')
 const path = require('path')
 import Handlebars from 'handlebars'
 import { cyan, red } from 'picocolors'
+import { isAddress } from 'viem'
 
 type BuilderParams = {
   projectName: string
@@ -63,6 +64,15 @@ function registerHelpers() {
   })
   Handlebars.registerHelper('capitalize', function (aString) {
     return capitalize(aString)
+  })
+  Handlebars.registerHelper('addressToString', function(value) {
+    if (typeof value === 'string' && isAddress(value)) {
+      return `'${value}'`
+    }
+    return value
+  })
+  Handlebars.registerHelper('eq', function (this: unknown, arg1, arg2, options) {
+    return arg1 === arg2 ? options.fn(this) : options.inverse(this)
   })
 }
 
