ab39e47b0896e51749c859a4670a29468a99ebd3
mmackz <matthewmackenz@gmail.com>
2024-03-01 15:21:54 -0800

    remove catch block

--- .github/scripts/plugins/registerNewPlugins.ts
+++ .github/scripts/plugins/registerNewPlugins.ts
@@ -4,36 +4,31 @@ const _yaml = require("js-yaml");
 const _utils = require("./utils");
 
 async function sendPluginDetailsToAPI(detailsPath: string): Promise<void> {
-  try {
-    const fileContents = await _fs.readFile(detailsPath, "utf8");
-    const details = _yaml.load(fileContents);
+  const fileContents = await _fs.readFile(detailsPath, "utf8");
+  const details = _yaml.load(fileContents);
 
 
-    const { project, task } = details;
+  const { project, task } = details;
 
-    // send details to staging API
-    const stagingApiUrl = process.env.STAGING_API_URL;
-    const { projectId: stagingProjectId } = await _axios.post(`${stagingApiUrl}/plugins/add-project`, {
-      ...project,
-      approvedForTerminal: true,
-    });
-    await _axios.post(`${stagingApiUrl}/plugins/add-task`, { 
-      ...task,
-      projectId: stagingProjectId, 
-      approvedForTerminal: true 
-    });
+  // send details to staging API
+  const stagingApiUrl = process.env.STAGING_API_URL;
+  const { projectId: stagingProjectId } = await _axios.post(`${stagingApiUrl}/plugins/add-project`, {
+    ...project,
+    approvedForTerminal: true,
+  });
+  await _axios.post(`${stagingApiUrl}/plugins/add-task`, { 
+    ...task,
+    projectId: stagingProjectId, 
+    approvedForTerminal: true 
+  });
 
-    // send details to production API
-    const productionApiUrl = process.env.PRODUCTION_API_URL;
-    const { projectId } = await _axios.post(`${productionApiUrl}/plugins/add-project`, project);
-    await _axios.post(`${productionApiUrl}/plugins/add-task`, { 
-      ...task,
-      projectId,
-    });
-  } catch (error) {
-    console.error(error.data);
-    throw new Error(`Error sending plugin details to API: ${error}`);
-  }
+  // send details to production API
+  const productionApiUrl = process.env.PRODUCTION_API_URL;
+  const { projectId } = await _axios.post(`${productionApiUrl}/plugins/add-project`, project);
+  await _axios.post(`${productionApiUrl}/plugins/add-task`, { 
+    ...task,
+    projectId,
+  });
 }
 
 async function _main() {
