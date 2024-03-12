0235414c7dcef81c5b319c115ebf26e32a586842
mmackz <matthewmackenz@gmail.com>
2024-03-12 15:41:18 -0700

    feat(manifold): calculate instanceId from tokenId

--- packages/manifold/src/Manifold.ts
+++ packages/manifold/src/Manifold.ts
@@ -1,15 +1,25 @@
 import {
-  type TransactionFilter,
-  type MintActionParams,
   compressJson,
+  type MintActionParams,
+  type TransactionFilter,
 } from '@rabbitholegg/questdk'
-import { type Address } from 'viem'
-import { Chains, type FilterOperator } from '@rabbitholegg/questdk-plugin-utils'
 import {
-  ERC721_CONTRACT,
-  ERC1155_CONTRACT,
+  chainIdToViemChain,
+  Chains,
+  type FilterOperator,
+} from '@rabbitholegg/questdk-plugin-utils'
+import {
+  createPublicClient,
+  http,
+  type Address,
+  type ContractFunctionReturnType,
+} from 'viem'
+import {
   ABI_MINT,
   ABI_MULTI,
+  ERC1155_CONTRACT,
+  ERC721_CONTRACT,
+  INSTANCEID_ABI,
 } from './constants'
 
 interface ManifoldInput {
@@ -20,14 +30,13 @@ interface ManifoldInput {
   mintFor?: string
 }
 
-// Function to evaluate if the ABI_MINT should not be included
 const shouldIncludeAbiMint = (amount: FilterOperator | undefined): boolean => {
   if (amount == null) return true
   if (typeof amount === 'object') {
     if ('$gte' in amount && (amount.$gte as bigint) >= 2) return false
     if ('$gt' in amount && (amount.$gt as bigint) >= 1) return false
     if ('$eq' in amount && (amount.$eq as bigint) >= 2) return false
-    if ('$lt' in amount && (amount.$lt as bigint) <= 1) return false // This might need special handling
+    if ('$lt' in amount && (amount.$lt as bigint) <= 1) return false
   } else {
     return Number(amount) === 1
   }
@@ -39,11 +48,38 @@ export const mint = async (
 ): Promise<TransactionFilter> => {
   const { chainId, contractAddress, tokenId, amount, recipient } = mint
 
+  let instanceId: number | undefined = undefined
+
+  if (tokenId) {
+    const client = createPublicClient({
+      chain: chainIdToViemChain(chainId),
+      transport: http(),
+    })
+
+    try {
+      const data: ContractFunctionReturnType<typeof INSTANCEID_ABI> =
+        await client.readContract({
+          address: ERC1155_CONTRACT, // ERC-721 should not use tokenId
+          abi: INSTANCEID_ABI,
+          functionName: 'getClaimForToken',
+          args: [contractAddress, tokenId],
+        })
+
+      if (typeof data === 'object' && Array.isArray(data)) {
+        instanceId = data[0]
+      }
+    } catch (e) {
+      // no instanceId can be found for this tokenId
+      // we should fail the transaction
+      instanceId = 0
+    }
+  }
+
   const inputConditions: ManifoldInput[] = [
     {
       $abi: ABI_MULTI,
       creatorContractAddress: contractAddress,
-      instanceId: undefined,
+      instanceId,
       mintCount: amount,
       mintFor: recipient,
     },
@@ -53,7 +89,7 @@ export const mint = async (
     inputConditions.push({
       $abi: ABI_MINT,
       creatorContractAddress: contractAddress,
-      instanceId: undefined,
+      instanceId,
       mintFor: recipient,
     })
   }
@@ -72,11 +108,9 @@ export const mint = async (
 export const getSupportedTokenAddresses = async (
   _chainId: number,
 ): Promise<Address[]> => {
-  // Given a specific chain we would expect this function to return a list of supported token addresses
   return []
 }
 
 export const getSupportedChainIds = async (): Promise<number[]> => {
-  // This should return all of the ChainIds that are supported by the Project we're integrating
   return [Chains.OPTIMISM, Chains.BASE]
 }
--- packages/manifold/src/constants.ts
+++ packages/manifold/src/constants.ts
@@ -107,3 +107,50 @@ export const ERC721_MINT_ABI = [
     type: 'function',
   },
 ]
+
+export const INSTANCEID_ABI = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'creatorContractAddress',
+        type: 'address',
+      },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    name: 'getClaimForToken',
+    outputs: [
+      { internalType: 'uint256', name: 'instanceId', type: 'uint256' },
+      {
+        components: [
+          { internalType: 'uint32', name: 'total', type: 'uint32' },
+          { internalType: 'uint32', name: 'totalMax', type: 'uint32' },
+          { internalType: 'uint32', name: 'walletMax', type: 'uint32' },
+          { internalType: 'uint48', name: 'startDate', type: 'uint48' },
+          { internalType: 'uint48', name: 'endDate', type: 'uint48' },
+          {
+            internalType: 'enum ILazyPayableClaim.StorageProtocol',
+            name: 'storageProtocol',
+            type: 'uint8',
+          },
+          { internalType: 'bytes32', name: 'merkleRoot', type: 'bytes32' },
+          { internalType: 'string', name: 'location', type: 'string' },
+          { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+          { internalType: 'uint256', name: 'cost', type: 'uint256' },
+          {
+            internalType: 'address payable',
+            name: 'paymentReceiver',
+            type: 'address',
+          },
+          { internalType: 'address', name: 'erc20', type: 'address' },
+          { internalType: 'address', name: 'signingAddress', type: 'address' },
+        ],
+        internalType: 'struct IERC1155LazyPayableClaim.Claim',
+        name: 'claim',
+        type: 'tuple',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+]
