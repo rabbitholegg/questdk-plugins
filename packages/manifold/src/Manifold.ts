e75dfd329d250af47e8a64c11c9b21b069230c42
mmackz <matthewmackenz@gmail.com>
2024-03-12 17:35:38 -0700

    refactor(manifold): extract getInstanceId function to utils file

--- packages/manifold/src/Manifold.ts
+++ packages/manifold/src/Manifold.ts
@@ -3,35 +3,19 @@ import {
   type MintActionParams,
   type TransactionFilter,
 } from '@rabbitholegg/questdk'
-import {
-  chainIdToViemChain,
-  Chains,
-  type FilterOperator,
-} from '@rabbitholegg/questdk-plugin-utils'
-import {
-  createPublicClient,
-  http,
-  type Address,
-  type ContractFunctionReturnType,
-} from 'viem'
+import { Chains } from '@rabbitholegg/questdk-plugin-utils'
+import { type Address } from 'viem'
 import {
   ABI_MINT,
   ABI_MULTI,
   ERC1155_CONTRACT,
   ERC721_CONTRACT,
-  INSTANCEID_ABI,
 } from './constants'
-import { shouldIncludeAbiMint } from './utils'
-
-interface ManifoldInput {
-  $abi: typeof ABI_MULTI | typeof ABI_MINT
-  creatorContractAddress: string
-  instanceId?: number | string
-  mintCount?: FilterOperator | undefined
-  mintFor?: string
-}
-
-const instanceIdCache: Record<string, number | undefined> = {}
+import {
+  shouldIncludeAbiMint,
+  getInstanceId,
+  type ManifoldInput,
+} from './utils'
 
 export const mint = async (
   mint: MintActionParams,
@@ -41,33 +25,7 @@ export const mint = async (
   let instanceId: number | undefined = undefined
 
   if (tokenId) {
-    const cacheKey = `${chainId}-${contractAddress}-${tokenId}`
-    if (instanceIdCache[cacheKey] !== undefined) {
-      instanceId = instanceIdCache[cacheKey]
-    } else {
-      const client = createPublicClient({
-        chain: chainIdToViemChain(chainId),
-        transport: http(),
-      })
-
-      try {
-        const data: ContractFunctionReturnType<typeof INSTANCEID_ABI> =
-          await client.readContract({
-            address: ERC1155_CONTRACT, // ERC-721 should not use tokenId
-            abi: INSTANCEID_ABI,
-            functionName: 'getClaimForToken',
-            args: [contractAddress, tokenId],
-          })
-
-        if (typeof data === 'object' && Array.isArray(data)) {
-          instanceId = data[0]
-          instanceIdCache[cacheKey] = instanceId
-        }
-      } catch (e) {
-        instanceId = 0
-        instanceIdCache[cacheKey] = instanceId
-      }
-    }
+    instanceId = await getInstanceId(chainId, contractAddress, tokenId)
   }
 
   const inputConditions: ManifoldInput[] = [
--- packages/manifold/src/utils.ts
+++ packages/manifold/src/utils.ts
@@ -1,4 +1,27 @@
-import { type FilterOperator } from '@rabbitholegg/questdk'
+import {
+  chainIdToViemChain,
+  type FilterOperator,
+} from '@rabbitholegg/questdk-plugin-utils'
+import {
+  ERC1155_CONTRACT,
+  INSTANCEID_ABI,
+  ABI_MINT,
+  ABI_MULTI,
+} from './constants'
+import {
+  createPublicClient,
+  http,
+  type Address,
+  type ContractFunctionReturnType,
+} from 'viem'
+
+export interface ManifoldInput {
+  $abi: typeof ABI_MULTI | typeof ABI_MINT
+  creatorContractAddress: string
+  instanceId?: number | string
+  mintCount?: FilterOperator | undefined
+  mintFor?: string
+}
 
 export const shouldIncludeAbiMint = (
   amount: FilterOperator | undefined,
@@ -13,4 +36,43 @@ export const shouldIncludeAbiMint = (
     return Number(amount) === 1
   }
   return true
+}
+
+const instanceIdCache: Record<string, number | undefined> = {}
+
+export async function getInstanceId(
+  chainId: number,
+  contractAddress: Address,
+  tokenId?: number,
+): Promise<number | undefined> {
+  if (tokenId == null) return undefined
+
+  const cacheKey = `${chainId}-${contractAddress}-${tokenId}`
+  if (instanceIdCache[cacheKey]) {
+    return instanceIdCache[cacheKey]
+  }
+
+  const client = createPublicClient({
+    chain: chainIdToViemChain(chainId),
+    transport: http(),
+  })
+
+  try {
+    const data: ContractFunctionReturnType<typeof INSTANCEID_ABI> =
+      await client.readContract({
+        address: ERC1155_CONTRACT, // ERC-721 should not use tokenId
+        abi: INSTANCEID_ABI,
+        functionName: 'getClaimForToken',
+        args: [contractAddress, tokenId],
+      })
+
+    if (typeof data === 'object' && Array.isArray(data)) {
+      instanceIdCache[cacheKey] = data[0]
+      return data[0]
+    }
+  } catch (e) {
+    instanceIdCache[cacheKey] = 0
+    return 0
+  }
+  return undefined
 }
