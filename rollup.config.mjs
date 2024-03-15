4a97acecc8f5e71c5fb8c6cb098c3103549e2f9a
Quazia <alf40k@gmail.com>
2024-03-14 21:05:31 -0400

    Fix(rollup): tweak root rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,33 +1,47 @@
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import typescript from '@rollup/plugin-typescript';
 import terser from "@rollup/plugin-terser";
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-;
-import json from '@rollup/plugin-json';
 
-export default [
-  {
-    input: 'src/index.ts', // Your source entry point
-    output: [
-      {
-        dir: 'dist/cjs',
-        format: 'cjs', // CommonJS output
-        sourcemap: true,
-      },
-      {
-        dir: 'dist/esm',
-        format: 'esm', // ESM output
-        sourcemap: true,
-      }
-    ],
-    plugins: [
-      resolve(), // Resolves node modules
-      commonjs(), // Converts CommonJS modules to ES6
-      terser(), // Minify the output (optional),
-      json(), // Support JSON imports
-    ]
-  }
-];
+const extensions = ['.js', '.ts' ];
+
+export default  {
+  input: 'src/index.ts',
+  output: [
+    {
+      inlineDynamicImports: true,
+      file: 'dist/bundles/bundle.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      inlineDynamicImports: true,
+      file: 'dist/bundles/bundle.esm.min.js',
+      format: 'esm',
+      plugins: [terser()],
+      sourcemap: true
+    },
+    {
+      inlineDynamicImports: true, 
+      file: 'dist/bundles/bundle.umd.js',
+      format: 'umd',
+      name: 'myLibrary',
+      sourcemap: true
+    },
+    {
+      inlineDynamicImports: true,
+      file: 'dist/bundles/bundle.umd.min.js',
+      format: 'umd',
+      name: 'myLibrary',
+      plugins: [terser()],
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    json(),
+    resolve({ extensions,  preferBuiltins: true }),
+    commonjs(),    
+    babel({ babelHelpers: 'bundled', include: ['src/**/*.ts'], extensions, exclude: './node_modules/**'})
+  ]
+}
