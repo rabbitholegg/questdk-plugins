a12ee950b725e35766b6e9cf7acea643258b242d
abs3ntdev <abs3ntdev@users.noreply.github.com>
2024-03-15 14:28:34 -0700

    format

--- packages/okutrade/src/constants.ts
+++ packages/okutrade/src/constants.ts
@@ -1,5 +1,5 @@
-import { Chains } from "@rabbitholegg/questdk-plugin-utils";
-import type { Address } from "viem";
+import { Chains } from '@rabbitholegg/questdk-plugin-utils'
+import type { Address } from 'viem'
 
 export const CHAIN_ID_ARRAY = [
   Chains.ETHEREUM,
@@ -8,61 +8,61 @@ export const CHAIN_ID_ARRAY = [
   Chains.POLYGON_POS,
   Chains.ZK_SYNC_ERA,
   Chains.BASE,
-] as number[];
+] as number[]
 
 /* 
   Command param type definitions:
   https://github.com/Uniswap/universal-router-sdk/blob/6ec60ce9ff2853e236ba8f40a3aaa8819a97bd8b/src/utils/routerCommands.ts#L74
 */
 export const V3_SWAP_EXACT_TYPES = [
-  "address recipient",
-  "uint256 amountIn",
-  "uint256 amountOut",
-  "bytes path",
-  "bool payerIsUser",
-];
+  'address recipient',
+  'uint256 amountIn',
+  'uint256 amountOut',
+  'bytes path',
+  'bool payerIsUser',
+]
 
 export const V2_SWAP_EXACT_TYPES = [
-  "address recipient",
-  "uint256 amountIn",
-  "uint256 amountOut",
-  "address[] path",
-  "bool payerIsUser",
-];
+  'address recipient',
+  'uint256 amountIn',
+  'uint256 amountOut',
+  'address[] path',
+  'bool payerIsUser',
+]
 
 export const EXECUTE_ABI_FRAGMENTS = [
   {
     inputs: [
-      { internalType: "bytes", name: "commands", type: "bytes" },
-      { internalType: "bytes[]", name: "inputs", type: "bytes[]" },
+      { internalType: 'bytes', name: 'commands', type: 'bytes' },
+      { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
     ],
-    name: "execute",
+    name: 'execute',
     outputs: [],
-    stateMutability: "payable",
-    type: "function",
+    stateMutability: 'payable',
+    type: 'function',
   },
   {
     inputs: [
-      { internalType: "bytes", name: "commands", type: "bytes" },
-      { internalType: "bytes[]", name: "inputs", type: "bytes[]" },
-      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: 'bytes', name: 'commands', type: 'bytes' },
+      { internalType: 'bytes[]', name: 'inputs', type: 'bytes[]' },
+      { internalType: 'uint256', name: 'deadline', type: 'uint256' },
     ],
-    name: "execute",
+    name: 'execute',
     outputs: [],
-    stateMutability: "payable",
-    type: "function",
+    stateMutability: 'payable',
+    type: 'function',
   },
-];
+]
 
-export const LIMIT_ORDER_REGISTRY_ABI = [{}];
+export const LIMIT_ORDER_REGISTRY_ABI = [{}]
 
 export const LIMIT_ORDER_REGISTRY_CONTRACT: {
-  [chainId: number]: Address | undefined;
+  [chainId: number]: Address | undefined
 } = {
-  [Chains.ETHEREUM]: "0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf",
-  [Chains.OPTIMISM]: "0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf",
-  [Chains.ARBITRUM_ONE]: "0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf",
-  [Chains.POLYGON_POS]: "0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf",
-  [Chains.ZK_SYNC_ERA]: "0x0FD66bD1e0974e2535CB424E6675D60aC52a84Fa",
-  [Chains.BASE]: "0xff8b754c64e9a8473bd6e1118d0eac67f0a8ae27",
-};
+  [Chains.ETHEREUM]: '0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf',
+  [Chains.OPTIMISM]: '0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf',
+  [Chains.ARBITRUM_ONE]: '0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf',
+  [Chains.POLYGON_POS]: '0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf',
+  [Chains.ZK_SYNC_ERA]: '0x0FD66bD1e0974e2535CB424E6675D60aC52a84Fa',
+  [Chains.BASE]: '0xff8b754c64e9a8473bd6e1118d0eac67f0a8ae27',
+}
--- packages/okutrade/src/utils.ts
+++ packages/okutrade/src/utils.ts
@@ -1,117 +1,117 @@
-import type { FilterOperator } from "@rabbitholegg/questdk";
-import { getAddress, type Address } from "viem";
-import { Chains } from "@rabbitholegg/questdk-plugin-utils";
-import axios from "axios";
+import type { FilterOperator } from '@rabbitholegg/questdk'
+import { getAddress, type Address } from 'viem'
+import { Chains } from '@rabbitholegg/questdk-plugin-utils'
+import axios from 'axios'
 
 const request = async (chain: string, method: string, data: any) => {
   const result = await fetch(`https://omni.oku.zone/${chain}`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "content-type": "application/json",
+      'content-type': 'application/json',
     },
     body: JSON.stringify({
-      jsonrpc: "2.0",
-      id: "1",
+      jsonrpc: '2.0',
+      id: '1',
       method: method,
       params: data,
     }),
-  });
-  const resp = await result.json();
+  })
+  const resp = await result.json()
   if (resp.error) {
-    throw resp.error;
+    throw resp.error
   }
-  return resp.result;
-};
+  return resp.result
+}
 
 export const buildV3PathQuery = (tokenIn?: string, tokenOut?: string) => {
   // v3 paths are formatted as 0x<token><fee><token>
 
-  const conditions: FilterOperator[] = [];
+  const conditions: FilterOperator[] = []
 
   if (tokenIn) {
-    conditions.push({ $regex: `^${tokenIn.toLowerCase()}` });
+    conditions.push({ $regex: `^${tokenIn.toLowerCase()}` })
   }
 
   if (tokenOut) {
     // Chop the 0x prefix before comparing
-    conditions.push({ $regex: `${tokenOut.toLowerCase().slice(2)}$` });
+    conditions.push({ $regex: `${tokenOut.toLowerCase().slice(2)}$` })
   }
 
   return {
     $and: conditions,
-  };
-};
+  }
+}
 
 export const getPools = async (tokenIn: string, chainId: number) => {
-  const chain = getChainName(chainId);
-  const resp = await request(chain, "getTokenPools", [tokenIn]);
-  return resp as string[];
-};
+  const chain = getChainName(chainId)
+  const resp = await request(chain, 'getTokenPools', [tokenIn])
+  return resp as string[]
+}
 
 export const buildV2PathQuery = (tokenIn?: string, tokenOut?: string) => {
   // v2 paths are formatted as [<token>, <token>]
-  const conditions: FilterOperator[] = [];
+  const conditions: FilterOperator[] = []
 
   if (tokenIn) {
-    conditions.push({ $first: getAddress(tokenIn) });
+    conditions.push({ $first: getAddress(tokenIn) })
   }
 
   if (tokenOut) {
-    conditions.push({ $last: getAddress(tokenOut) });
+    conditions.push({ $last: getAddress(tokenOut) })
   }
 
   return {
     $and: conditions,
-  };
-};
+  }
+}
 
 const chainToContract: Record<number, Address> = {
-  [Chains.ETHEREUM]: "0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B",
-  [Chains.OPTIMISM]: "0xb555edF5dcF85f42cEeF1f3630a52A108E55A654",
-  [Chains.POLYGON_POS]: "0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5",
-  [Chains.ZK_SYNC_ERA]: "0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06",
-  [Chains.ARBITRUM_ONE]: "0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5",
-  [Chains.BASE]: "0xeC8B0F7Ffe3ae75d7FfAb09429e3675bb63503e4",
-};
+  [Chains.ETHEREUM]: '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B',
+  [Chains.OPTIMISM]: '0xb555edF5dcF85f42cEeF1f3630a52A108E55A654',
+  [Chains.POLYGON_POS]: '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5',
+  [Chains.ZK_SYNC_ERA]: '0x28731BCC616B5f51dD52CF2e4dF0E78dD1136C06',
+  [Chains.ARBITRUM_ONE]: '0x4C60051384bd2d3C01bfc845Cf5F4b44bcbE9de5',
+  [Chains.BASE]: '0xeC8B0F7Ffe3ae75d7FfAb09429e3675bb63503e4',
+}
 
 const chainToWETH: Record<number, Address> = {
-  [Chains.ETHEREUM]: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-  [Chains.OPTIMISM]: "0x4200000000000000000000000000000000000006",
-  [Chains.ARBITRUM_ONE]: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-  [Chains.POLYGON_POS]: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
-  [Chains.ZK_SYNC_ERA]: "0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91",
-  [Chains.BASE]: "0x4200000000000000000000000000000000000006",
-};
+  [Chains.ETHEREUM]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  [Chains.OPTIMISM]: '0x4200000000000000000000000000000000000006',
+  [Chains.ARBITRUM_ONE]: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+  [Chains.POLYGON_POS]: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+  [Chains.ZK_SYNC_ERA]: '0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91',
+  [Chains.BASE]: '0x4200000000000000000000000000000000000006',
+}
 
 const chainIdToName: Record<number, string> = {
-  1: "ethereum",
-  10: "optimism",
-  42161: "arbitrum",
-  137: "polygon",
-  324: "zksync",
-  8453: "base",
-};
+  1: 'ethereum',
+  10: 'optimism',
+  42161: 'arbitrum',
+  137: 'polygon',
+  324: 'zksync',
+  8453: 'base',
+}
 
 export function getUniversalRouter(chainId: number): Address {
-  const address = chainToContract[chainId];
+  const address = chainToContract[chainId]
   if (!address) {
-    throw new Error(`Contract address not found for chain ID ${chainId}`);
+    throw new Error(`Contract address not found for chain ID ${chainId}`)
   }
-  return address;
+  return address
 }
 
 export function getWETHAddress(chainId: number): Address {
-  const address = chainToWETH[chainId];
+  const address = chainToWETH[chainId]
   if (!address) {
-    throw new Error(`WETH address not found for chain ID ${chainId}`);
+    throw new Error(`WETH address not found for chain ID ${chainId}`)
   }
-  return address;
+  return address
 }
 
 export function getChainName(chainId: number): string {
-  const name = chainIdToName[chainId];
+  const name = chainIdToName[chainId]
   if (!name) {
-    throw new Error(`Chain name not found for chain ID ${chainId}`);
+    throw new Error(`Chain name not found for chain ID ${chainId}`)
   }
-  return name;
+  return name
 }
