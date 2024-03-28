d53edb676a17b4f80dc3f9e7bb1128b1a7c7c158
mmackz <matthewmackenz@gmail.com>
2024-03-28 14:11:30 -0700

    feat(sound): split v1 and v2 abi

--- packages/soundxyz/src/constants.ts
+++ packages/soundxyz/src/constants.ts
@@ -1,6 +1,53 @@
 export const SUPERMINTER = '0x0000000000CF4558c36229ac0026ee16D3aE35Cd'
 export const SUPERMINTER_V2 = '0x000000000001A36777f9930aAEFf623771b13e70'
-export const SUPERMINTER_ABI = [
+
+export const SUPERMINTER_V1_ABI = [
+  {
+    inputs: [
+      {
+        components: [
+          { internalType: 'address', name: 'edition', type: 'address' },
+          { internalType: 'uint8', name: 'tier', type: 'uint8' },
+          { internalType: 'uint8', name: 'scheduleNum', type: 'uint8' },
+          { internalType: 'address', name: 'to', type: 'address' },
+          { internalType: 'uint32', name: 'quantity', type: 'uint32' },
+          { internalType: 'address', name: 'allowlisted', type: 'address' },
+          {
+            internalType: 'uint32',
+            name: 'allowlistedQuantity',
+            type: 'uint32',
+          },
+          {
+            internalType: 'bytes32[]',
+            name: 'allowlistProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint96', name: 'signedPrice', type: 'uint96' },
+          { internalType: 'uint32', name: 'signedQuantity', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedClaimTicket', type: 'uint32' },
+          { internalType: 'uint32', name: 'signedDeadline', type: 'uint32' },
+          { internalType: 'bytes', name: 'signature', type: 'bytes' },
+          { internalType: 'address', name: 'affiliate', type: 'address' },
+          {
+            internalType: 'bytes32[]',
+            name: 'affiliateProof',
+            type: 'bytes32[]',
+          },
+          { internalType: 'uint256', name: 'attributionId', type: 'uint256' },
+        ],
+        internalType: 'struct ISuperMinter.MintTo',
+        name: 'p',
+        type: 'tuple',
+      },
+    ],
+    name: 'mintTo',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
+
+export const SUPERMINTER_V2_ABI = [
   {
     inputs: [
       {
@@ -67,129 +114,53 @@ export const SUPERMINTER_ABI = [
 export const NEXT_SCHEDULE_NUM_ABI = [
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: 'edition',
-        type: 'address',
-      },
-      {
-        internalType: 'uint8',
-        name: 'tier',
-        type: 'uint8',
-      },
+      { internalType: 'address', name: 'edition', type: 'address' },
+      { internalType: 'uint8', name: 'tier', type: 'uint8' },
     ],
     name: 'nextScheduleNum',
-    outputs: [
-      {
-        internalType: 'uint8',
-        name: '',
-        type: 'uint8',
-      },
-    ],
+    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
     stateMutability: 'view',
     type: 'function',
   },
+]
+
+export const TOTAL_PRICE_AND_FEES_V1_ABI = [
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: 'edition',
-        type: 'address',
-      },
+      { internalType: 'address', name: 'edition', type: 'address' },
+      { internalType: 'uint8', name: 'tier', type: 'uint8' },
+      { internalType: 'uint8', name: 'scheduleNum', type: 'uint8' },
+      { internalType: 'uint32', name: 'quantity', type: 'uint32' },
     ],
-    name: 'mintInfoList',
+    name: 'totalPriceAndFees',
     outputs: [
       {
         components: [
+          { internalType: 'uint256', name: 'total', type: 'uint256' },
+          { internalType: 'uint256', name: 'subTotal', type: 'uint256' },
+          { internalType: 'uint256', name: 'unitPrice', type: 'uint256' },
+          { internalType: 'uint256', name: 'platformFee', type: 'uint256' },
+          { internalType: 'uint256', name: 'platformFlatFee', type: 'uint256' },
           {
-            internalType: 'address',
-            name: 'edition',
-            type: 'address',
+            internalType: 'uint256',
+            name: 'platformTxFlatFee',
+            type: 'uint256',
           },
           {
-            internalType: 'uint8',
-            name: 'tier',
-            type: 'uint8',
+            internalType: 'uint256',
+            name: 'platformMintFlatFee',
+            type: 'uint256',
           },
           {
-            internalType: 'uint8',
-            name: 'scheduleNum',
-            type: 'uint8',
+            internalType: 'uint256',
+            name: 'platformMintBPSFee',
+            type: 'uint256',
           },
-          {
-            internalType: 'address',
-            name: 'platform',
-            type: 'address',
-          },
-          {
-            internalType: 'uint96',
-            name: 'price',
-            type: 'uint96',
-          },
-          {
-            internalType: 'uint32',
-            name: 'startTime',
-            type: 'uint32',
-          },
-          {
-            internalType: 'uint32',
-            name: 'endTime',
-            type: 'uint32',
-          },
-          {
-            internalType: 'uint32',
-            name: 'maxMintablePerAccount',
-            type: 'uint32',
-          },
-          {
-            internalType: 'uint32',
-            name: 'maxMintable',
-            type: 'uint32',
-          },
-          {
-            internalType: 'uint32',
-            name: 'minted',
-            type: 'uint32',
-          },
-          {
-            internalType: 'uint16',
-            name: 'affiliateFeeBPS',
-            type: 'uint16',
-          },
-          {
-            internalType: 'uint8',
-            name: 'mode',
-            type: 'uint8',
-          },
-          {
-            internalType: 'bool',
-            name: 'paused',
-            type: 'bool',
-          },
-          {
-            internalType: 'bool',
-            name: 'hasMints',
-            type: 'bool',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'affiliateMerkleRoot',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'bytes32',
-            name: 'merkleRoot',
-            type: 'bytes32',
-          },
-          {
-            internalType: 'address',
-            name: 'signer',
-            type: 'address',
-          },
+          { internalType: 'uint256', name: 'affiliateFee', type: 'uint256' },
         ],
-        internalType: 'struct ISuperMinterV2.MintInfo[]',
-        name: 'a',
-        type: 'tuple[]',
+        internalType: 'struct ISuperMinter.TotalPriceAndFees',
+        name: '',
+        type: 'tuple',
       },
     ],
     stateMutability: 'view',
@@ -197,7 +168,7 @@ export const NEXT_SCHEDULE_NUM_ABI = [
   },
 ]
 
-export const TOTAL_PRICE_AND_FEES_ABI = [
+export const TOTAL_PRICE_AND_FEES_V2_ABI = [
   {
     inputs: [],
     name: 'ExceedsMaxPerAccount',
