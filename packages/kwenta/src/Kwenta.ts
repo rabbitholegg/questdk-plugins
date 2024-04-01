4134ba85f5b46f67cccf953ee1f332e65435e91d
mmackz <matthewmackenz@gmail.com>
2024-04-01 11:40:32 -0700

    refactor(kwenta): remove ordertype

--- packages/kwenta/src/Kwenta.ts
+++ packages/kwenta/src/Kwenta.ts
@@ -10,20 +10,19 @@ import {
   type TransactionFilter,
   compressJson,
 } from '@rabbitholegg/questdk'
-import { Chains, OrderType } from '@rabbitholegg/questdk-plugin-utils'
+import { Chains } from '@rabbitholegg/questdk-plugin-utils'
 import { type Address } from 'viem'
 
 export const options = async (
   options: OptionsActionParams,
 ): Promise<TransactionFilter> => {
-  const { chainId, recipient, amount, token, orderType } = options
+  const { chainId, recipient, amount, token } = options
   const marketId = token !== undefined ? TOKEN_TO_MARKET[token] : undefined
-  const from = orderType === OrderType.Market ? '0x0' : recipient
 
   return compressJson({
     chainId,
     to: SMART_MARGIN_V3,
-    from,
+    from: recipient,
     input: {
       $abiAbstract: SMART_MARGIN_V3_COMMIT_ORDER_ABI,
       _sizeDelta: amount,
