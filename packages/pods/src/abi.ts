c65026b8cb6e4aa03ec07afe18dca597bbc28a60
mmackz <matthewmackenz@gmail.com>
2024-04-01 17:29:18 -0700

    refactor(pods): remove unused zora abi and add abi for fees

--- packages/pods/src/abi.ts
+++ packages/pods/src/abi.ts
@@ -1,261 +1,3 @@
-export const ZORA_MINTER_ABI_721 = [
-  // https://github.com/ourzora/zora-721-contracts/blob/main/src/ERC721Drop.sol#L384
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-    ],
-    name: 'purchase',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'string',
-        name: 'comment',
-        type: 'string',
-      },
-    ],
-    name: 'purchaseWithComment',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'recipient',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'string',
-        name: 'comment',
-        type: 'string',
-      },
-    ],
-    name: 'purchaseWithRecipient',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'recipient',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'string',
-        name: 'comment',
-        type: 'string',
-      },
-      {
-        internalType: 'address',
-        name: 'mintReferral',
-        type: 'address',
-      },
-    ],
-    name: 'mintWithRewards',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'maxQuantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'pricePerToken',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: 'merkleProof',
-        type: 'bytes32[]',
-      },
-    ],
-    name: 'purchasePresale',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'maxQuantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'pricePerToken',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: 'merkleProof',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'string',
-        name: 'comment',
-        type: 'string',
-      },
-    ],
-    name: 'purchasePresaleWithComment',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'maxQuantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'pricePerToken',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes32[]',
-        name: 'merkleProof',
-        type: 'bytes32[]',
-      },
-      {
-        internalType: 'string',
-        name: 'comment',
-        type: 'string',
-      },
-      {
-        internalType: 'address',
-        name: 'mintReferral',
-        type: 'address',
-      },
-    ],
-    name: 'purchasePresaleWithRewards',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // ERC721Drop
-  // https://github.com/ourzora/zora-protocol/blob/8d1fe9bdd79a552a8f74b4712451185f6aebf9a0/packages/1155-contracts/src/nft/ZoraCreator1155Impl.sol#L427
-]
-
-export const ZORA_MINTER_ABI_1155_LEGACY = [
-  {
-    inputs: [
-      {
-        internalType: 'contract IMinter1155',
-        name: 'minter',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'tokenId',
-        type: 'uint256',
-      },
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-      {
-        internalType: 'bytes',
-        name: 'minterArguments',
-        type: 'bytes',
-      },
-    ],
-    name: 'mint',
-    outputs: [],
-    stateMutability: 'payable',
-    type: 'function',
-  },
-]
 export const ZORA_MINTER_ABI_1155 = [
   {
     inputs: [
@@ -323,80 +65,41 @@ export const ZORA_MINTER_ABI_1155 = [
     stateMutability: 'payable',
     type: 'function',
   }, // ZoraCreator1155Impl,
+]
+
+export const FEES_ABI = [
   {
     inputs: [
-      {
-        internalType: 'address',
-        name: 'tokenRecipient',
-        type: 'address',
-      },
-      {
-        internalType: 'string',
-        name: 'message',
-        type: 'string',
-      },
+      { internalType: 'address', name: 'tokenContract', type: 'address' },
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
     ],
-    name: 'purchase',
+    name: 'sale',
     outputs: [
       {
-        internalType: 'uint256',
-        name: 'tokenId',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // Legacy WritingEditions
-  // https://github.com/soundxyz/sound-protocol/blob/0c29ae2f5ce2f27f3823cacf66b5180906070805/contracts/core/SoundEditionV1_2.sol#L274
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-      {
-        internalType: 'uint256',
-        name: 'quantity',
-        type: 'uint256',
-      },
-    ],
-    name: 'mint',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: 'fromTokenId',
-        type: 'uint256',
-      },
-    ],
-    stateMutability: 'payable',
-    type: 'function',
-  }, // Legacy SoundEditionV1_2
-  {
-    inputs: [],
-    name: 'nextTokenId',
-    outputs: [
-      {
-        internalType: 'uint256',
+        components: [
+          { internalType: 'uint64', name: 'saleStart', type: 'uint64' },
+          { internalType: 'uint64', name: 'saleEnd', type: 'uint64' },
+          {
+            internalType: 'uint64',
+            name: 'maxTokensPerAddress',
+            type: 'uint64',
+          },
+          { internalType: 'uint96', name: 'pricePerToken', type: 'uint96' },
+          { internalType: 'address', name: 'fundsRecipient', type: 'address' },
+        ],
+        internalType: 'struct ZoraCreatorFixedPriceSaleStrategy.SalesConfig',
         name: '',
-        type: 'uint256',
+        type: 'tuple',
       },
     ],
     stateMutability: 'view',
     type: 'function',
   },
-]
-
-export const UNIVERSAL_MINTER_ABI = [
   {
-    inputs: [
-      { internalType: 'address[]', name: '_targets', type: 'address[]' },
-      { internalType: 'bytes[]', name: '_calldatas', type: 'bytes[]' },
-      { internalType: 'uint256[]', name: '_values', type: 'uint256[]' },
-    ],
-    name: 'mintBatchWithoutFees',
-    outputs: [],
-    stateMutability: 'payable',
+    inputs: [],
+    name: 'mintFee',
+    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    stateMutability: 'pure',
     type: 'function',
   },
-] // universal batch mint
+]
