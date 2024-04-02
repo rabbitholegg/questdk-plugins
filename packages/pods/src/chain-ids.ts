a0b1c61fe1192a21fded5404dce5fb14c41611a7
mmackz <matthewmackenz@gmail.com>
2024-04-01 17:29:50 -0700

    refactor(pods): remove unused chains

--- packages/pods/src/chain-ids.ts
+++ packages/pods/src/chain-ids.ts
@@ -1,9 +1,3 @@
 import { Chains } from '@rabbitholegg/questdk-plugin-utils'
 
-export const CHAIN_ID_ARRAY = [
-  Chains.ARBITRUM_ONE,
-  Chains.BASE,
-  Chains.ETHEREUM,
-  Chains.OPTIMISM,
-  Chains.ZORA,
-]
+export const CHAIN_ID_ARRAY = [Chains.BASE]
