a725f5ae81e51bdb1a8bbf407944669209e9dcc2
mmackz <matthewmackenz@gmail.com>
2024-04-04 16:45:19 -0700

    feat(thruster): add V3 tx filter

--- packages/thruster/src/Thruster.ts
+++ packages/thruster/src/Thruster.ts
@@ -3,7 +3,8 @@ import {
   ETH_FOR_EXACT_TOKENS_FRAGMENT,
   ETH_FOR_TOKENS_FRAGMENTS,
   THRUSTER_V2_ADDRESS,
-  THRUSTER_V3_ABI,
+  THRUSTER_V3_EXACT_INPUT_ABI,
+  THRUSTER_V3_EXACT_OUTPUT_ABI,
   THRUSTER_V3_ADDRESS,
   TOKENS_FOR_ETH_FRAGMENTS,
   TOKENS_FOR_EXACT_ETH_FRAGMENT,
@@ -25,6 +26,9 @@ export const swap = async (
   const { chainId, tokenIn, tokenOut, amountIn, amountOut, recipient } = swap
 
   const ethUsedIn = tokenIn === zeroAddress
+  const ethUsedOut = tokenOut === zeroAddress
+  const tokenInOrWeth = ethUsedIn ? BLAST_WETH : tokenIn
+  const tokenOutOrWeth = ethUsedOut ? BLAST_WETH : tokenOut
 
   return compressJson({
     chainId: chainId,
@@ -38,48 +42,89 @@ export const swap = async (
     value: ethUsedIn ? amountIn : undefined,
     input: {
       $or: [
-        // V2 Routes
         {
-          $abi: ETH_FOR_TOKENS_FRAGMENTS,
-          path: buildV2PathQuery(BLAST_WETH, tokenOut),
-          amountOutMin: amountOut,
-          to: recipient,
+          // V2 Routes
+          $or: [
+            {
+              $abi: ETH_FOR_TOKENS_FRAGMENTS,
+              path: buildV2PathQuery(BLAST_WETH, tokenOut),
+              amountOutMin: amountOut,
+              to: recipient,
+            },
+            {
+              $abi: TOKENS_FOR_ETH_FRAGMENTS,
+              path: buildV2PathQuery(tokenIn, BLAST_WETH),
+              amountIn,
+              amountOutMin: amountOut,
+              to: recipient,
+            },
+            {
+              $abi: TOKENS_FOR_TOKENS_FRAGMENTS,
+              path: buildV2PathQuery(tokenIn, tokenOut),
+              amountIn,
+              amountOutMin: amountOut,
+              to: recipient,
+            },
+            {
+              $abi: [ETH_FOR_EXACT_TOKENS_FRAGMENT],
+              path: buildV2PathQuery(BLAST_WETH, tokenOut),
+              amountOut,
+              to: recipient,
+            },
+            {
+              $abi: [TOKENS_FOR_EXACT_ETH_FRAGMENT],
+              path: buildV2PathQuery(tokenIn, BLAST_WETH),
+              amountInMax: amountIn,
+              amountOut,
+              to: recipient,
+            },
+            {
+              $abi: [TOKENS_FOR_EXACT_TOKENS_FRAGMENT],
+              path: buildV2PathQuery(tokenIn, tokenOut),
+              amountInMax: amountIn,
+              amountOut,
+              to: recipient,
+            },
+          ],
         },
         {
-          $abi: TOKENS_FOR_ETH_FRAGMENTS,
-          path: buildV2PathQuery(tokenIn, BLAST_WETH),
-          amountIn,
-          amountOutMin: amountOut,
-          to: recipient,
+          // V3 Routes
+          $or: [
+            {
+              $abiAbstract: THRUSTER_V3_EXACT_OUTPUT_ABI,
+              params: {
+                $or: [
+                  {
+                    tokenIn: tokenInOrWeth,
+                    tokenOut: tokenOutOrWeth,
+                  },
+                  {
+                    // exact output has the reverse structure (tokenOut first)
+                    path: buildV3PathQuery(tokenOutOrWeth, tokenInOrWeth),
+                  },
+                ],
+                amountInMaximum: amountIn,
+                amountOut,
+              },
+            },
+            {
+              $abiAbstract: THRUSTER_V3_EXACT_INPUT_ABI,
+              params: {
+                $or: [
+                  {
+                    tokenIn: tokenInOrWeth,
+                    tokenOut: tokenOutOrWeth,
+                  },
+                  {
+                    path: buildV3PathQuery(tokenInOrWeth, tokenOutOrWeth),
+                  },
+                ],
+                amountIn,
+                amountOutMinimum: amountOut,
+              },
+            },
+          ],
         },
-        {
-          $abi: TOKENS_FOR_TOKENS_FRAGMENTS,
-          path: buildV2PathQuery(tokenIn, tokenOut),
-          amountIn,
-          amountOutMin: amountOut,
-          to: recipient,
-        },
-        {
-          $abi: [ETH_FOR_EXACT_TOKENS_FRAGMENT],
-          path: buildV2PathQuery(BLAST_WETH, tokenOut),
-          amountOut,
-          to: recipient,
-        },
-        {
-          $abi: [TOKENS_FOR_EXACT_ETH_FRAGMENT],
-          path: buildV2PathQuery(tokenIn, BLAST_WETH),
-          amountInMax: amountIn,
-          amountOut,
-          to: recipient,
-        },
-        {
-          $abi: [TOKENS_FOR_EXACT_TOKENS_FRAGMENT],
-          path: buildV2PathQuery(tokenIn, tokenOut),
-          amountInMax: amountIn,
-          amountOut,
-          to: recipient,
-        },
-        // V3 Routes
       ],
     },
   })
