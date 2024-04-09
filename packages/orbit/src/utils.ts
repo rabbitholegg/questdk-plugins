3176aa5d0e3f69007c3c8888b4198e300cab23a9
mmackz <matthewmackenz@gmail.com>
2024-04-09 11:06:05 -0700

    refactor(orbit): use filter operator type for duration

--- packages/orbit/src/utils.ts
+++ packages/orbit/src/utils.ts
@@ -1,16 +1,16 @@
-import { type FilterOperator } from '@rabbitholegg/questdk'
+import type { FilterOperator } from '@rabbitholegg/questdk'
 
-type Amount = FilterOperator | BigInt | number | string | undefined
-
-export function getUnlockTime(amount: Amount): FilterOperator | undefined {
-  if (amount === undefined) return undefined
+export function getUnlockTime(
+  duration: FilterOperator | undefined,
+): FilterOperator | undefined {
+  if (duration === undefined) return undefined
 
   // in seconds
   const now = Math.floor(new Date().getTime() / 1000)
 
-  if (typeof amount === 'object') {
-    const [operator, value] = Object.entries(amount)[0]
+  if (typeof duration === 'object') {
+    const [operator, value] = Object.entries(duration)[0]
     return { [operator]: BigInt(value) + BigInt(now) }
   }
-  return { $gte: BigInt(amount) + BigInt(now) }
+  return { $gte: BigInt(duration) + BigInt(now) }
 }
