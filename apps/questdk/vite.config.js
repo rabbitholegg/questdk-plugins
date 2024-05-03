9d3218fa9a5518a365e1bc257feb7de6c91e13de
Quazia <Quazia@users.noreply.github.com>
2024-05-03 01:18:54 +0000

    chore: format

--- apps/questdk/vite.config.js
+++ apps/questdk/vite.config.js
@@ -3,15 +3,17 @@ export default {
   build: {
     lib: {
       entry: {
-        'index': 'src/index.ts',
-        'actions': 'src/actions/index.ts',
-        'filter': 'src/filter/index.ts',
-        'utils': 'src/utils/index.ts',
-        'constants': 'src/constants.ts',
+        index: 'src/index.ts',
+        actions: 'src/actions/index.ts',
+        filter: 'src/filter/index.ts',
+        utils: 'src/utils/index.ts',
+        constants: 'src/constants.ts',
       },
       emptyOutDir: false,
       fileName: (module, name) => {
-        const outPath = `${module === 'es' ? 'esm' : 'cjs'}/${name.startsWith('index') ? 'index.js' : name + '/index.js'}`
+        const outPath = `${module === 'es' ? 'esm' : 'cjs'}/${
+          name.startsWith('index') ? 'index.js' : name + '/index.js'
+        }`
         return outPath
       },
     },
