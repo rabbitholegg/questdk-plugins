069d9d632afad2961b4a3643796dfa6368825167
Quazia <alf40k@gmail.com>
2024-05-03 03:33:07 -0400

    Test(neynar:follow): adjust API

--- packages/neynar/src/Neynar.test.ts
+++ packages/neynar/src/Neynar.test.ts
@@ -4,28 +4,18 @@ import axios from 'axios'
 import { type FollowersResponse, type Follower } from './types'
 
 const MockedFollowerSchema: Follower = {
+  object: 'user',
   fid: 1,
   username: 'actor',
-  display_name: 'Actor',
-  custody_address: 'actor_address',
-  pfp_url: 'http://example.com/pfp.jpg',
-  profile: {
-    follower_count: 100,
-    following_count: 100,
-    verifications: ['Verification 1', 'Verification 2'],
-    verified_addresses: {},
-    active_status: 'active',
-    power_badge: true,
+  viewer_context: {
+    following: true,
   },
 }
 
 const MockedFollowersResponse: FollowersResponse = {
   users: [
-    {
-      ...MockedFollowerSchema,
-    },
+    MockedFollowerSchema,
   ],
-  next: { cursor: null },
 }
 
 vi.mock('axios', () => {
@@ -55,71 +45,33 @@ describe('validateFollow function', () => {
   })
 
   it('should return true if the actor is a follower of the target', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: MockedFollowersResponse,
     })
 
     const result = await validateFollow(
       { target: 'target_fid' },
-      { actor: 'actor_address' },
+      { actor: '1' },
     )
     expect(result).toBe(true)
   })
 
   it('should return false if the actor is not a follower of the target', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: {},
     })
 
     const result = await validateFollow(
-      { target: 'target_fid' },
-      { actor: 'actor_address' },
+      { target: '3' },
+      { actor: '0xd7053a3e7f8c02a6939377e2ca32bcc2a23023a1' },
     )
     expect(result).toBe(false)
   })
 
-  it('should handle pagination correctly', async () => {
-    const firstResponse = {
-      users: [
-        {
-          ...MockedFollowerSchema,
-          custody_address: 'not_custody_address',
-        },
-      ],
-      next: { cursor: '50' },
-    }
-
-    const secondResponse = {
-      users: [
-        {
-          ...MockedFollowerSchema,
-          custody_address: 'actor_address',
-        },
-      ],
-      next: { cursor: null },
-    }
-    ;(axios.get as MockedFunction<typeof axios.get>)
-      .mockResolvedValueOnce({
-        status: 200,
-        data: firstResponse,
-      })
-      .mockResolvedValueOnce({
-        status: 200,
-        data: secondResponse,
-      })
-
-    const result = await validateFollow(
-      { target: 'target_fid' },
-      { actor: 'actor_address' },
-    )
-    expect(result).toBe(true)
-    expect(axios.get).toHaveBeenCalledTimes(2) // Ensure pagination was handled correctly
-  })
-
   it('should return false on API failure', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockRejectedValue(
+    (axios.get as MockedFunction<typeof axios.get>).mockRejectedValue(
       new Error('API failure'),
     )
 
@@ -133,19 +85,21 @@ describe('validateFollow function', () => {
 
 describe('translateAddressToFID function', () => {
   it('should return the custody address if the input is a valid address', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
-      data: [
-        {
-          custody_address: 'custody_address',
-        },
-      ],
+      data: {
+        '0xB2f784dCC11a696D8f54dC1692fEb2b660959A6A': [
+          {
+            fid: 1,
+          },
+        ],
+      },
     })
 
     const result = await translateAddressToFID(
       '0xB2f784dCC11a696D8f54dC1692fEb2b660959A6A',
     )
-    expect(result).toBe('custody_address')
+    expect(result).toBe(1)
   })
 
   it('should return null if the input is not a valid address', async () => {
@@ -154,7 +108,7 @@ describe('translateAddressToFID function', () => {
   })
 
   it('should return null if the API response does not contain a custody address', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: [{}],
     })
