06a4c1edfaca4fa85237394e038a981be8cc73ba
Quazia <alf40k@gmail.com>
2024-05-14 12:03:01 -0400

    test(utils): add getExitAddresses helper testing

--- packages/utils/src/tests/test-helpers.test.ts
+++ packages/utils/src/tests/test-helpers.test.ts
@@ -1,7 +1,8 @@
-import { createTestCase, type TestParams } from '../'
+import { Chains, createTestCase, getExitAddresses, type TestParams } from '../'
 import { describe, expect, test } from 'vitest'
 import type { Address } from 'viem'
 import type { SwapActionParams } from '../types'
+import { RELAYER_ADDRESSES } from '../constants/layer-zero-relayer-addresses'
 
 describe('createTestCase', () => {
   test('should create a test case with the given parameters', () => {
@@ -31,5 +32,46 @@ describe('createTestCase', () => {
       params: { ...testParams.params, ...overrides },
       description,
     })
+  })
+})
+describe('getExitAddresses', () => {
+  test('should return the correct exit addresses', () => {
+    const chainId = 7777777
+    const additionalAddresses: Address[] = ['0x3', '0x4']
+
+    const result = getExitAddresses(chainId, additionalAddresses)
+
+    const expectedAddresses = [
+      RELAYER_ADDRESSES[chainId],
+      ...additionalAddresses,
+    ].map((address) => address ? address.toLowerCase() : '')
+    console.log(result)
+    expect(result).toEqual({ $or: expectedAddresses })
+  })
+
+  test('should handle a single additional address as a string', () => {
+    const chainId = Chains.BASE
+    const additionalAddress: Address = '0x3'
+
+    const result = getExitAddresses(chainId, additionalAddress)
+
+    const expectedAddresses = [
+      RELAYER_ADDRESSES[chainId],
+      additionalAddress,
+    ].map((address) => address ? address.toLowerCase() : '')
+
+    expect(result).toEqual({ $or: expectedAddresses })
+  })
+
+  test('should handle no additional addresses', () => {
+    const chainId = Chains.ARBITRUM_ONE
+
+    const result = getExitAddresses(chainId)
+
+    const expectedAddresses = [RELAYER_ADDRESSES[chainId as keyof typeof RELAYER_ADDRESSES]].map((address) =>
+      address ? address.toLowerCase() : '',
+    )
+
+    expect(result).toEqual({ $or: expectedAddresses })
   })
 })
