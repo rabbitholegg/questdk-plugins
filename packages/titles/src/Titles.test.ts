6541d88357aa99d85871bfd7170efcf91a625833
mmackz <matthewmackenz@gmail.com>
2024-05-14 20:40:50 -0700

    test(titles): setup tests for create action

--- packages/titles/src/Titles.test.ts
+++ packages/titles/src/Titles.test.ts
@@ -6,8 +6,28 @@ import { create } from './Titles'
 describe('Given the titles plugin', () => {
   describe('When handling the create action', () => {
     describe('should return a valid action filter', () => {
-      // test that a valid filter is returned, check the link for a specific example from the sound.xyz package
-      // https://github.com/rabbitholegg/questdk-plugins/blob/6c7c91c6f6393e15f0bb58558ad0edb2c79a77ff/packages/soundxyz/src/Soundxyz.test.ts#L14-L34
+      test('when making a valid create action', async () => {
+        const filter = await create({
+          chainId: 8453,
+        })
+        expect(filter).toBeTypeOf('object')
+        expect(Number(filter.chainId)).toBe(8453)
+        if (typeof filter.to === 'string') {
+          expect(filter.to).toMatch(/^0x[a-fA-F0-9]{40}$/)
+        } else {
+          // if to is an object, it should have a logical operator as the only key
+          expect(filter.to).toBeTypeOf('object')
+          expect(Object.keys(filter.to)).toHaveLength(1)
+          expect(
+            ['$or', '$and'].some((prop) =>
+              Object.hasOwnProperty.call(filter.to, prop),
+            ),
+          ).to.be.true
+          expect(Object.values(filter.to)[0]).to.satisfy((arr: string[]) =>
+            arr.every((val) => val.match(/^0x[a-fA-F0-9]{40}$/)),
+          )
+        }
+      })
     })
 
     describe('should pass filter with valid transactions', () => {
