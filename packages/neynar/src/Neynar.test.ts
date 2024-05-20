8295899bfdf12ba5173214cfc721cb9725469755
Sam McCord <sam.mccord@protonmail.com>
2024-05-20 12:03:57 -0600

    chore(tests): test neynar recast functionality

--- packages/neynar/src/Neynar.test.ts
+++ packages/neynar/src/Neynar.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, beforeEach, vi, MockedFunction } from 'vitest'
-import { validateFollow, translateAddressToFID } from './Neynar'
+import { validateFollow, translateAddressToFID, validateRecast } from './Neynar'
 import axios from 'axios'
-import { type FollowersResponse, type Follower } from './types'
+import { type FollowersResponse, type Follower, ConversationResponse, Conversation } from './types'
 
 const MockedFollowerSchema: Follower = {
   object: 'user',
@@ -14,6 +14,37 @@ const MockedFollowerSchema: Follower = {
 
 const MockedFollowersResponse: FollowersResponse = {
   users: [MockedFollowerSchema],
+}
+
+const MockedConversationResponseSchema: ConversationResponse = {
+  'conversation': {
+    'cast': {
+      'reactions': {
+        'recasts': [
+          {
+            'fid': 23039,
+            'fname': '0mbre',
+          },
+          {
+            'fid': 19691,
+            'fname': 'rafael12',
+          },
+          {
+            'fid': 17848,
+            'fname': 'saeid1088',
+          },
+          {
+            'fid': 17213,
+            'fname': '4337',
+          },
+          {
+            'fid': 18136,
+            'fname': 'javad126070',
+          },
+        ],
+      },
+    },
+  },
 }
 
 vi.mock('axios', () => {
@@ -43,7 +74,7 @@ describe('validateFollow function', () => {
   })
 
   it('should return true if the actor is a follower of the target', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: MockedFollowersResponse,
     })
@@ -56,7 +87,7 @@ describe('validateFollow function', () => {
   })
 
   it('should return false if the actor is not a follower of the target', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: {},
     })
@@ -69,7 +100,7 @@ describe('validateFollow function', () => {
   })
 
   it('should return false on API failure', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockRejectedValue(
+    (axios.get as MockedFunction<typeof axios.get>).mockRejectedValue(
       new Error('API failure'),
     )
 
@@ -81,9 +112,54 @@ describe('validateFollow function', () => {
   })
 })
 
+describe('validateRecast function', () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('should return true if the actor has recast the target', async () => {
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+      status: 200,
+      data: MockedConversationResponseSchema,
+    })
+
+    const result = await validateRecast(
+      { identifier: '0x9288c1e862aa72bd69d0e383a28b9a76b63cbdb4' },
+      { actor: '17848' },
+    )
+    expect(result).toBe(true)
+  })
+
+  it('should return false if the actor has not recast the target', async () => {
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+      status: 200,
+      data: {},
+    })
+
+    const result = await validateRecast(
+      { identifier: '0x9288c1e862aa72bd69d0e383a28b9a76b63cbdb4' },
+      { actor: '999999999999' },
+    )
+    expect(result).toBe(false)
+  })
+
+  it('should return false on API failure', async () => {
+    (axios.get as MockedFunction<typeof axios.get>).mockRejectedValue(
+      new Error('API failure'),
+    )
+
+    const result = await validateRecast(
+      { identifier: '0x9288c1e862aa72bd69d0e383a28b9a76b63cbdb4' },
+      { actor: '999999999999' },
+    )
+    expect(result).toBe(false)
+  })
+})
+
+
 describe('translateAddressToFID function', () => {
   it('should return the custody address if the input is a valid address', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: {
         '0xB2f784dCC11a696D8f54dC1692fEb2b660959A6A': [
@@ -106,7 +182,7 @@ describe('translateAddressToFID function', () => {
   })
 
   it('should return null if the API response does not contain a custody address', async () => {
-    ;(axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
+    (axios.get as MockedFunction<typeof axios.get>).mockResolvedValue({
       status: 200,
       data: [{}],
     })
@@ -117,3 +193,5 @@ describe('translateAddressToFID function', () => {
     expect(result).toBe(null)
   })
 })
+
+
