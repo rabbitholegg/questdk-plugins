d328c308a929b9f329d0525bb58c5bc5960e78fd
mmackz <matthewmackenz@gmail.com>
2024-05-22 17:43:44 -0700

    feat(boost): add complete function to IActionPlugin

--- packages/boost/src/index.ts
+++ packages/boost/src/index.ts
@@ -3,12 +3,13 @@ import {
   PluginActionNotImplementedError,
 } from '@rabbitholegg/questdk'
 
-import { mint, getSupportedChainIds, getSupportedTokenAddresses } from './Boost'
+import { complete, getSupportedChainIds, getSupportedTokenAddresses, mint } from './Boost'
 
 export const Boost: IActionPlugin = {
   pluginId: 'boost',
   getSupportedTokenAddresses,
   getSupportedChainIds,
+  complete,
   bridge: async () => new PluginActionNotImplementedError(),
   swap: async () => new PluginActionNotImplementedError(),
   mint,
