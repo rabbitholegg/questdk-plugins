111e0fa44406512a729d9406e04f0916868948fe
mmackz <matthewmackenz@gmail.com>
2024-05-22 17:23:57 -0700

    feat(boost): add draft for complete plugin

--- packages/boost/src/Boost.ts
+++ packages/boost/src/Boost.ts
@@ -1,6 +1,7 @@
 import {
   type TransactionFilter,
   type MintActionParams,
+  type CompleteActionParams,
   compressJson,
 } from '@rabbitholegg/questdk'
 import { Chains } from '@rabbitholegg/questdk-plugin-utils'
@@ -8,8 +9,31 @@ import { type Address } from 'viem'
 import {
   BOOST_PASS_CONTRACT,
   BOOST_PASS_ABI,
+  COMPLETE_BOOST_ABI,
   DATA_ABI_PARAMS,
+  QUEST_FACTORY_CONTRACT,
 } from './constants'
+
+export const complete = async (
+  complete: CompleteActionParams,
+): Promise<TransactionFilter> => {
+  const { chainId, boostId, actionType } = complete
+
+  return compressJson({
+    chainId,
+    to: QUEST_FACTORY_CONTRACT,
+    input: {
+      $abi: COMPLETE_BOOST_ABI,
+      compressedData_: {
+        $and: [
+          // decompress data and compare to incoming value
+          {$compareCompressed: { value: boostId }},
+          {$compareCompressed: { value: actionType }},
+        ],
+      },
+    },
+  })
+}
 
 export const mint = async (
   mint: MintActionParams,
--- packages/boost/src/constants.ts
+++ packages/boost/src/constants.ts
@@ -1,4 +1,6 @@
 export const BOOST_PASS_CONTRACT = '0x9A618D6302f27cdBb97206Ce269A31C1F7da3913'
+export const QUEST_FACTORY_CONTRACT =
+  '0x52629961f71c1c2564c5aa22372cb1b9fa9eba3e'
 
 export const BOOST_PASS_ABI = [
   {
@@ -14,3 +16,13 @@ export const BOOST_PASS_ABI = [
 ]
 
 export const DATA_ABI_PARAMS = ['address recipient', 'address referrer']
+
+export const COMPLETE_BOOST_ABI = [
+  {
+    inputs: [{ internalType: 'bytes', name: 'compressedData_', type: 'bytes' }],
+    name: 'claimCompressed',
+    outputs: [],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
