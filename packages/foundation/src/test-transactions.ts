d150d2b790528ad03fe7e626296fd0c3ac79a919
mmackz <matthewmackenz@gmail.com>
2024-05-31 19:54:24 -0700

    test(foundation): add additional test for dutch auction mint

--- packages/foundation/src/test-transactions.ts
+++ packages/foundation/src/test-transactions.ts
@@ -4,7 +4,7 @@ import {
   type TestParams,
 } from '@rabbitholegg/questdk-plugin-utils'
 
-export const MINT_BASE: TestParams<MintActionParams> = {
+const MINT_BASE: TestParams<MintActionParams> = {
   transaction: {
     chainId: 8453,
     from: '0x22cf7cb48c74b07e1c0dcab5c047c5ed73292805',
@@ -16,11 +16,12 @@ export const MINT_BASE: TestParams<MintActionParams> = {
   },
   params: {
     chainId: 8453,
-    contractAddress: '0x62037b26fff91929655aa3a060f327b47d1e2b3e',
+    contractAddress: '0xead6dca70b0465725a57eb81f7d3ab8b5e0b81b4',
     recipient: '0x22cf7cb48c74b07e1c0dcab5c047c5ed73292805',
   },
 }
-export const MINT_ETHEREUM: TestParams<MintActionParams> = {
+
+const MINT_ETHEREUM: TestParams<MintActionParams> = {
   transaction: {
     chainId: 1,
     from: '0xd76dfe29f0371fb0640906c699165b6bab33c522',
@@ -32,18 +33,39 @@ export const MINT_ETHEREUM: TestParams<MintActionParams> = {
   },
   params: {
     chainId: 1,
-    contractAddress: '0x53f451165ba6fdbe39a134673d13948261b2334a',
+    contractAddress: '0x42cfdea063311dfe9bbb3d7b598fea24067909b4',
     recipient: '0xd76dfe29f0371fb0640906c699165b6bab33c522',
+  },
+}
+
+const MINT_DUTCH_AUCTION: TestParams<MintActionParams> = {
+  transaction: {
+    chainId: 1,
+    from: '0x2A44195b273dA05b0071D801C996B83E7FB460cA',
+    hash: '0xde69746ceb05818f7394aaeeb33abea5a5710417b4d700c2c4ef6506c96c904f',
+    input:
+      '0x16da9864000000000000000000000000ed3b072d1f2f8fbd42a13fdc02c6e9f7fab29d7100000000000000000000000000000000000000000000000000000000000000030000000000000000000000002a44195b273da05b0071d801c996b83e7fb460ca',
+    to: '0x53F451165Ba6fdbe39A134673d13948261B2334A',
+    value: '2400000000000001',
+  },
+  params: {
+    chainId: 1,
+    contractAddress: '0xed3b072d1f2f8fbd42a13fdc02c6e9f7fab29d71',
+    recipient: '0x2a44195b273da05b0071d801c996b83e7fb460ca',
+    amount: '3',
   },
 }
 
 export const passingTestCases = [
   createTestCase(MINT_BASE, 'when when minting a drop on base'),
   createTestCase(MINT_ETHEREUM, 'when when minting a drop on ethereum'),
+  createTestCase(MINT_DUTCH_AUCTION, 'when when minting a drop on dutch auction'),
 ]
 
 export const failingTestCases = [
   createTestCase(MINT_BASE, 'when chainId is not correct', { chainId: 10 }),
-  createTestCase(MINT_ETHEREUM, 'when contract is not correct', { contractAddress: '0x62037b26fff91929655aa3a060f327b47d1e2b3e' }),
+  createTestCase(MINT_ETHEREUM, 'when contract is not correct', {
+    contractAddress: '0x62037b26fff91929655aa3a060f327b47d1e2b3e',
+  }),
   createTestCase(MINT_BASE, 'when amount is not sufficient', { amount: '99' }),
 ]
