71791f134619084ac36e841f247bf48c2cf3ef14
mmackz <matthewmackenz@gmail.com>
2024-06-05 16:55:30 -0700

    test(lens): use mock for apollo client

--- packages/lens/src/Lens.test.ts
+++ packages/lens/src/Lens.test.ts
@@ -1,4 +1,5 @@
-import { describe, expect, test, vi } from 'vitest'
+import { client } from './graphql'
+import { beforeEach, describe, expect, MockedFunction, test, vi } from 'vitest'
 import { validateCollect } from './Lens'
 
 vi.mock('@apollo/client', async () => {
@@ -6,34 +7,7 @@ vi.mock('@apollo/client', async () => {
   return {
     ...(actualApollo as object),
     ApolloClient: vi.fn(() => ({
-      query: vi.fn().mockImplementation(({ variables }) => {
-        if (variables.request.on === '0x5dbb-0xd5') {
-          return Promise.resolve({
-            data: {
-              whoActedOnPublication: {
-                items: [
-                  {
-                    ownedBy: {
-                      address:
-                        '0xA99F898530dF1514A566f1a6562D62809e99557D'.toLowerCase(),
-                    },
-                  },
-                ],
-                pageInfo: { next: null },
-              },
-            },
-          })
-        } else {
-          return Promise.resolve({
-            data: {
-              whoActedOnPublication: {
-                items: [],
-                pageInfo: { next: null },
-              },
-            },
-          })
-        }
-      }),
+      query: vi.fn(),
       InMemoryCache: vi.fn(),
     })),
   }
@@ -41,7 +15,42 @@ vi.mock('@apollo/client', async () => {
 
 describe('Given the lens plugin', () => {
   describe('When handling the collect action', () => {
+    beforeEach(() => {
+      vi.resetAllMocks()
+    })
     test('return true if actor has collected a specific post', async () => {
+      // use mock
+      ;(
+        client.query as MockedFunction<typeof client.query>
+      ).mockResolvedValueOnce({
+        data: {
+          whoActedOnPublication: {
+            __typename: 'PaginatedProfileResult',
+            items: [
+              {
+                __typename: 'Profile',
+                ownedBy: {
+                  __typename: 'NetworkAddress',
+                  address: '0x53c64e9D4fCCaf0CF539ECFDa391c4783b5Ae335',
+                },
+              },
+              {
+                __typename: 'Profile',
+                ownedBy: {
+                  __typename: 'NetworkAddress',
+                  address: '0xA99F898530dF1514A566f1a6562D62809e99557D',
+                },
+              },
+            ],
+            pageInfo: {
+              __typename: 'PaginatedResultInfo',
+              next: null,
+            },
+          },
+        },
+        loading: false,
+        networkStatus: 7,
+      })
       const hasCollected = await validateCollect(
         { postId: '0x5dbb-0xd5' },
         { actor: '0xA99F898530dF1514A566f1a6562D62809e99557D' },
@@ -50,6 +59,23 @@ describe('Given the lens plugin', () => {
     })
 
     test('return false if actor has not collected a specific post', async () => {
+      // use mock
+      ;(
+        client.query as MockedFunction<typeof client.query>
+      ).mockResolvedValueOnce({
+        data: {
+          whoActedOnPublication: {
+            __typename: 'PaginatedProfileResult',
+            items: [],
+            pageInfo: {
+              __typename: 'PaginatedResultInfo',
+              next: null,
+            },
+          },
+        },
+        loading: false,
+        networkStatus: 7,
+      })
       const hasCollected = await validateCollect(
         { postId: '0x2fb0-0x69-DA-3096022f' },
         { actor: '0xA99F898530dF1514A566f1a6562D62809e99557D' },
