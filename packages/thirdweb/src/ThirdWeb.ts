0e3ad32680ce3c86e15d98d1940859c117c64022
mmackz <matthewmackenz@gmail.com>
2024-06-09 17:35:11 -0700

    feat(thirdweb): implement getFees function

--- packages/thirdweb/src/ThirdWeb.ts
+++ packages/thirdweb/src/ThirdWeb.ts
@@ -1,11 +1,25 @@
-import { CLAIM_1155_FRAGMENT, CLAIM_721_FRAGMENT } from './constants'
-import { getAmount, getContractType } from './utils'
+import { getClient } from './client'
+import {
+  CLAIM_1155_FRAGMENT,
+  CLAIM_721_FRAGMENT,
+  GET_CLAIM_ID_1155_FRAGMENT,
+  GET_CLAIM_CONDITION_1155_FRAGMENT,
+  GET_CLAIM_CONDITION_721_FRAGMENT,
+  GET_CLAIM_ID_721_FRAGMENT,
+} from './constants'
+import {
+  getContractType,
+  getMintAmount,
+  formatAmount,
+  getClaimConditionId,
+  getMintFee,
+} from './utils'
+import { type Address } from 'viem'
 import {
   type TransactionFilter,
   type MintActionParams,
   compressJson,
 } from '@rabbitholegg/questdk'
-import { type Address } from 'viem'
 import { Chains } from '@rabbitholegg/questdk-plugin-utils'
 
 export const mint = async (
@@ -18,13 +32,13 @@ export const mint = async (
   const Erc721Filter = {
     $abi: [CLAIM_721_FRAGMENT],
     _receiver: recipient,
-    _quantity: getAmount(amount),
+    _quantity: formatAmount(amount),
   }
 
   const Erc1155Filter = {
     $abi: [CLAIM_1155_FRAGMENT],
     _receiver: recipient,
-    _quantity: getAmount(amount),
+    _quantity: formatAmount(amount),
     _tokenId: tokenId,
   }
 
@@ -33,6 +47,59 @@ export const mint = async (
     to: contractAddress,
     input: contractType === '721' ? Erc721Filter : Erc1155Filter,
   })
+}
+
+export const getProjectFees = async (
+  mint: MintActionParams,
+): Promise<bigint> => {
+  const fees = await getFees(mint)
+  return fees.projectFee + fees.actionFee
+}
+
+export const getFees = async (
+  mint: MintActionParams,
+): Promise<{ actionFee: bigint; projectFee: bigint }> => {
+  const { chainId, contractAddress, amount, tokenId } = mint
+  const quantityToMint = getMintAmount(amount)
+  const client = getClient(chainId)
+
+  try {
+    const contractType = await getContractType(chainId, contractAddress)
+    const claimIdFragment =
+      contractType === '721'
+        ? GET_CLAIM_ID_721_FRAGMENT
+        : GET_CLAIM_ID_1155_FRAGMENT
+    const claimConditionFragment =
+      contractType === '721'
+        ? GET_CLAIM_CONDITION_721_FRAGMENT
+        : GET_CLAIM_CONDITION_1155_FRAGMENT
+
+    const claimConditionId = await getClaimConditionId(
+      client,
+      contractAddress,
+      [claimIdFragment],
+      contractType === '1155' ? tokenId : undefined,
+    )
+
+    const mintFee = await getMintFee(
+      client,
+      contractAddress,
+      [claimConditionFragment],
+      claimConditionId,
+      contractType === '1155' ? tokenId : undefined,
+    )
+
+    return {
+      actionFee: mintFee * quantityToMint,
+      projectFee: 0n,
+    }
+  } catch (error) {
+    // return fallback if any errors occur
+    return {
+      actionFee: 0n,
+      projectFee: 0n,
+    }
+  }
 }
 
 export const getSupportedTokenAddresses = async (
--- packages/thirdweb/src/index.ts
+++ packages/thirdweb/src/index.ts
@@ -1,6 +1,12 @@
 import { type IActionPlugin } from '@rabbitholegg/questdk'
+import {
+  type ActionParams,
+  type MintActionParams,
+} from '@rabbitholegg/questdk-plugin-utils'
 
 import {
+  getFees,
+  getProjectFees,
   getSupportedChainIds,
   getSupportedTokenAddresses,
   mint,
@@ -10,5 +16,9 @@ export const ThirdWeb: IActionPlugin = {
   pluginId: 'thirdweb',
   getSupportedTokenAddresses,
   getSupportedChainIds,
+  getFees: async (params: ActionParams) =>
+    getFees(params as unknown as MintActionParams),
+  getProjectFees: async (params: ActionParams) =>
+    getProjectFees(params as unknown as MintActionParams),
   mint,
 }
