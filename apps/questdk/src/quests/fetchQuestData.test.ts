261694907cc0d6de4ca598698d1ceefc75842588
mmackz <matthewmackenz@gmail.com>
2024-06-13 19:32:08 -0700

    refactor: update API endpoint for fetching quest data

--- apps/questdk/src/quests/fetchQuestData.test.ts
+++ apps/questdk/src/quests/fetchQuestData.test.ts
@@ -49,7 +49,7 @@ describe('fetchQuestActionParams', () => {
       actionParams: {} as any, // Fill with correct test data
     }
     mock
-      .onGet(`https://api.boost.xyz/boosts/${TEST_UUID}`)
+      .onGet(`https://api.rabbithole.gg/quests/${TEST_UUID}?isPublic=true`)
       .reply(200, mockResponse)
 
     const result = await fetchQuestByUUID(TEST_UUID)
@@ -58,7 +58,9 @@ describe('fetchQuestActionParams', () => {
   })
 
   test('should throw an error when the request fails', async () => {
-    mock.onGet(`https://api.boost.xyz/boosts/${TEST_UUID}`).reply(500)
+    mock
+      .onGet(`https://api.rabbithole.gg/quests/${TEST_UUID}?isPublic=true`)
+      .reply(500)
     await expect(fetchQuestByUUID(TEST_UUID)).rejects.toThrow(
       'Failed to fetch quest data',
     )
