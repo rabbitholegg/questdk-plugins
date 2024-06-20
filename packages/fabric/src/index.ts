56737f59d84985d871851bb519490987fa43935b
mmackz <matthewmackenz@gmail.com>
2024-06-20 14:05:25 -0700

    chore: export functions for getFees/simulateMint

--- packages/fabric/src/index.ts
+++ packages/fabric/src/index.ts
@@ -1,9 +1,17 @@
-import { type IActionPlugin } from '@rabbitholegg/questdk'
+import type {
+  ActionParams,
+  IActionPlugin,
+  MintActionParams,
+} from '@rabbitholegg/questdk-plugin-utils'
 
 import {
+  getFees,
+  getProjectFees,
+  getMintIntent,
   getSupportedChainIds,
   getSupportedTokenAddresses,
   mint,
+  simulateMint,
 } from './Fabric.js'
 
 export const Fabric: IActionPlugin = {
@@ -11,4 +19,10 @@ export const Fabric: IActionPlugin = {
   getSupportedTokenAddresses,
   getSupportedChainIds,
   mint,
+  getProjectFees: async (params: ActionParams) =>
+    getProjectFees(params as unknown as MintActionParams),
+  getFees: async (params: ActionParams) =>
+    getFees(params as unknown as MintActionParams),
+  getMintIntent,
+  simulateMint,
 }
