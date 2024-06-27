107b59c865a25f7f6d0e05efc7a58241ca2e8a1c
mmackz <matthewmackenz@gmail.com>
2024-06-27 16:01:47 -0700

    feat(lens): use alchemy sdk for validation

--- packages/lens/src/validate.ts
+++ packages/lens/src/validate.ts
@@ -1,11 +1,13 @@
+import { alchemy } from './alchemy'
 import {
   LensClient,
   type MultirecipientFeeCollectOpenActionSettingsFragment,
   type SimpleCollectOpenActionSettingsFragment,
   production,
 } from '@lens-protocol/client'
+import { GetTransfersForOwnerTransferType } from 'alchemy-sdk'
 import axios from 'axios'
-import { Address } from 'viem'
+import { Address, zeroAddress } from 'viem'
 
 const lensClient = new LensClient({
   environment: production,
@@ -57,44 +59,31 @@ export async function checkAddressMintedCollect(
   collectAddress: Address,
 ) {
   try {
-    return await checkMintedUsingBlockspan(actor, collectAddress)
+    return await checkMintedUsingAlchemy(actor, collectAddress)
   } catch (err) {
-    console.error('Error while using blockspan api')
-    if (err instanceof Error) {
-      console.error(err.message)
-    }
+    console.error('Error while using alchemy api', err)
     return await checkMintedUsingReservoir(actor, collectAddress)
   }
 }
 
-async function checkMintedUsingBlockspan(
+async function checkMintedUsingAlchemy(
   actor: Address,
   collectAddress: Address,
 ) {
-  if (!process.env.BLOCKSPAN_API_KEY) {
-    throw new Error('Blockspan API key not found')
-  }
-
-  const baseUrl = 'https://api.blockspan.com/v1/transfers/contract'
-  const params = new URLSearchParams({
-    chain: 'poly-main',
-    transfer_type: 'mint',
-    from_address: '0x0000000000000000000000000000000000000000',
-    to_address: actor,
-    page_size: '1',
-  })
-
   const options = {
-    method: 'GET',
-    url: `${baseUrl}/${collectAddress}?${params.toString()}`,
-    headers: {
-      accept: 'application/json',
-      'X-API-KEY': process.env.BLOCKSPAN_API_KEY,
-    },
+    contractAddresses: [collectAddress],
   }
+  
+  const transfers = await alchemy.nft.getTransfersForOwner(
+    actor,
+    GetTransfersForOwnerTransferType.TO,
+    options,
+  )
 
-  const response = await axios.request(options)
-  return Boolean(response.data?.results?.length > 0)
+  // only count nfts that originate from zeroAddress
+  const mintedNfts = transfers.nfts.filter((nft) => nft.from === zeroAddress)
+
+  return Boolean(mintedNfts.length > 0)
 }
 
 async function checkMintedUsingReservoir(
