34a43162f2088ddfb628c0b3f99d1482d070f590
mmackz <matthewmackenz@gmail.com>
2024-07-15 19:06:48 -0700

    refactor(utils): use Address type for referral

--- packages/utils/src/types/intents.ts
+++ packages/utils/src/types/intents.ts
@@ -1,9 +1,10 @@
 import type { MintActionParams } from './actions'
+import type { Address } from 'viem'
 export type MintIntentParams = Required<
   Omit<MintActionParams, 'amount' | 'tokenId' | 'referral'>
 > & {
   amount: bigint
   tokenId?: number
-  referral?: string
+  referral?: Address
 }
 export type IntentParams = MintIntentParams
