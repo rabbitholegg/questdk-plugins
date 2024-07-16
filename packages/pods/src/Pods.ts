c10163182b0a44d3a465feffedce4ab060f24bf3
mmackz <matthewmackenz@gmail.com>
2024-07-15 22:10:27 -0700

    refactor(pods): rename variable

--- packages/pods/src/Pods.ts
+++ packages/pods/src/Pods.ts
@@ -43,13 +43,13 @@ export const mint = async (
     },
   ]
   if (referral) {
-    const checksumAddress = getAddress(referral)
+    const referralAddress = getAddress(referral)
     andArray1155.push({
       $or: [
-        { mintReferral: checksumAddress },
+        { mintReferral: referralAddress },
         {
           rewardsRecipients: {
-            $and: [{ $first: checksumAddress }, { $last: checksumAddress }],
+            $and: [{ $first: referralAddress }, { $last: referralAddress }],
           },
         },
       ],
