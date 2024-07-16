17763d5d4fa96dae948b5a83b410eab1f77bfb0d
mmackz <matthewmackenz@gmail.com>
2024-07-15 18:50:55 -0700

    feat(utils): allow referral to be optional in mintIntentParams

--- packages/utils/src/types/intents.ts
+++ packages/utils/src/types/intents.ts
@@ -1,8 +1,9 @@
 import type { MintActionParams } from './actions'
 export type MintIntentParams = Required<
-  Omit<MintActionParams, 'amount' | 'tokenId'>
+  Omit<MintActionParams, 'amount' | 'tokenId' | 'referral'>
 > & {
   amount: bigint
   tokenId?: number
+  referral?: string
 }
 export type IntentParams = MintIntentParams
