da0625591efff621435b380786d812317ba9ccce
mmackz <matthewmackenz@gmail.com>
2024-07-19 12:19:17 -0700

    feat(zora): add support for sep and bsep url slugs

--- packages/zora/src/chain-ids.ts
+++ packages/zora/src/chain-ids.ts
@@ -12,8 +12,10 @@ export const CHAIN_ID_ARRAY = [
 export const CHAIN_ID_TO_ZORA_SLUG: Record<number, string> = {
   [Chains.ARBITRUM_ONE]: 'arb',
   [Chains.BASE]: 'base',
+  [Chains.BASE_SEPOLIA]: 'bsep',
   [Chains.BLAST]: 'blast',
   [Chains.ETHEREUM]: 'eth',
   [Chains.OPTIMISM]: 'oeth',
   [Chains.ZORA]: 'zora',
+  [Chains.SEPOLIA]: 'sep',
 }
