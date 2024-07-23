a02eb7bff907f5031bc956dca4674cb9a9585104
mmackz <matthewmackenz@gmail.com>
2024-07-23 16:04:44 -0600

    feat(titles): export plugin functions

--- packages/titles/src/index.ts
+++ packages/titles/src/index.ts
@@ -1,9 +1,19 @@
 import { type IActionPlugin } from '@rabbitholegg/questdk'
+import {
+  type ActionParams,
+  type MintActionParams,
+} from '@rabbitholegg/questdk-plugin-utils'
 
 import {
   create,
+  getExternalUrl,
+  getFees,
+  getMintIntent,
+  getProjectFees,
   getSupportedChainIds,
   getSupportedTokenAddresses,
+  mint,
+  simulateMint,
 } from './Titles'
 
 export const Titles: IActionPlugin = {
@@ -11,4 +21,13 @@ export const Titles: IActionPlugin = {
   getSupportedTokenAddresses,
   getSupportedChainIds,
   create,
+  getMintIntent,
+  mint,
+  simulateMint,
+  getExternalUrl: async (params: ActionParams) =>
+    getExternalUrl(params as unknown as MintActionParams),
+  getProjectFees: async (params: ActionParams) =>
+    getProjectFees(params as unknown as MintActionParams),
+  getFees: async (params: ActionParams) =>
+    getFees(params as unknown as MintActionParams),
 }
