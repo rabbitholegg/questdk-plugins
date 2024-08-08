7e1afda990af24cb5a6be4269371b165233725cb
mmackz <matthewmackenz@gmail.com>
2024-08-08 15:15:38 -0700

    feat: add functions for publicClient and contractType

--- packages/zora/src/utils.ts
+++ packages/zora/src/utils.ts
@@ -1,7 +1,10 @@
+import { chainIdToViemChain } from '@rabbitholegg/questdk-plugin-utils'
 import { FUNCTION_SELECTORS, ZORA_MINTER_ABI_1155 } from './abi'
 import {
   type Address,
   PublicClient,
+  createPublicClient,
+  http,
   keccak256,
   stringToBytes,
   toHex,
@@ -64,4 +67,54 @@ export const getNextTokenId = async (
   }
 
   return _tokenId
+}
+
+export const getPublicClient = (chainId: number) => {
+  return createPublicClient({
+    chain: chainIdToViemChain(chainId),
+    transport: http(),
+  }) as PublicClient
+}
+
+/**
+ * Determines the type of a contract (ERC-1155 or ERC-721) based on the supported interfaces.
+ * 
+ * @param client - The PublicClient used to interact with the blockchain.
+ * @param contractAddress - The address of the contract to check.
+ * @returns A promise that resolves to '1155' if the contract is ERC-1155, '721' if it is ERC-721, or null if neither.
+ */
+export async function getContractType(
+  client: PublicClient,
+  contractAddress: Address,
+): Promise<'1155' | '721' | null> {
+  const abi = [
+    {
+      inputs: [{ internalType: 'bytes4', name: 'interfaceId', type: 'bytes4' }],
+      name: 'supportsInterface',
+      outputs: [{ internalType: 'bool', name: 'isSupported', type: 'bool' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ]
+  const interfaceIds = {
+    '1155': '0xd9b67a26', // ERC-1155
+    '721': '0x80ac58cd', // ERC-721
+  }
+
+  for (const [type, interfaceId] of Object.entries(interfaceIds)) {
+    try {
+      const supportsInterface = (await client.readContract({
+        address: contractAddress,
+        abi,
+        functionName: 'supportsInterface',
+        args: [interfaceId],
+      })) as boolean
+
+      if (supportsInterface) {
+        return type as '1155' | '721'
+      }
+      // eslint-disable-next-line no-empty
+    } catch {}
+  }
+  return null
 }
